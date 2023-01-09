973f6643980431d28560386a57b17837d7f2aa55
solairerove <solairerove@gmail.com>
2023-01-09 14:09:02 +0800

    feature: merge two sorted lists

---
+++ linked_list/MergeTwoSortedLists.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from linked_list.ListNode import ListNode
+
+
+# O(n) time || O(1) space
+def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    if not list1:
+        return list2
+
+    if not list2:
+        return list1
+
+    sentinel = ListNode()
+    prev = sentinel
+    while list1 and list2:
+        if list1.val <= list2.val:
+            prev.next = list1
+            list1 = list1.next
+        else:
+            prev.next = list2
+            list2 = list2.next
+
+        prev = prev.next
+
+    if list1:
+        prev.next = list1
+    else:
+        prev.next = list2
+
+    return sentinel.next
---
+++ linked_list/test/test_merge_two_lists.py
@@ -0,0 +1,39 @@
+import unittest
+
+from linked_list.ListNode import ListNode
+from linked_list.MergeTwoSortedLists import merge_two_lists
+
+
+class MyTestCase(unittest.TestCase):
+    def test_merge_two_lists(self):
+        list1 = ListNode(
+            val=1,
+            next=ListNode(
+                val=2,
+                next=ListNode(
+                    val=4
+                )
+            )
+        )
+
+        list2 = ListNode(
+            val=1,
+            next=ListNode(
+                val=3,
+                next=ListNode(
+                    val=4
+                )
+            )
+        )
+
+        res = merge_two_lists(__name__, list1, list2)
+        arr = []
+        while res:
+            arr.append(res.val)
+            res = res.next
+
+        self.assertEqual([1, 1, 2, 3, 4, 4], arr)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
