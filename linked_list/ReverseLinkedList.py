67795f92f90840ad64fd8852310d8e574d4852ec
solairerove <solairerove@gmail.com>
2023-01-09 14:28:47 +0800

    feature: reverse linked list revisit

--- linked_list/ReverseLinkedList.py
+++ linked_list/ReverseLinkedList.py
@@ -6,10 +6,10 @@ from linked_list.ListNode import ListNode
 # O(n) time || O(1) space
 def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
     prev, curr = None, head
-    while curr:
-        nxt = curr.next
-        curr.next = prev
-        prev = curr
-        curr = nxt
+    while head:
+        nxt = head.next
+        head.next = prev
+        prev = head
+        head = nxt
 
     return prev
