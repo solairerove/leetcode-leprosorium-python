1ff62ea95b7772a6347f7deae432adf3f1d7215b
solairerove <solairerove@gmail.com>
2023-01-13 23:49:12 +0800

    feature: reverse nodes in k group

---
+++ linked_list/ReverseNodesInKGroup.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from linked_list.ListNode import ListNode
+
+
+# O(2n) time || O(1) space
+def reverse_k_group(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    sentinel = ListNode(-1, head)
+    group_prev = sentinel
+    while True:
+        kth = find_kth(group_prev, k)
+        if not kth:
+            break
+        group_next = kth.next
+
+        prev, curr = kth.next, group_prev.next
+        while curr != group_next:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        tmp = group_prev.next
+        group_prev.next = kth
+        group_prev = tmp
+
+    return sentinel.next
+
+
+def find_kth(self, curr, k) -> Optional[ListNode]:
+    while curr and k > 0:
+        k -= 1
+        curr = curr.next
+
+    return curr
---
+++ linked_list/test/test_reverse_k_group.py
@@ -0,0 +1,60 @@
+import unittest
+
+from linked_list.ListNode import ListNode
+from linked_list.ReverseNodesInKGroup import reverse_k_group
+
+
+class MyTestCase(unittest.TestCase):
+    def test_reverse_k_group(self):
+        head = ListNode(
+            val=1,
+            next=ListNode(
+                val=2,
+                next=ListNode(
+                    val=3,
+                    next=ListNode(
+                        val=4,
+                        next=ListNode(
+                            val=5
+                        )
+                    )
+                )
+            )
+        )
+
+        res = reverse_k_group(__name__, head, 2)
+        arr = []
+        while res:
+            arr.append(res.val)
+            res = res.next
+
+        self.assertEqual([2, 1, 4, 3, 5], arr)  # add assertion here
+
+    def test_reverse_k_group_1(self):
+        head = ListNode(
+            val=1,
+            next=ListNode(
+                val=2,
+                next=ListNode(
+                    val=3,
+                    next=ListNode(
+                        val=4,
+                        next=ListNode(
+                            val=5
+                        )
+                    )
+                )
+            )
+        )
+
+        res = reverse_k_group(__name__, head, 3)
+        arr = []
+        while res:
+            arr.append(res.val)
+            res = res.next
+
+        self.assertEqual([3, 2, 1, 4, 5], arr)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
