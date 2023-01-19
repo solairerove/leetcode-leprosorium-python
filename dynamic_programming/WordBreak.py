baf8b865e368d712b2827462a96c517b305fc4ca
solairerove <solairerove@gmail.com>
2023-01-19 13:36:17 +0800

    feature: fucking dp, word break constant space solution

--- dynamic_programming/WordBreak.py
+++ dynamic_programming/WordBreak.py
@@ -1,6 +1,20 @@
 from typing import List
 
 
+# O(n^2 * m) time || O(1) space
+def word_break_constant(self, s: str, word_dict: List[str]) -> bool:
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+    for i in range(len(s) - 1, -1, -1):
+        for w in word_dict:
+            if (i + len(w)) <= len(s) and s[i:i + len(w)] == w:
+                dp[i] = dp[i + len(w)]
+            if dp[i]:
+                break
+
+    return dp[0]
+
+
 # O(n^3) time || O(n) space
 def word_break(self, s: str, word_dict: List[str]) -> bool:
     word_dic = set(word_dict)
--- dynamic_programming/test/test_word_break.py
+++ dynamic_programming/test/test_word_break.py
@@ -1,14 +1,24 @@
 import unittest
 
-from dynamic_programming.WordBreak import word_break
+from dynamic_programming.WordBreak import word_break, word_break_constant
 
 
 class MyTestCase(unittest.TestCase):
+
+    def test_word_break_constant(self):
+        self.assertEqual(True, word_break_constant(self, "leetcode", ["leet", "code"]))
+
+    def test_word_break_constant_1(self):
+        self.assertEqual(True, word_break_constant(self, "applepenapple", ["apple", "pen"]))
+
+    def test_word_break_constant_2(self):
+        self.assertEqual(False, word_break_constant(self, "catsandog", ["cats", "dog", "sand", "and", "cat"]))
+
     def test_word_break(self):
         self.assertEqual(True, word_break(self, "leetcode", ["leet", "code"]))
 
     def test_word_break_1(self):
-        self.assertEqual(True, word_break(self, "applepenapple", ["apple", "pen"])) 
+        self.assertEqual(True, word_break(self, "applepenapple", ["apple", "pen"]))
 
     def test_word_break_2(self):
         self.assertEqual(False, word_break(self, "catsandog", ["cats", "dog", "sand", "and", "cat"]))
