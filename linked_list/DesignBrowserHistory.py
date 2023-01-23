20c85f0f5fe5e84dadcc154aff5b5e1acf8a3287
solairerove <solairerove@gmail.com>
2023-01-23 17:41:35 +0800

    feature: design browser history revisit

--- linked_list/DesignBrowserHistory.py
+++ linked_list/DesignBrowserHistory.py
@@ -2,40 +2,28 @@ class Node:
 
     def __init__(self, val: str):
         self.val = val
-        self.prev, self.nxt = None, None
+        self.prev = self.nxt = None
 
 
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.head, self.tail = Node("-1"), Node("-1")
-        self.head.nxt, self.tail.prev = self.tail, self.head
-
-        self.page = Node(homepage)
-        self.page.prev, self.page.nxt = self.head, self.tail
-        self.head.nxt = self.tail.prev = self.page
+        self.head = Node(homepage)
 
     def visit(self, url: str) -> None:
-        back_history = self.page
-        self.page = Node(url)
-
-        self.page.prev, self.page.nxt = self.head, back_history
-        self.head.nxt = back_history.prev = self.page
+        node = Node(url)
+        node.prev = self.head
+        self.head.nxt = node
+        self.head = node
 
     def back(self, steps: int) -> str:
-        curr = self.page
-        while curr and steps > 0 and curr.nxt != self.tail:
-            curr, steps = curr.nxt, steps - 1
+        while steps > 0 and self.head.prev:
+            self.head, steps = self.head.prev, steps - 1
 
-        self.page = curr
-
-        return self.page.val
+        return self.head.val
 
     def forward(self, steps: int) -> str:
-        curr = self.page  # can use it actually
-        while curr and steps > 0 and curr.prev != self.head:
-            curr, steps = curr.prev, steps - 1
+        while steps and self.head.nxt:
+            self.head, steps = self.head.nxt, steps - 1
 
-        self.page = curr
-
-        return self.page.val
+        return self.head.val
