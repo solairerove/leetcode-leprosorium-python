2f3d1cdfd2595c3086567a305f6e25471f3acbe1
solairerove <solairerove@gmail.com>
2023-01-26 16:56:20 +0800

    feature: add two numbers II stack revisit

--- linked_list/AddTwoNumbersII.py
+++ linked_list/AddTwoNumbersII.py
@@ -5,6 +5,42 @@ from linked_list.ListNode import ListNode
 from linked_list.ReverseLinkedList import reverse_list
 
 
+# O(n + m) time || O(n + m) space
+def add_two_numbers_stack_ii(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    if not l1 and not l2:
+        return None
+
+    if not l1 or not l2:
+        return l1 if l1 else l2
+
+    s1, s2 = [], []
+
+    while l1:
+        s1.append(l1.val)
+        l1 = l1.next
+
+    while l2:
+        s2.append(l2.val)
+        l2 = l2.next
+
+    head = None
+    carry = 0
+    while s1 or s2:
+        x = s1.pop() if s1 else 0
+        y = s2.pop() if s2 else 0
+        sm = x + y + carry
+        carry = sm // 10
+
+        curr = ListNode(sm % 10)
+        curr.next, head = head, curr
+
+    if carry == 1:
+        curr = ListNode(1)
+        curr.next, head = head, curr
+
+    return head
+
+
 # O(max(3n, 3m)) time || O(max(n, m) + 1) space
 def add_two_numbers_naive_ii(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     reversed_l1, reversed_l2 = reverse_list(self, l1), reverse_list(self, l2)
--- linked_list/test/test_add_two_numbers_ll.py
+++ linked_list/test/test_add_two_numbers_ll.py
@@ -1,11 +1,11 @@
 import unittest
 
-from linked_list.AddTwoNumbersII import add_two_numbers_naive_ii
+from linked_list.AddTwoNumbersII import add_two_numbers_naive_ii, add_two_numbers_stack_ii
 from linked_list.test.utils import transform_array_to_list_node, transform_list_node_to_array
 
 
 class MyTestCase(unittest.TestCase):
-    def test_add_two_numbers(self):
+    def test_add_two_numbers_naive(self):
         list1 = transform_array_to_list_node(self, [7, 2, 4, 3])
         list2 = transform_array_to_list_node(self, [5, 6, 4])
         res = add_two_numbers_naive_ii(self, list1, list2)
@@ -13,7 +13,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual([7, 8, 0, 7], actual)
 
-    def test_add_two_numbers_1(self):
+    def test_add_two_numbers_naive_1(self):
         list1 = transform_array_to_list_node(self, [2, 4, 3])
         list2 = transform_array_to_list_node(self, [5, 6, 4])
         res = add_two_numbers_naive_ii(self, list1, list2)
@@ -21,10 +21,34 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual([8, 0, 7], actual)
 
-    def test_add_two_numbers_2(self):
+    def test_add_two_numbers_naive_2(self):
         list1 = transform_array_to_list_node(self, [0])
         list2 = transform_array_to_list_node(self, [0])
         res = add_two_numbers_naive_ii(self, list1, list2)
+        actual = transform_list_node_to_array(self, res)
+
+        self.assertEqual([0], actual)
+
+    def test_add_two_numbers_stack(self):
+        list1 = transform_array_to_list_node(self, [7, 2, 4, 3])
+        list2 = transform_array_to_list_node(self, [5, 6, 4])
+        res = add_two_numbers_stack_ii(self, list1, list2)
+        actual = transform_list_node_to_array(self, res)
+
+        self.assertEqual([7, 8, 0, 7], actual)
+
+    def test_add_two_numbers_stack_1(self):
+        list1 = transform_array_to_list_node(self, [2, 4, 3])
+        list2 = transform_array_to_list_node(self, [5, 6, 4])
+        res = add_two_numbers_stack_ii(self, list1, list2)
+        actual = transform_list_node_to_array(self, res)
+
+        self.assertEqual([8, 0, 7], actual)
+
+    def test_add_two_numbers_stack_2(self):
+        list1 = transform_array_to_list_node(self, [0])
+        list2 = transform_array_to_list_node(self, [0])
+        res = add_two_numbers_stack_ii(self, list1, list2)
         actual = transform_list_node_to_array(self, res)
 
         self.assertEqual([0], actual)
