75f48e6302942331fa3749720181f3bda255123a
solairerove <solairerove@gmail.com>
2023-01-30 13:44:28 +0800

    feature: remove duplicates from an unsorted linked list revisit

--- linked_list/RemoveDuplicatesFromAnUnsortedLinkedList.py
+++ linked_list/RemoveDuplicatesFromAnUnsortedLinkedList.py
@@ -4,18 +4,19 @@ from linked_list.ListNode import ListNode
 # O(n) time || O(n) space
 def delete_duplicates_unsorted(self, head: ListNode) -> ListNode:
     prev = sentinel = ListNode()
-    curr = sentinel.next = head
-    counter = dict()
+    curr = head
+    counter = {}
     while curr:
         counter[curr.val] = counter.get(curr.val, 0) + 1
         curr = curr.next
 
     curr = head
     while curr:
-        if counter[curr.val] > 1:
-            prev.next = curr.next
-        else:
-            prev = curr
+        if counter[curr.val] == 1:
+            prev.next = curr
+            prev = prev.next
         curr = curr.next
+
+    prev.next = None
 
     return sentinel.next
