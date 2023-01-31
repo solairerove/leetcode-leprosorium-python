4f2f2da22572e9651526f4c7eb48b70aa3619105
solairerove <solairerove@gmail.com>
2023-01-31 14:50:39 +0800

    feature: house robber II revisit

--- dynamic_programming/HouseRobberII.py
+++ dynamic_programming/HouseRobberII.py
@@ -3,12 +3,11 @@ from typing import List
 
 # O(n) time || O(1) space
 def rob(self, nums: List[int]) -> int:
-    return max(nums[0], helper(self, nums[1:]), helper(self, nums[:-1]))
+    def helper(arr: List[int]) -> int:
+        rob1 = rob2 = 0
+        for n in arr:
+            rob1, rob2 = rob2, max(rob1 + n, rob2)
 
+        return rob2
 
-def helper(self, nums: List[int]) -> int:
-    rob1 = rob2 = 0
-    for n in nums:
-        rob1, rob2 = rob2, max(rob1 + n, rob2)
-
-    return rob2
+    return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
