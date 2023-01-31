8bed512af99b0618e07b864ee440acc417c81392
solairerove <solairerove@gmail.com>
2023-01-31 16:56:31 +0800

    feature: longest palindromic substring revisit

--- dynamic_programming/LongestPalindromicSubstring.py
+++ dynamic_programming/LongestPalindromicSubstring.py
@@ -1,17 +1,17 @@
 # O(n^2) time || O(1) space
 def longest_palindrome(self, s: str) -> str:
-    res = ""
+    longest = ""
+
+    def get_palindrome_from(res: str, low: int, high: int) -> str:
+        while low >= 0 and high < len(s) and s[low] == s[high]:
+            if high - low + 1 > len(res):
+                res = s[low: high + 1]
+            low, high = low - 1, high + 1
+
+        return res
+
     for i in range(len(s)):
-        res = get_palindrome_from(self, s, res, i, i)
-        res = get_palindrome_from(self, s, res, i, i + 1)
+        longest = get_palindrome_from(longest, i, i)
+        longest = get_palindrome_from(longest, i, i + 1)
 
-    return res
-
-
-def get_palindrome_from(self, s: str, res: str, low: int, high: int) -> str:
-    while low >= 0 and high < len(s) and s[low] == s[high]:
-        if high - low + 1 > len(res):
-            res = s[low: high + 1]
-        low, high = low - 1, high + 1
-
-    return res
+    return longest
