a883ea643ef359eb28aa542c6ff947abf179d18f
solairerove <solairerove@gmail.com>
2023-01-31 14:14:27 +0800

    feature: house robber revisit

--- dynamic_programming/HouseRobber.py
+++ dynamic_programming/HouseRobber.py
@@ -5,20 +5,6 @@ from typing import List
 def rob(self, nums: List[int]) -> int:
     rob1 = rob2 = 0
     for n in nums:
-        tmp = max(n + rob1, rob2)
-        rob1, rob2 = rob2, tmp
+        rob1, rob2 = rob2, max(n + rob1, rob2)
 
     return rob2
-
-
-"""
-rob1 = rob2 = 0
-[1, 2, 3, 1] 4
-[1, 2, 3, 1] 4
-max(1 + 0, 0), rob1 = 0, rob2 = 1
-max(2 + 0, 1), rob1 = 1, rob2 = 2
-max(3 + 1, 2), rob1 = 2, rob2 = 4
-max(1 + 2, 4), rob1 = 4, rob2 = 4
-
-[2, 7, 9, 3, 1] 12
-"""
