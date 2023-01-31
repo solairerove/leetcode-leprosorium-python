cb015955f909c6a98f5110a0cf92e50153a95302
solairerove <solairerove@gmail.com>
2023-01-31 17:02:51 +0800

    feature: palindromic substrings revisit

--- dynamic_programming/PalindromicSubstrings.py
+++ dynamic_programming/PalindromicSubstrings.py
@@ -1,17 +1,16 @@
 # O(n^2) time || O(1) space
 def count_substrings(self, s: str) -> int:
-    res = 0
+    def count_palindromes(low: int, high: int) -> int:
+        res = 0
+        while low >= 0 and high < len(s) and s[low] == s[high]:
+            res += 1
+            low, high = low - 1, high + 1
+
+        return res
+
+    count = 0
     for i in range(len(s)):
-        res += count_palindromes(self, s, i, i)
-        res += count_palindromes(self, s, i, i + 1)
+        count += count_palindromes(i, i)
+        count += count_palindromes(i, i + 1)
 
-    return res
-
-
-def count_palindromes(self, s: str, low: int, high: int) -> int:
-    res = 0
-    while low >= 0 and high < len(s) and s[low] == s[high]:
-        res += 1
-        low, high = low - 1, high + 1
-
-    return res
+    return count
