45cb9042208f9b5562f57f5988911453140a362d
solairerove <solairerove@gmail.com>
2023-02-01 19:54:02 +0800

    feature: coin change recursive solution

--- dynamic_programming/CoinChange.py
+++ dynamic_programming/CoinChange.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import List
 
 
@@ -15,3 +16,24 @@ def coin_change(self, coins: List[int], amount: int) -> int:
                 dp[a] = min(dp[a], dp[a - c] + 1)
 
     return [dp[amount], -1][dp[amount] == amount + 1]
+
+
+# O(c * a) time || O(a) space
+def coin_change_rec(self, coins: List[int], amount: int) -> int:
+    @lru_cache(None)
+    def dfs(a):
+        if a < 0:
+            return -1
+
+        if a == 0:
+            return 0
+
+        min_cost = float(a + 1)
+        for c in coins:
+            res = dfs(a - c)
+            if res != -1:
+                min_cost = min(min_cost, res + 1)
+
+        return [min_cost, -1][min_cost == float(a + 1)]
+
+    return dfs(amount)
--- dynamic_programming/test/test_coin_change.py
+++ dynamic_programming/test/test_coin_change.py
@@ -1,17 +1,20 @@
 import unittest
 
-from dynamic_programming.CoinChange import coin_change
+from dynamic_programming.CoinChange import coin_change, coin_change_rec
 
 
 class MyTestCase(unittest.TestCase):
     def test_coin_change(self):
         self.assertEqual(3, coin_change(self, [1, 2, 5], 11))  
+        self.assertEqual(3, coin_change_rec(self, [1, 2, 5], 11))
 
     def test_coin_change_1(self):
         self.assertEqual(-1, coin_change(self, [2], 3))  
+        self.assertEqual(-1, coin_change_rec(self, [2], 3))
 
     def test_coin_change_2(self):
         self.assertEqual(0, coin_change(self, [1], 0))  
+        self.assertEqual(0, coin_change_rec(self, [1], 0))
 
 
 if __name__ == '__main__':
