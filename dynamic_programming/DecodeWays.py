583e48ffd8bd0d397edb6c09a7701308bc12d842
solairerove <solairerove@gmail.com>
2023-02-01 13:13:17 +0800

    feature: decode ways test recursive solution

--- dynamic_programming/DecodeWays.py
+++ dynamic_programming/DecodeWays.py
@@ -1,4 +1,7 @@
 # O(n) time || O(1) space
+from functools import lru_cache
+
+
 def num_decodings(self, s: str) -> int:
     if s[0] == "0":
         return 0
@@ -15,3 +18,25 @@ def num_decodings(self, s: str) -> int:
         two_back, one_back = one_back, curr
 
     return one_back
+
+
+# O(n) time || O(n) space
+def num_decodings_recursive(self, s: str) -> int:
+    @lru_cache(maxsize=None)
+    def helper_with_memo(index: int, s: str) -> int:
+        if index == len(s):
+            return 1
+
+        if s[index] == "0":
+            return 0
+
+        if index == len(s) - 1:
+            return 1
+
+        ans = helper_with_memo(index + 1, s)
+        if int(s[index: index + 2]) <= 26:
+            ans += helper_with_memo(index + 2, s)
+
+        return ans
+
+    return helper_with_memo(0, s)
--- dynamic_programming/test/test_num_decodings.py
+++ dynamic_programming/test/test_num_decodings.py
@@ -1,20 +1,24 @@
 import unittest
 
-from dynamic_programming.DecodeWays import num_decodings
+from dynamic_programming.DecodeWays import num_decodings, num_decodings_recursive
 
 
 class MyTestCase(unittest.TestCase):
     def test_num_decodings(self):
-        self.assertEqual(2, num_decodings(self, "12"))  
+        self.assertEqual(2, num_decodings(self, "12"))
+        self.assertEqual(2, num_decodings_recursive(self, "12"))
 
     def test_num_decodings_1(self):
-        self.assertEqual(3, num_decodings(self, "226"))  
+        self.assertEqual(3, num_decodings(self, "226"))
+        self.assertEqual(3, num_decodings_recursive(self, "226"))
 
     def test_num_decodings_2(self):
         self.assertEqual(0, num_decodings(self, "06"))
+        self.assertEqual(0, num_decodings_recursive(self, "06"))
 
     def test_num_decodings_3(self):
         self.assertEqual(5, num_decodings(self, "2125"))
+        self.assertEqual(5, num_decodings_recursive(self, "2125"))
 
 
 if __name__ == '__main__':
