4cd677560675f1e3d0963c6b7f663ed2fe7f97ad
solairerove <solairerove@gmail.com>
2023-02-02 17:18:20 +0800

    feature: climbing stairs using recursion with memo

--- dynamic_programming/ClimbingStairs.py
+++ dynamic_programming/ClimbingStairs.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 # O(n) time || O(1) space
 def climb_stairs(self, n: int) -> int:
     a, b = 1, 1
@@ -6,3 +9,18 @@ def climb_stairs(self, n: int) -> int:
         a, b = a + b, a
 
     return a
+
+
+# O(n) time || O(1) space
+def climb_stairs_rec(self, n: int) -> int:
+    @lru_cache(None)
+    def helper(s) -> int:
+        if s > n:
+            return 0
+
+        if s == n:
+            return 1
+
+        return helper(s + 1) + helper(s + 2)
+
+    return helper(0)
--- dynamic_programming/test/test_climb_stairs.py
+++ dynamic_programming/test/test_climb_stairs.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.ClimbingStairs import climb_stairs
+from dynamic_programming.ClimbingStairs import climb_stairs, climb_stairs_rec
 
 
 class MyTestCase(unittest.TestCase):
     def test_climb_stairs(self):
         self.assertEqual(2, climb_stairs(self, 2))  
+        self.assertEqual(2, climb_stairs_rec(self, 2))
 
     def test_climb_stairs_1(self):
         self.assertEqual(3, climb_stairs(self, 3))  
+        self.assertEqual(3, climb_stairs_rec(self, 3))
 
 
 if __name__ == '__main__':
