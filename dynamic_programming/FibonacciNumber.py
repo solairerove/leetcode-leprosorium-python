332bf8f1459cf0d750d536d6ce825af987e6032c
solairerove <solairerove@gmail.com>
2023-02-02 15:56:35 +0800

    feature: fibonacci number using recursion with memo

--- dynamic_programming/FibonacciNumber.py
+++ dynamic_programming/FibonacciNumber.py
@@ -1,7 +1,19 @@
 # O(n) time || O(1) space
+from functools import lru_cache
+
+
 def fib(self, n: int) -> int:
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
 
     return a
+
+
+# O(n) time || O(n) space
+@lru_cache(None)
+def fib_rec(self, n: int) -> int:
+    if n <= 1:
+        return n
+
+    return fib_rec(self, n - 1) + fib_rec(self, n - 2)
--- dynamic_programming/test/test_fib.py
+++ dynamic_programming/test/test_fib.py
@@ -1,17 +1,20 @@
 import unittest
 
-from dynamic_programming.FibonacciNumber import fib
+from dynamic_programming.FibonacciNumber import fib, fib_rec
 
 
 class MyTestCase(unittest.TestCase):
     def test_fib(self):
         self.assertEqual(1, fib(self, 2))  
+        self.assertEqual(1, fib_rec(self, 2))
 
     def test_fib_1(self):
         self.assertEqual(2, fib(self, 3))  
+        self.assertEqual(2, fib_rec(self, 3))
 
     def test_fib_2(self):
         self.assertEqual(3, fib(self, 4))  
+        self.assertEqual(3, fib_rec(self, 4))
 
 
 if __name__ == '__main__':
