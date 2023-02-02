33021d9c807303a90fc6febecc1e02b37926db03
solairerove <solairerove@gmail.com>
2023-02-02 17:29:00 +0800

    feature: min cost climbing stairs using recursion with memo

--- dynamic_programming/MinCostClimbingStairs.py
+++ dynamic_programming/MinCostClimbingStairs.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import List
 
 
@@ -7,3 +8,15 @@ def min_cost_climbing_stairs(self, cost: List[int]) -> int:
         cost[i] += min(cost[i + 1], cost[i + 2])
 
     return min(cost[0], cost[1])
+
+
+# O(n) time || O(n) space
+def min_cost_climbing_stairs_rec(self, cost: List[int]) -> int:
+    @lru_cache(None)
+    def helper(s) -> int:
+        if s <= 1:
+            return 0
+
+        return min(cost[s - 1] + helper(s - 1), cost[s - 2] + helper(s - 2))
+
+    return helper(len(cost))
