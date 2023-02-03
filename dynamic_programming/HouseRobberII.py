ff3fb9098c36ba56a130126c46d650cefb7e0cb2
solairerove <solairerove@gmail.com>
2023-02-03 19:03:38 +0800

    feature: house robber ii recursive solution revisit

--- dynamic_programming/HouseRobberII.py
+++ dynamic_programming/HouseRobberII.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import List
 
 
@@ -11,3 +12,18 @@ def rob(self, nums: List[int]) -> int:
         return rob2
 
     return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+
+
+# O(n) time || O(n) space
+def rob_rec(self, nums: List[int]) -> int:
+    def classic_rob_rec(arr: List[int]) -> int:
+        @lru_cache(None)
+        def helper(ith: int) -> int:
+            if ith >= len(arr):
+                return 0
+
+            return max(helper(ith + 1), helper(ith + 2) + arr[ith])
+
+        return helper(0)
+
+    return max(nums[0], classic_rob_rec(nums[1:]), classic_rob_rec(nums[:-1]))
