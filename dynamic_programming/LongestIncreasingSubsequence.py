9c1e3cc35633bb55192cdcb403c47540fb69f1fe
solairerove <solairerove@gmail.com>
2023-02-06 12:49:45 +0800

    feature: longest increasing subsequence revisit

--- dynamic_programming/LongestIncreasingSubsequence.py
+++ dynamic_programming/LongestIncreasingSubsequence.py
@@ -5,10 +5,10 @@ from typing import List
 # O(n^2) time || O(n) space
 def length_of_lis(self, nums: List[int]) -> int:
     dp = [1] * len(nums)
-    for i in range(len(nums) - 1, -1, -1):
-        for j in range(i + 1, len(nums)):
-            if nums[i] < nums[j]:
-                dp[i] = max(dp[j] + 1, dp[i])
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
 
     return max(dp)
 
--- dynamic_programming/test/test_length_of_lis.py
+++ dynamic_programming/test/test_length_of_lis.py
@@ -6,20 +6,14 @@ from dynamic_programming.LongestIncreasingSubsequence import length_of_lis, leng
 class MyTestCase(unittest.TestCase):
     def test_length_of_lis(self):
         self.assertEqual(4, length_of_lis(self, [10, 9, 2, 5, 3, 7, 101, 18]))
+        self.assertEqual(4, length_of_lis_bs(self, [10, 9, 2, 5, 3, 7, 101, 18]))
 
     def test_length_of_lis_1(self):
         self.assertEqual(4, length_of_lis(self, [0, 1, 0, 3, 2, 3]))
+        self.assertEqual(4, length_of_lis_bs(self, [0, 1, 0, 3, 2, 3]))
 
     def test_length_of_lis_2(self):
         self.assertEqual(1, length_of_lis(self, [7, 7, 7, 7, 7, 7, 7]))
-
-    def test_length_of_lis_bs(self):
-        self.assertEqual(4, length_of_lis_bs(self, [10, 9, 2, 5, 3, 7, 101, 18]))
-
-    def test_length_of_lis_bs_1(self):
-        self.assertEqual(4, length_of_lis_bs(self, [0, 1, 0, 3, 2, 3]))
-
-    def test_length_of_lis_bs_2(self):
         self.assertEqual(1, length_of_lis_bs(self, [7, 7, 7, 7, 7, 7, 7]))
 
 
