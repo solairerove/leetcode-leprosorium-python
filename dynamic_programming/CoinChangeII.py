f5331b1223a25761fe501807dd5b1aea2a568b73
solairerove <solairerove@gmail.com>
2023-02-06 17:27:02 +0800

    feature: coin change ii effective solution

--- dynamic_programming/CoinChangeII.py
+++ dynamic_programming/CoinChangeII.py
@@ -1,9 +1,23 @@
 from typing import List
 
 
-# O(a * c) time || O(a * c) space
+# O(a * c) time || O(a) space
 def change(self, amount: int, coins: List[int]) -> int:
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for c in coins:
+        for a in range(c, amount + 1):
+            dp[a] += dp[a - c]
+
+    return dp[amount]
+
+
+# O(a * c) time || O(a * c) space
+def change_classic_knapsack(self, amount: int, coins: List[int]) -> int:
     dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
+    dp[0][0] = 1
+
     for c in range(1, len(coins) + 1):
         dp[c][0] = 1
         for a in range(1, amount + 1):
--- dynamic_programming/test/test_coin_change_ii.py
+++ dynamic_programming/test/test_coin_change_ii.py
@@ -1,20 +1,24 @@
 import unittest
 
-from dynamic_programming.CoinChangeII import change
+from dynamic_programming.CoinChangeII import change, change_classic_knapsack
 
 
 class MyTestCase(unittest.TestCase):
     def test_coin_change(self):
         self.assertEqual(4, change(self, 5, [1, 2, 5]))
+        self.assertEqual(4, change_classic_knapsack(self, 5, [1, 2, 5]))
 
     def test_coin_change_1(self):
         self.assertEqual(0, change(self, 3, [2]))
+        self.assertEqual(0, change_classic_knapsack(self, 3, [2]))
 
     def test_coin_change_2(self):
         self.assertEqual(1, change(self, 10, [10]))
+        self.assertEqual(1, change_classic_knapsack(self, 10, [10]))
 
     def test_coin_change_3(self):
         self.assertEqual(1, change(self, 100, [1, 101, 102, 103]))
+        self.assertEqual(1, change_classic_knapsack(self, 100, [1, 101, 102, 103]))
 
 
 if __name__ == '__main__':
