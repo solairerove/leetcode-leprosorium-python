a5cd8e0e98919ec165b4dc904939f2f41f948ece
solairerove <solairerove@gmail.com>
2023-02-06 13:03:38 +0800

    feature: partition equal subset sum recursive solution

--- dynamic_programming/PartitionEqualSubsetSum.py
+++ dynamic_programming/PartitionEqualSubsetSum.py
@@ -1,4 +1,5 @@
-from typing import List
+from functools import lru_cache
+from typing import List, Tuple
 
 
 # O(n * m) time || O(m) space, n is len(nums), s is subset_sum
@@ -15,3 +16,27 @@ def can_partition(self, nums: List[int]) -> bool:
             dp[i] = dp[i] or dp[i - n]
 
     return dp[subset_sum]
+
+
+# O(n * m) time || O(n * m) space, n is len(nums), s is subset_sum
+def can_partition_rec(self, nums: List[int]) -> bool:
+    @lru_cache(None)
+    def dfs(arr: Tuple[int], n: int, subset_sum: int) -> bool:
+        if subset_sum == 0:
+            return True
+
+        if n == 0 or subset_sum < 0:
+            return False
+
+        return (
+                dfs(arr, n - 1, subset_sum - nums[n - 1])
+                or
+                dfs(arr, n - 1, subset_sum)
+        )
+
+    total_sum = sum(nums)
+
+    if total_sum % 2 != 0:
+        return False
+
+    return dfs(tuple(nums), len(nums) - 1, total_sum // 2)
--- dynamic_programming/test/test_can_partition.py
+++ dynamic_programming/test/test_can_partition.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.PartitionEqualSubsetSum import can_partition
+from dynamic_programming.PartitionEqualSubsetSum import can_partition, can_partition_rec
 
 
 class MyTestCase(unittest.TestCase):
     def test_can_partition(self):
         self.assertEqual(True, can_partition(self, [1, 5, 11, 5]))
+        self.assertEqual(True, can_partition_rec(self, [1, 5, 11, 5]))
 
     def test_can_partition_1(self):
         self.assertEqual(False, can_partition(self, [1, 2, 3, 5]))
+        self.assertEqual(False, can_partition_rec(self, [1, 2, 3, 5]))
 
 
 if __name__ == '__main__':
