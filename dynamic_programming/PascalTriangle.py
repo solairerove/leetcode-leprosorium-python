67303b6055e896ef92b2fe87722c271acb0de8e9
solairerove <solairerove@gmail.com>
2023-02-07 12:40:12 +0800

    feature: pascal triangle dp naive approach n^2 time | 1 space

--- dynamic_programming/PascalTriangle.py
+++ dynamic_programming/PascalTriangle.py
@@ -2,6 +2,20 @@ from operator import add
 from typing import List
 
 
+# O(n ^ 2) time || O(1) space
+def generate(self, num_rows: int) -> List[List[int]]:
+    triangle = []
+    for row_idx in range(num_rows):
+        row = [1 for _ in range(row_idx + 1)]
+
+        for i in range(1, len(row) - 1):
+            row[i] = triangle[row_idx - 1][i - 1] + triangle[row_idx - 1][i]
+
+        triangle.append(row)
+
+    return triangle
+
+
 def generate_from_smart_guys(self, num_rows: int) -> List[List[int]]:
     res = [[1]]
     for _ in range(1, num_rows):
--- dynamic_programming/test/test_generate.py
+++ dynamic_programming/test/test_generate.py
@@ -1,11 +1,16 @@
 import unittest
 
-from dynamic_programming.PascalTriangle import generate_from_smart_guys
+from dynamic_programming.PascalTriangle import generate_from_smart_guys, generate
 
 
 class MyTestCase(unittest.TestCase):
     def test_generate(self):
         self.assertEqual([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]], generate_from_smart_guys(self, 5))
+        self.assertEqual([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]], generate(self, 5))
+
+    def test_generate_1(self):
+        self.assertEqual([[1]], generate_from_smart_guys(self, 1))
+        self.assertEqual([[1]], generate(self, 1))
 
 
 if __name__ == '__main__':
