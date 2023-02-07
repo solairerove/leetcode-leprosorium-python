aca18f340243996dfdc4ac9064c3fb400fdb6e89
solairerove <solairerove@gmail.com>
2023-02-07 17:25:36 +0800

    feature: classic 0-1 knapsack problem recursion revisit, add memo

--- dynamic_programming/KnapsackProblem.py
+++ dynamic_programming/KnapsackProblem.py
@@ -1,8 +1,10 @@
+from functools import lru_cache
 from typing import List
 
 
 # O(2^n) time || O(n) space
 def knapsack_recursion(self, items: List[int], weights: List[int], n: int, capacity: int) -> int:
+    @lru_cache(None)
     def helper(ith: int, free_space: int) -> int:
         if ith >= n - 1 or free_space < 0:
             return 0
