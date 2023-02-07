150a40bb8a873d3de298164fd99443a4784137d2
solairerove <solairerove@gmail.com>
2023-02-07 17:19:30 +0800

    feature: classic 0-1 knapsack problem recursion revisit

--- dynamic_programming/KnapsackProblem.py
+++ dynamic_programming/KnapsackProblem.py
@@ -3,6 +3,20 @@ from typing import List
 
 # O(2^n) time || O(n) space
 def knapsack_recursion(self, items: List[int], weights: List[int], n: int, capacity: int) -> int:
+    # def helper(ith: int, free_space: int) -> int:
+    #     if ith >= n - 1 or free_space < 0:
+    #         return 0
+    #
+    #     if weights[ith + 1] > free_space:
+    #         return 0
+    #
+    #     return max(
+    #         helper(ith + 1, free_space),
+    #         helper(ith, free_space - weights[ith + 1]) + items[ith + 1]
+    #     )
+    #
+    # return helper(-1, capacity)
+    #
     if n == 0 or capacity == 0 or weights[n - 1] > capacity:
         return 0
 
--- dynamic_programming/test/test_knapsack.py
+++ dynamic_programming/test/test_knapsack.py
@@ -16,6 +16,12 @@ class MyTestCase(unittest.TestCase):
     def test_knapsack_3(self):
         self.assertEqual(5, knapsack_recursion(self, [2, 3, 1, 4], [3, 4, 6, 5], 4, 8))
 
+    def test_knapsack_4(self):
+        self.assertEqual(3500, knapsack_recursion(self, [3000, 2000, 1500], [4, 3, 1], 3, 4))
+
+    def test_knapsack_5(self):
+        self.assertEqual(4000, knapsack_recursion(self, [3000, 2000, 1500, 2000], [4, 3, 1, 1], 4, 4))
+
 
 if __name__ == '__main__':
     unittest.main()
