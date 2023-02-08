aa9369fdcd7a59f4288d5bfc21f2cd1cdf949363
solairerove <solairerove@gmail.com>
2023-02-08 18:40:52 +0800

    feature: house robber bottom up solution

--- dynamic_programming/HouseRobber.py
+++ dynamic_programming/HouseRobber.py
@@ -24,3 +24,18 @@ def rob_rec(self, nums: List[int]) -> int:
         return max(dp(i - 1), dp(i - 2) + nums[i])
 
     return dp(len(nums) - 1)
+
+
+# O(n) time || O(n) space
+def rob_bottom_up(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+    return dp[-1]
