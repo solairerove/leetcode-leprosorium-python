2d5bd49b1773b0092d0ec7fbfe586ab629d794ed
solairerove <solairerove@gmail.com>
2023-02-08 18:13:21 +0800

    feature: climbing stairs iterative solution

--- dynamic_programming/ClimbingStairs.py
+++ dynamic_programming/ClimbingStairs.py
@@ -21,3 +21,16 @@ def climb_stairs_rec(self, n: int) -> int:
         return dp(i - 1) + dp(i - 2)
 
     return dp(n)
+
+
+# O(n) time || O(n) space
+def climb_stairs_bottom_up(self, n: int) -> int:
+    if n <= 2:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
