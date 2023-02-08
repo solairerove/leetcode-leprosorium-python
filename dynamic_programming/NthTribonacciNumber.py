d3976f7f3d578554d418caee90ad01a7282b4a74
solairerove <solairerove@gmail.com>
2023-02-08 19:29:45 +0800

    feature: n-th tribonacci number from smart guy

--- dynamic_programming/NthTribonacciNumber.py
+++ dynamic_programming/NthTribonacciNumber.py
@@ -5,3 +5,12 @@ def tribonacci(self, n: int) -> int:
         a, b, c = b, c, a + b + c
 
     return c
+
+
+# O(n) time || O(1) space
+def tribonacci_from_smart_guy(self, n: int) -> int:
+    dp = [0, 1, 1]
+    for i in range(3, n + 1):
+        dp[i % 3] = sum(dp)
+
+    return dp[n % 3]
--- dynamic_programming/test/test_tribonacci.py
+++ dynamic_programming/test/test_tribonacci.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.NthTribonacciNumber import tribonacci
+from dynamic_programming.NthTribonacciNumber import tribonacci, tribonacci_from_smart_guy
 
 
 class MyTestCase(unittest.TestCase):
     def test_tribonacci(self):
         self.assertEqual(4, tribonacci(self, 4))
+        self.assertEqual(4, tribonacci_from_smart_guy(self, 4))
 
     def test_tribonacci_1(self):
         self.assertEqual(1389537, tribonacci(self, 25))
+        self.assertEqual(1389537, tribonacci_from_smart_guy(self, 25))
 
 
 if __name__ == '__main__':
