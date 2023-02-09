6b045a6fe888d5ff433a76d06b19c8702e888d3b
solairerove <solairerove@gmail.com>
2023-02-09 20:00:58 +0800

    feature: maximum score from performing multiplication operations bottom up solution

--- dynamic_programming/MaximumScoreFromPerformingMultiplicationOperations.py
+++ dynamic_programming/MaximumScoreFromPerformingMultiplicationOperations.py
@@ -2,8 +2,8 @@ from functools import lru_cache
 from typing import List
 
 
-# O(n) time || O(n) space
-def maximum_score(self, nums: List[int], multipliers: List[int]) -> int:
+# O(m^2) time || O(m^2) space
+def maximum_score_top_down(self, nums: List[int], multipliers: List[int]) -> int:
     n, m = len(nums), len(multipliers)
 
     @lru_cache(None)
@@ -20,3 +20,20 @@ def maximum_score(self, nums: List[int], multipliers: List[int]) -> int:
         )
 
     return dp(0, 0)
+
+
+# O(m^2) time || O(m^2) space
+def maximum_score_bottom_up(self, nums: List[int], multipliers: List[int]) -> int:
+    n, m = len(nums), len(multipliers)
+    dp = [[0] * (m + 1) for _ in range(m + 1)]
+
+    for i in range(m - 1, -1, -1):
+        for left in range(i, -1, -1):
+            mult = multipliers[i]
+            right = n - 1 - (i - left)
+            dp[i][left] = max(
+                mult * nums[left] + dp[i + 1][left + 1],
+                mult * nums[right] + dp[i + 1][left]
+            )
+
+    return dp[0][0]
--- dynamic_programming/test/test_maximum_score.py
+++ dynamic_programming/test/test_maximum_score.py
@@ -1,14 +1,17 @@
 import unittest
 
-from dynamic_programming.MaximumScoreFromPerformingMultiplicationOperations import maximum_score
+from dynamic_programming.MaximumScoreFromPerformingMultiplicationOperations import maximum_score_top_down, \
+    maximum_score_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_maximum_score(self):
-        self.assertEqual(14, maximum_score(self, [1, 2, 3], [3, 2, 1]))
+        self.assertEqual(14, maximum_score_top_down(self, [1, 2, 3], [3, 2, 1]))
+        self.assertEqual(14, maximum_score_bottom_up(self, [1, 2, 3], [3, 2, 1]))
 
     def test_maximum_score_1(self):
-        self.assertEqual(102, maximum_score(self, [-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6]))
+        self.assertEqual(102, maximum_score_top_down(self, [-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6]))
+        self.assertEqual(102, maximum_score_bottom_up(self, [-5, -3, -3, -2, 7, 1], [-10, -5, 3, 4, 6]))
 
 
 if __name__ == '__main__':
