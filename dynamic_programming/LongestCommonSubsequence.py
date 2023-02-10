b521e66c0049a1f25d31c88e502961d9475cf8c7
solairerove <solairerove@gmail.com>
2023-02-10 12:35:17 +0800

    feature: longest common subsequence classic top down solution

--- dynamic_programming/LongestCommonSubsequence.py
+++ dynamic_programming/LongestCommonSubsequence.py
@@ -1,12 +1,31 @@
+from functools import lru_cache
+
+
 # O(n * m) time || O(n * m) space
 def longest_common_subsequence(self, s1: str, s2: str) -> int:
     n, m = len(s1), len(s2)
     dp = [[0] * (m + 1) for _ in range(n + 1)]
+    # TODO: in range(1, n - 1)
     for i in range(n):
         for j in range(m):
             if s1[i] == s2[j]:
                 dp[i + 1][j + 1] = dp[i][j] + 1
             else:
-                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
 
     return dp[-1][-1]
+
+
+# O(n * m) time || O(n * m) space
+def longest_common_subsequence_req(self, s1: str, s2: str) -> int:
+    @lru_cache(None)
+    def dp(i: int, j: int) -> int:
+        if i < 0 or j < 0:
+            return 0
+
+        if s1[i] == s2[j]:
+            return dp(i - 1, j - 1) + 1
+
+        return max(dp(i - 1, j), dp(i, j - 1))
+
+    return dp(len(s1) - 1, len(s2) - 1)
--- dynamic_programming/test/test_longest_common_subsequence.py
+++ dynamic_programming/test/test_longest_common_subsequence.py
@@ -1,23 +1,28 @@
 import unittest
 
-from dynamic_programming.LongestCommonSubsequence import longest_common_subsequence
+from dynamic_programming.LongestCommonSubsequence import longest_common_subsequence, longest_common_subsequence_req
 
 
 class MyTestCase(unittest.TestCase):
     def test_longest_common_subsequence(self):
         self.assertEqual(3, longest_common_subsequence(self, "abcde", "ace"))
+        self.assertEqual(3, longest_common_subsequence_req(self, "abcde", "ace"))
 
     def test_longest_common_subsequence_1(self):
         self.assertEqual(3, longest_common_subsequence(self, "abc", "abc"))
+        self.assertEqual(3, longest_common_subsequence_req(self, "abc", "abc"))
 
     def test_longest_common_subsequence_2(self):
         self.assertEqual(0, longest_common_subsequence(self, "abc", "def"))
+        self.assertEqual(0, longest_common_subsequence_req(self, "abc", "def"))
 
     def test_longest_common_subsequence_3(self):
         self.assertEqual(3, longest_common_subsequence(self, "hish", "fish"))
+        self.assertEqual(3, longest_common_subsequence_req(self, "hish", "fish"))
 
     def test_longest_common_subsequence_4(self):
         self.assertEqual(2, longest_common_subsequence(self, "hish", "vista"))
+        self.assertEqual(2, longest_common_subsequence_req(self, "hish", "vista"))
 
 
 if __name__ == '__main__':
