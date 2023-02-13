306c76c88512a3ed3262cab8b675dd2386cbfe9c
solairerove <solairerove@gmail.com>
2023-02-13 19:20:03 +0800

    feature: minimum difficulty of a job schedule bottom up

--- dynamic_programming/MinimumDifficultyOfAJobSchedule.py
+++ dynamic_programming/MinimumDifficultyOfAJobSchedule.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 # O(d * ((n - d) ^ 2)) time || O(d * (n - d)) space
-def min_difficulty(self, job_difficulty: List[int], d: int) -> int:
+def min_difficulty_top_down(self, job_difficulty: List[int], d: int) -> int:
     n = len(job_difficulty)
 
     if n < d:
@@ -29,3 +29,26 @@ def min_difficulty(self, job_difficulty: List[int], d: int) -> int:
         return best
 
     return dp(0, 1)
+
+
+# O(d * ((n - d) ^ 2)) time || O(d * n) space
+def min_difficulty_bottom_up(self, job_difficulty: List[int], d: int) -> int:
+    n = len(job_difficulty)
+
+    if n < d:
+        return -1
+
+    dp = [[float("inf")] * (d + 1) for _ in range(n)]
+    dp[-1][d] = job_difficulty[-1]
+
+    for i in range(n - 2, -1, -1):
+        dp[i][d] = max(dp[i + 1][d], job_difficulty[i])
+
+    for day in range(d - 1, 0, -1):
+        for i in range(day - 1, n - (d - day)):
+            hardest = 0
+            for j in range(i, n - (d - day)):
+                hardest = max(hardest, job_difficulty[j])
+                dp[i][day] = min(dp[i][day], hardest + dp[j + 1][day + 1])
+
+    return int(dp[0][1])
--- dynamic_programming/test/test_min_difficulty.py
+++ dynamic_programming/test/test_min_difficulty.py
@@ -1,17 +1,21 @@
 import unittest
 
-from dynamic_programming.MinimumDifficultyOfAJobSchedule import min_difficulty
+from dynamic_programming.MinimumDifficultyOfAJobSchedule import min_difficulty_top_down, \
+    min_difficulty_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_min_difficulty(self):
-        self.assertEqual(7, min_difficulty(self, [6, 5, 4, 3, 2, 1], 2))
+        self.assertEqual(7, min_difficulty_top_down(self, [6, 5, 4, 3, 2, 1], 2))
+        self.assertEqual(7, min_difficulty_bottom_up(self, [6, 5, 4, 3, 2, 1], 2))
 
     def test_min_difficulty_1(self):
-        self.assertEqual(-1, min_difficulty(self, [9, 9, 9], 4))
+        self.assertEqual(-1, min_difficulty_top_down(self, [9, 9, 9], 4))
+        self.assertEqual(-1, min_difficulty_bottom_up(self, [9, 9, 9], 4))
 
     def test_min_difficulty_2(self):
-        self.assertEqual(3, min_difficulty(self, [1, 1, 1], 3))
+        self.assertEqual(3, min_difficulty_top_down(self, [1, 1, 1], 3))
+        self.assertEqual(3, min_difficulty_bottom_up(self, [1, 1, 1], 3))
 
 
 if __name__ == '__main__':
