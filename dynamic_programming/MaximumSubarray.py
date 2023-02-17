60d9421a95bf401edd283419e7037463d40a905a
solairerove <solairerove@gmail.com>
2023-02-17 19:20:44 +0800

    feature: maximum subarray using kadane algorithm and const space

--- dynamic_programming/MaximumSubarray.py
+++ dynamic_programming/MaximumSubarray.py
@@ -9,3 +9,11 @@ def max_sub_array(self, nums: List[int]) -> int:
         dp[i] = max(nums[i], dp[i - 1] + nums[i])
 
     return max(dp)
+
+
+# O(n) time || O(1) space
+def max_sub_array_const(self, nums: List[int]) -> int:
+    for i in range(1, len(nums)):
+        nums[i] = max(nums[i], nums[i] + nums[i - 1])
+
+    return max(nums)
