2f2f3bfe170bc3e51c64269d9dc71a03d2daa20c
solairerove <solairerove@gmail.com>
2023-02-19 18:15:00 +0800

    doc: update readme

---
+++ trees/BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,8 @@
+from typing import Optional, List
+
+from trees.TreeNode import TreeNode
+
+
+# O(n) time || O(h) space
+def zigzag_level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
+    return []
---
+++ trees/test/test_zigzag_level_order.py
@@ -0,0 +1,12 @@
+import unittest
+
+from trees.BinaryTreeZigzagLevelOrderTraversal import zigzag_level_order
+
+
+class MyTestCase(unittest.TestCase):
+    def test_zigzag_level_order(self):
+        self.assertEqual(True, zigzag_level_order(self, ))
+
+
+if __name__ == '__main__':
+    unittest.main()
