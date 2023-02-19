2654dcbe3f9af25b80acf24f8cd080eae22d5450
solairerove <solairerove@gmail.com>
2023-02-19 18:23:40 +0800

    feature: binary tree zigzag level order traversal using stack

--- trees/BinaryTreeZigzagLevelOrderTraversal.py
+++ trees/BinaryTreeZigzagLevelOrderTraversal.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional, List
 
 from trees.TreeNode import TreeNode
@@ -5,4 +6,24 @@ from trees.TreeNode import TreeNode
 
 # O(n) time || O(h) space
 def zigzag_level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
-    return []
+    if not root:
+        return []
+
+    queue = deque([root])
+    res, direction = [], 1
+    while queue:
+        level = []
+        for i in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        res.append(level[::direction])
+        direction *= -1
+
+    return res
--- trees/test/test_zigzag_level_order.py
+++ trees/test/test_zigzag_level_order.py
@@ -1,11 +1,20 @@
 import unittest
 
+from dynamic_programming.HouseRobberIII import TreeNode
 from trees.BinaryTreeZigzagLevelOrderTraversal import zigzag_level_order
 
 
 class MyTestCase(unittest.TestCase):
     def test_zigzag_level_order(self):
-        self.assertEqual(True, zigzag_level_order(self, ))
+        root = TreeNode(3)
+        left = TreeNode(9)
+        right = TreeNode(20)
+        r_left = TreeNode(15)
+        r_right = TreeNode(7)
+        right.left, right.right = r_left, r_right
+        root.left, root.right = left, right
+
+        self.assertEqual([[3], [20, 9], [15, 7]], zigzag_level_order(self, root))
 
 
 if __name__ == '__main__':
