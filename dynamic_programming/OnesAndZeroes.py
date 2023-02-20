2ef0c796050df79ceaed4c3bdb5032e261c98e65
solairerove <solairerove@gmail.com>
2023-02-20 20:19:58 +0800

    feature: ones and zeros top down memo solution

--- dynamic_programming/OnesAndZeroes.py
+++ dynamic_programming/OnesAndZeroes.py
@@ -1,3 +1,5 @@
+import math
+from functools import lru_cache
 from typing import List
 
 
@@ -11,3 +13,20 @@ def find_max_form(self, strs: List[str], m: int, n: int) -> int:
                 dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
 
     return dp[-1][-1]
+
+
+# O(l * m * n) time || O(l * m * n) space
+def find_max_form_rec(self, strs: List[str], m: int, n: int) -> int:
+    counter = [[s.count("0"), s.count("1")] for s in strs]
+
+    @lru_cache(None)
+    def dp(i, j, idx):
+        if i < 0 or j < 0:
+            return -math.inf
+
+        if idx == len(strs):
+            return 0
+
+        return max(dp(i, j, idx + 1), 1 + dp(i - counter[idx][0], j - counter[idx][1], idx + 1))
+
+    return dp(m, n, 0)
--- dynamic_programming/test/test_find_max_form.py
+++ dynamic_programming/test/test_find_max_form.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.OnesAndZeroes import find_max_form
+from dynamic_programming.OnesAndZeroes import find_max_form, find_max_form_rec
 
 
 class MyTestCase(unittest.TestCase):
     def test_find_max_form(self):
         self.assertEqual(4, find_max_form(self, ["10", "0001", "111001", "1", "0"], 5, 3))
+        self.assertEqual(4, find_max_form_rec(self, ["10", "0001", "111001", "1", "0"], 5, 3))
 
     def test_find_max_form_1(self):
         self.assertEqual(2, find_max_form(self, ["10", "0", "1"], 1, 1))
+        self.assertEqual(2, find_max_form_rec(self, ["10", "0", "1"], 1, 1))
 
 
 if __name__ == '__main__':
