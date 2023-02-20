de23dd21906908d85b529e025beb6004966a67e9
solairerove <solairerove@gmail.com>
2023-02-20 20:14:24 +0800

    feature: ones and zeros bottom up solution

---
+++ dynamic_programming/OnesAndZeroes.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+# O(l * m * n) time || O(m * n) space
+def find_max_form(self, strs: List[str], m: int, n: int) -> int:
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    counter = [[s.count("0"), s.count("1")] for s in strs]
+    for zeros, ones in counter:
+        for i in range(m, zeros - 1, -1):
+            for j in range(n, ones - 1, -1):
+                dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
+
+    return dp[-1][-1]
---
+++ dynamic_programming/test/test_find_max_form.py
@@ -0,0 +1,15 @@
+import unittest
+
+from dynamic_programming.OnesAndZeroes import find_max_form
+
+
+class MyTestCase(unittest.TestCase):
+    def test_find_max_form(self):
+        self.assertEqual(4, find_max_form(self, ["10", "0001", "111001", "1", "0"], 5, 3))
+
+    def test_find_max_form_1(self):
+        self.assertEqual(2, find_max_form(self, ["10", "0", "1"], 1, 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
