3935f078e53e5c358bdcba2cd11ccc27e9504ba6
solairerove <solairerove@gmail.com>
2023-02-22 13:35:49 +0800

    feature: kth missing positive number linear solution

--- binary_search/KthMissingPositiveNumber.py
+++ binary_search/KthMissingPositiveNumber.py
@@ -12,3 +12,12 @@ def find_kth_positive(self, arr: List[int], k: int) -> int:
             high = mid - 1
 
     return low + k
+
+
+# O(n) time || O(1) space
+def find_kth_positive_smart(self, arr: List[int], k: int) -> int:
+    kth_missed, i = k, 0
+    while i < len(arr) and arr[i] <= kth_missed:
+        kth_missed, i = kth_missed + 1, i + 1
+
+    return kth_missed
--- binary_search/test/test_find_kth_positive.py
+++ binary_search/test/test_find_kth_positive.py
@@ -1,14 +1,16 @@
 import unittest
 
-from binary_search.KthMissingPositiveNumber import find_kth_positive
+from binary_search.KthMissingPositiveNumber import find_kth_positive, find_kth_positive_smart
 
 
 class MyTestCase(unittest.TestCase):
     def test_find_kth_positive(self):
         self.assertEqual(9, find_kth_positive(self, [2, 3, 4, 7, 11], 5))
+        self.assertEqual(9, find_kth_positive_smart(self, [2, 3, 4, 7, 11], 5))
 
     def test_find_kth_positive_1(self):
         self.assertEqual(6, find_kth_positive(self, [1, 2, 3, 4], 2))
+        self.assertEqual(6, find_kth_positive_smart(self, [1, 2, 3, 4], 2))
 
 
 if __name__ == '__main__':
