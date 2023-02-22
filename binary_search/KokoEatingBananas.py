ddc4b0dbacdfac0fcc43e4d1b61f184d741d217a
solairerove <solairerove@gmail.com>
2023-02-22 13:12:21 +0800

    feature: koko eating bananas revisit

--- binary_search/KokoEatingBananas.py
+++ binary_search/KokoEatingBananas.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -6,7 +7,7 @@ def min_eating_speed(self, piles: List[int], h: int) -> int:
     low, high = 1, max(piles)
     while low <= high:
         mid = low + (high - low) // 2
-        if sum((pile + mid - 1) // mid for pile in piles) > h:
+        if sum(math.ceil(pile / mid) for pile in piles) > h:  # (pile + mid - 1) // mid
             low = mid + 1
         else:
             high = mid - 1
