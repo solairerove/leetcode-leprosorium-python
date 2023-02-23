5283af71a57ee2c0559112109f5002a6527dd705
solairerove <solairerove@gmail.com>
2023-02-24 00:04:43 +0800

    feature: unique paths bottom up solution

--- dynamic_programming/UniquePaths.py
+++ dynamic_programming/UniquePaths.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 
 
 # O(m * n) time || O(m * n) space
-def unique_paths(self, m: int, n: int) -> int:
+def unique_paths_top_down(self, m: int, n: int) -> int:
     @lru_cache(None)
     def dp(i, j):
         if i < 1 or j < 1:
@@ -14,3 +14,15 @@ def unique_paths(self, m: int, n: int) -> int:
         return dp(i - 1, j) + dp(i, j - 1)
 
     return dp(m, n)
+
+
+# O(m * n) time || O(n) space
+def unique_paths_bottom_up(self, m: int, n: int) -> int:
+    row = [1] * n
+    for i in range(m - 1):
+        new_row = [1] * n
+        for j in range(n - 2, -1, -1):
+            new_row[j] = new_row[j + 1] + row[j]
+        row = new_row
+
+    return row[0]
--- dynamic_programming/test/test_unique_paths.py
+++ dynamic_programming/test/test_unique_paths.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.UniquePaths import unique_paths
+from dynamic_programming.UniquePaths import unique_paths_top_down, unique_paths_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_unique_paths(self):
-        self.assertEqual(28, unique_paths(self, 3, 7))
+        self.assertEqual(28, unique_paths_top_down(self, 3, 7))
+        self.assertEqual(28, unique_paths_bottom_up(self, 3, 7))
 
     def test_unique_paths_1(self):
-        self.assertEqual(3, unique_paths(self, 3, 2))
+        self.assertEqual(3, unique_paths_top_down(self, 3, 2))
+        self.assertEqual(3, unique_paths_bottom_up(self, 3, 2))
 
 
 if __name__ == '__main__':
