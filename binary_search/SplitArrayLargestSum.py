0aefc02c047a3032d88a8ddb9e4f6a2b577f169a
solairerove <solairerove@gmail.com>
2023-02-25 16:27:38 +0800

    feature: split array largest sum revisit, rename curr sum

--- binary_search/SplitArrayLargestSum.py
+++ binary_search/SplitArrayLargestSum.py
@@ -10,9 +10,9 @@ def split_array_top_down(self, nums: List[int], k: int) -> int:
 def split_array_bs(self, nums: List[int], k: int) -> int:
     low, high = max(nums), sum(nums)
     while low < high:
-        mid, curr_sum, cnt = low + (high - low) // 2, 0, 1
+        mid, total, cnt = low + (high - low) // 2, 0, 1
         for num in nums:
-            curr_sum, cnt = [(curr_sum + num, cnt), (num, cnt + 1)][curr_sum + num > mid]
+            total, cnt = [(total + num, cnt), (num, cnt + 1)][total + num > mid]
         low, high = [(low, mid), (mid + 1, high)][cnt > k]
 
     return low
