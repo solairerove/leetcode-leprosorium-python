320d991ce689bdb6ba8868ef048cc5ce7903ce5f
solairerove <solairerove@gmail.com>
2023-02-25 15:28:38 +0800

    feature: best time to buy and sell stock

--- dynamic_programming/BestTimeToBuyAndSellStock.py
+++ dynamic_programming/BestTimeToBuyAndSellStock.py
@@ -3,12 +3,11 @@ from typing import List
 
 # O(n) time || O(1) space
 def max_profit(self, prices: List[int]) -> int:
-    profit = 0
-    curr_min = prices[0]
+    profit, min_price = 0, prices[0]
     for i in range(1, len(prices)):
         price = prices[i]
 
-        profit = max(profit, price - curr_min)
-        curr_min = min(curr_min, price)
+        profit = max(profit, price - min_price)
+        min_price = min(min_price, price)
 
     return profit
