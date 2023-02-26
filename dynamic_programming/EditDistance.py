bbfe1e94be0a6e2f5642671b85407fa117caf8fe
solairerove <solairerove@gmail.com>
2023-02-26 14:20:45 +0800

    feature: edit distance top down dp solution

--- dynamic_programming/EditDistance.py
+++ dynamic_programming/EditDistance.py
@@ -17,3 +17,31 @@ def min_distance_naive_recursive(self, word1: str, word2: str) -> int:
     replace = 1 + min_distance_naive_recursive(self, word1[1:], word2[1:])
 
     return min(insert, delete, replace)
+
+
+# O(n * m) time || O(n * m) space
+def min_distance_top_down(self, word1: str, word2: str) -> int:
+    def dp(i, j, dic):
+        if i == len(word1) and j == len(word2):
+            return 0
+
+        if i == len(word1):
+            return len(word2) - j
+
+        if j == len(word2):
+            return len(word1) - i
+
+        if (i, j) not in dic:
+            if word1[i] == word2[j]:
+                ans = dp(i + 1, j + 1, dic)
+            else:
+                insert = 1 + dp(i, j + 1, dic)
+                delete = 1 + dp(i + 1, j, dic)
+                replace = 1 + dp(i + 1, j + 1, dic)
+
+                ans = min(insert, delete, replace)
+            dic[(i, j)] = ans
+
+        return dic[(i, j)]
+
+    return dp(0, 0, {})
--- dynamic_programming/test/test_min_distance.py
+++ dynamic_programming/test/test_min_distance.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.EditDistance import min_distance_naive_recursive
+from dynamic_programming.EditDistance import min_distance_naive_recursive, min_distance_top_down
 
 
 class MyTestCase(unittest.TestCase):
     def test_min_distance(self):
         self.assertEqual(3, min_distance_naive_recursive(self, "horse", "ros"))
+        self.assertEqual(3, min_distance_top_down(self, "horse", "ros"))
 
     def test_min_distance_1(self):
         self.assertEqual(5, min_distance_naive_recursive(self, "intention", "execution"))
+        self.assertEqual(5, min_distance_top_down(self, "intention", "execution"))
 
 
 if __name__ == '__main__':
