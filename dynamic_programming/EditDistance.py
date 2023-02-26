9d613a4db2caa170a93459b0c1379e1ed5755c06
solairerove <solairerove@gmail.com>
2023-02-26 14:42:02 +0800

    feature: edit distance bottom up dp solution

--- dynamic_programming/EditDistance.py
+++ dynamic_programming/EditDistance.py
@@ -45,3 +45,25 @@ def min_distance_top_down(self, word1: str, word2: str) -> int:
         return dic[(i, j)]
 
     return dp(0, 0, {})
+
+
+# O(n * m) time || O(n * m) space
+def min_distance_bottom_up(self, word1: str, word2: str) -> int:
+    m = len(word1)
+    n = len(word2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+
+    for i in range(n + 1):
+        dp[0][i] = i
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[-1][-1]
