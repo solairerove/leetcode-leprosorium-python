56e9c0baaeb0569b15fcb8ef5d3e756ef5b10f0a
solairerove <solairerove@gmail.com>
2023-03-01 17:22:19 +0800

    feature: palindrome linked list revisit

--- linked_list/PalindromeLinkedList.py
+++ linked_list/PalindromeLinkedList.py
@@ -5,18 +5,19 @@ from linked_list.ListNode import ListNode
 
 # O(n) time || O(1) space
 def is_palindrome(self, head: Optional[ListNode]) -> bool:
-    slow, fast = head, head.next
+    slow = fast = head
     while fast and fast.next:
         slow, fast = slow.next, fast.next.next
 
-    curr, prev = slow.next, None
+    prev, curr = None, slow
     while curr:
         curr.next, prev, curr = prev, curr, curr.next
 
-    list1, list2 = head, prev
-    while list1 and list2:
-        if list1.val != list2.val:
+    curr1, curr2 = head, prev
+    while curr1 and curr2:
+        if curr1.val != curr2.val:
             return False
-        list1, list2 = list1.next, list2.next
+
+        curr1, curr2 = curr1.next, curr2.next
 
     return True
