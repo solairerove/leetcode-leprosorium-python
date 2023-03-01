f3f59222f076fd24e29b45af628d85ff0bcea6a9
solairerove <solairerove@gmail.com>
2023-03-01 17:44:09 +0800

    feature: remove duplicated from sorted list revisit

--- linked_list/RemoveDuplicatesFromSortedList.py
+++ linked_list/RemoveDuplicatesFromSortedList.py
@@ -12,3 +12,18 @@ def delete_duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = curr.next
 
     return head
+
+
+# O(n) time || O(1) space
+def delete_duplicates_dumb(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    sentinel = ListNode()
+    prev, curr = sentinel, head
+    while curr:
+        while curr and curr.next and curr.val == curr.next.val:
+            curr = curr.next
+
+        prev.next = curr
+        prev = prev.next
+        curr = curr.next
+
+    return sentinel.next
--- linked_list/test/test_delete_duplicates.py
+++ linked_list/test/test_delete_duplicates.py
@@ -1,6 +1,6 @@
 import unittest
 
-from linked_list.RemoveDuplicatesFromSortedList import delete_duplicates
+from linked_list.RemoveDuplicatesFromSortedList import delete_duplicates, delete_duplicates_dumb
 from linked_list.test.utils import transform_array_to_list_node, transform_list_node_to_array
 
 
@@ -20,6 +20,24 @@ class MyTestCase(unittest.TestCase):
     def test_delete_duplicates_2(self):
         head = transform_array_to_list_node(self, [1, 1, 1])
         res = delete_duplicates(self, head)
+        actual = transform_list_node_to_array(self, res)
+        self.assertEqual([1], actual)
+
+    def test_delete_duplicates_dumb(self):
+        head = transform_array_to_list_node(self, [1, 1, 2])
+        res = delete_duplicates_dumb(self, head)
+        actual = transform_list_node_to_array(self, res)
+        self.assertEqual([1, 2], actual)
+
+    def test_delete_duplicates_dumb_1(self):
+        head = transform_array_to_list_node(self, [1, 1, 2, 3, 3])
+        res = delete_duplicates_dumb(self, head)
+        actual = transform_list_node_to_array(self, res)
+        self.assertEqual([1, 2, 3], actual)
+
+    def test_delete_duplicates_dumb_2(self):
+        head = transform_array_to_list_node(self, [1, 1, 1])
+        res = delete_duplicates_dumb(self, head)
         actual = transform_list_node_to_array(self, res)
         self.assertEqual([1], actual)
 
