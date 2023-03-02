c4bd4315eec1a9b058a06e023d39438c0ddd86ce
solairerove <solairerove@gmail.com>
2023-03-02 20:57:03 +0800

    feature: valid anagram

--- arrays/ValidAnagram.py
+++ arrays/ValidAnagram.py
@@ -1,3 +1,18 @@
-# O(n * log(n)) time || O(1) space
+# O(n) time || O(1) space
 def is_anagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    arr = [0] * 26
+    for char in s:
+        arr[ord(char) - ord('a')] += 1
+
+    for char in t:
+        arr[ord(char) - ord('a')] -= 1
+
+    return max(arr) == 0
+
+
+# O(n * log(n)) time || O(1) space
+def is_anagram_naive(self, s: str, t: str) -> bool:
     return sorted(s) == sorted(t)
--- arrays/test/test_is_anagram.py
+++ arrays/test/test_is_anagram.py
@@ -1,16 +1,19 @@
 import unittest
 
-from arrays.ValidAnagram import is_anagram
+from arrays.ValidAnagram import is_anagram_naive, is_anagram
 
 
 class MyTestCase(unittest.TestCase):
     def test_is_anagram(self):
+        self.assertEqual(True, is_anagram_naive(self, "anagram", "nagaram"))
         self.assertEqual(True, is_anagram(self, "anagram", "nagaram"))
 
     def test_is_anagram_1(self):
+        self.assertEqual(False, is_anagram_naive(self, "rat", "car"))
         self.assertEqual(False, is_anagram(self, "rat", "car"))
 
     def test_is_anagram_2(self):
+        self.assertEqual(False, is_anagram_naive(self, "aa", "a"))
         self.assertEqual(False, is_anagram(self, "aa", "a"))
 
 
