19f006a9c82bd1809d11e0a4fe412d886a4f74e2
solairerove <solairerove@gmail.com>
2023-03-03 19:52:18 +0800

    feature: group anagrams revisit

--- arrays/GroupAnagrams.py
+++ arrays/GroupAnagrams.py
@@ -9,3 +9,17 @@ def group_anagrams(self, strs: List[str]) -> List[List[str]]:
         dic[''.join(sorted(s))].append(s)
 
     return list(dic.values())
+
+
+# O(n * m) time || O(n * m) space,
+# m - number of strings
+# n - average number of letters
+def group_anagrams_count_approach(self, strs: List[str]) -> List[List[str]]:
+    dic = collections.defaultdict(list)
+    for s in strs:
+        cnt = [0] * 26
+        for char in s:
+            cnt[ord(char) - ord('a')] += 1
+        dic[tuple(cnt)].append(s)
+
+    return list(dic.values())
--- arrays/test/test_group_anagrams.py
+++ arrays/test/test_group_anagrams.py
@@ -1,12 +1,15 @@
 import unittest
 
-from arrays.GroupAnagrams import group_anagrams
+from arrays.GroupAnagrams import group_anagrams, group_anagrams_count_approach
 
 
 class MyTestCase(unittest.TestCase):
     def test_group_anagrams(self):
         self.assertEqual([['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']],
                          group_anagrams(self, ["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+        self.assertEqual([['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']],
+                         group_anagrams_count_approach(self, ["eat", "tea", "tan", "ate", "nat", "bat"]))
 
 
 if __name__ == '__main__':
