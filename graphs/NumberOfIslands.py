8e4b54689ab9844b781d95f623ffeba557f33b03
solairerove <solairerove@gmail.com>
2023-03-06 13:14:21 +0800

    feature: number of islands revisit

--- graphs/NumberOfIslands.py
+++ graphs/NumberOfIslands.py
@@ -24,3 +24,17 @@ def num_islands_dfs(self, grid: List[List[str]]) -> int:
                 cnt += 1
 
     return cnt
+
+
+# O(n * m) time || O(n * m) space
+def num_islands_smart(self, grid: List[List[str]]) -> int:
+    def sink(i, j):
+        if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+            grid[i][j] = '0'
+            list(map(sink, (i + 1, i - 1, i, i), (j, j, j + 1, j - 1)))
+
+            return 1
+
+        return 0
+
+    return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
