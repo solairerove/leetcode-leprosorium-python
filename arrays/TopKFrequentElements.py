77bb57c11235ed613c12d43df371aa1996a663af
solairerove <solairerove@gmail.com>
2023-03-07 17:02:45 +0800

    feature: top k frequent elements quickselect

--- arrays/TopKFrequentElements.py
+++ arrays/TopKFrequentElements.py
@@ -1,6 +1,47 @@
 import heapq
+import random
 from collections import Counter
 from typing import List
+
+
+# O(n) time || O(n) space
+def top_k_frequent_quickselect(self, nums: List[int], k: int) -> List[int]:
+    cnt = Counter(nums)
+    unique = list(cnt.keys())
+
+    def partition(low, high, idx):
+        freq = cnt[unique[idx]]
+        unique[idx], unique[high] = unique[high], unique[idx]
+
+        store_idx = low
+        for i in range(low, high):
+            if cnt[unique[i]] < freq:
+                unique[store_idx], unique[i] = unique[i], unique[store_idx]
+                store_idx += 1
+
+        unique[high], unique[store_idx] = unique[store_idx], unique[high]
+
+        return store_idx
+
+    def quick_select(low, high, k_smallest):
+        if low == high:
+            return
+
+        pivot_idx = random.randint(low, high)
+        pivot_idx = partition(low, high, pivot_idx)
+
+        if k_smallest == pivot_idx:
+            return
+        elif k_smallest < pivot_idx:
+            quick_select(low, pivot_idx - 1, k_smallest)
+        else:
+            quick_select(pivot_idx + 1, high, k_smallest)
+
+    n = len(unique)
+
+    quick_select(0, n - 1, n - k)
+
+    return unique[n - k:]
 
 
 # O(n * log(k)) time || O(n + k) space
--- arrays/test/test_top_k_frequent.py
+++ arrays/test/test_top_k_frequent.py
@@ -1,16 +1,18 @@
 import unittest
 
-from arrays.TopKFrequentElements import top_k_frequent_heap, top_k_frequent_linear
+from arrays.TopKFrequentElements import top_k_frequent_heap, top_k_frequent_linear, top_k_frequent_quickselect
 
 
 class MyTestCase(unittest.TestCase):
     def test_top_k_frequent(self):
-        self.assertEqual([1, 2], top_k_frequent_heap(self, [1,1,1,2,2,3], 2))
-        self.assertEqual([1, 2], top_k_frequent_linear(self, [1,1,1,2,2,3], 2))
+        self.assertEqual([1, 2], top_k_frequent_heap(self, [1, 1, 1, 2, 2, 3], 2))
+        self.assertEqual([1, 2], top_k_frequent_linear(self, [1, 1, 1, 2, 2, 3], 2))
+        self.assertEqual([2, 1], top_k_frequent_quickselect(self, [1, 1, 1, 2, 2, 3], 2))
 
     def test_top_k_frequent_1(self):
         self.assertEqual([1], top_k_frequent_heap(self, [1], 1))
         self.assertEqual([1], top_k_frequent_linear(self, [1], 1))
+        self.assertEqual([1], top_k_frequent_quickselect(self, [1], 1))
 
 
 if __name__ == '__main__':
