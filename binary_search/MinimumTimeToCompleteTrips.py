725e27ee251f48dbed2714d30ba40fd987a39e49
solairerove <solairerove@gmail.com>
2023-03-07 20:00:32 +0800

    feature: minimum time to complete trips revisit

--- binary_search/MinimumTimeToCompleteTrips.py
+++ binary_search/MinimumTimeToCompleteTrips.py
@@ -6,20 +6,16 @@ from typing import List
 # m - total limit of total_trips
 # k - max time taken by one trip
 def minimum_time(self, time: List[int], total_trips: int) -> int:
-    low, high = 1, max(time) * total_trips
+    low, high = 1, min(time) * total_trips
 
     def enough_time(given_time):
-        actual_trips = 0
-        for t in time:
-            actual_trips += given_time // t
-
-        return actual_trips >= total_trips
+        return sum(given_time // t for t in time) >= total_trips
 
     while low < high:
         mid = low + (high - low) // 2
-        if enough_time(mid):
-            high = mid
-        else:
+        if not enough_time(mid):
             low = mid + 1
+        else:
+            high = mid
 
     return low
