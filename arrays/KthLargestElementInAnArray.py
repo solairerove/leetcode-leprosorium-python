23bc736570bc4e50c4b1f3d488287277d5c86f4e
solairerove <solairerove@gmail.com>
2023-03-08 13:33:35 +0800

    feature: kth largest element in an array quick select approach

--- arrays/KthLargestElementInAnArray.py
+++ arrays/KthLargestElementInAnArray.py
@@ -1,5 +1,39 @@
 import heapq
+import random
 from typing import List
+
+
+# O(max(n, n^2) time || O(1) space
+def find_kth_largest(self, nums: List[int], k: int) -> int:
+    def partition(low, high, pivot_idx):
+        pivot = nums[pivot_idx]
+        nums[pivot_idx], nums[high] = nums[high], nums[pivot_idx]
+
+        store_idx = low
+        for i in range(low, high):
+            if nums[i] < pivot:
+                nums[store_idx], nums[i] = nums[i], nums[store_idx]
+                store_idx += 1
+
+        nums[high], nums[store_idx] = nums[store_idx], nums[high]
+
+        return store_idx
+
+    def select(low, high, k_smallest):
+        if low == high:
+            return nums[low]
+
+        pivot_idx = random.randint(low, high)
+        pivot_idx = partition(low, high, pivot_idx)
+
+        if k_smallest == pivot_idx:
+            return nums[k_smallest]
+        elif k_smallest < pivot_idx:
+            return select(low, pivot_idx - 1, k_smallest)
+        else:
+            return select(pivot_idx + 1, high, k_smallest)
+
+    return select(0, len(nums) - 1, len(nums) - k)
 
 
 # O(n * log(k)) time || O(k) space
--- arrays/test/test_find_kth_largest.py
+++ arrays/test/test_find_kth_largest.py
@@ -1,13 +1,15 @@
 import unittest
 
-from arrays.KthLargestElementInAnArray import find_kth_largest_using_heap
+from arrays.KthLargestElementInAnArray import find_kth_largest_using_heap, find_kth_largest
 
 
 class MyTestCase(unittest.TestCase):
     def test_find_kth_largest(self):
-        self.assertEqual(2, find_kth_largest_using_heap(self, [3, 2, 1, 5, 6, 4], 2))
+        self.assertEqual(5, find_kth_largest(self, [3, 2, 1, 5, 6, 4], 2))
+        self.assertEqual(5, find_kth_largest_using_heap(self, [3, 2, 1, 5, 6, 4], 2))
 
     def test_find_kth_largest_1(self):
+        self.assertEqual(4, find_kth_largest(self, [3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
         self.assertEqual(4, find_kth_largest_using_heap(self, [3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
 
 
