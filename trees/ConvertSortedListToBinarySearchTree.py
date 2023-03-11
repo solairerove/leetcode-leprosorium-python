2bb84a18486b9d379fd18552e7a68d7f515c4c6b
solairerove <solairerove@gmail.com>
2023-03-11 18:04:48 +0800

    feature: convert sorted list to binary search tree using array

--- trees/ConvertSortedListToBinarySearchTree.py
+++ trees/ConvertSortedListToBinarySearchTree.py
@@ -4,8 +4,45 @@ from linked_list.ListNode import ListNode
 from trees.TreeNode import TreeNode
 
 
+# O(n) time || O(n) space
+def sorted_list_to_bst_using_array(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+    if not head:
+        return
+
+    if not head.next:
+        return TreeNode(head.val)
+
+    arr = linked_list_to_array(self, head)
+
+    def convert_arr_to_bst(low, high):
+        if high < low:
+            return None
+
+        mid = low + (high - low) // 2
+        node = TreeNode(arr[mid])
+
+        if low == high:
+            return node
+
+        node.left = convert_arr_to_bst(low, mid - 1)
+        node.right = convert_arr_to_bst(mid + 1, high)
+
+        return node
+
+    return convert_arr_to_bst(0, len(arr) - 1)
+
+
+def linked_list_to_array(self, head: Optional[ListNode]) -> []:
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+
+    return arr
+
+
 # O(max(n * log(n), n^2)) time || O((log(n), n)) space
-def sorted_list_to_bst(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+def sorted_list_to_bst_rec(self, head: Optional[ListNode]) -> Optional[TreeNode]:
     if not head:
         return
 
@@ -18,7 +55,7 @@ def sorted_list_to_bst(self, head: Optional[ListNode]) -> Optional[TreeNode]:
     prev.next = None
 
     root = TreeNode(slow.val)
-    root.left = sorted_list_to_bst(self, head)
-    root.right = sorted_list_to_bst(self, slow.next)
+    root.left = sorted_list_to_bst_rec(self, head)
+    root.right = sorted_list_to_bst_rec(self, slow.next)
 
     return root
--- trees/test/test_sorted_list_to_bst.py
+++ trees/test/test_sorted_list_to_bst.py
@@ -1,18 +1,27 @@
 import unittest
 
 from linked_list.test.utils import transform_array_to_list_node
-from trees.ConvertSortedListToBinarySearchTree import sorted_list_to_bst
+from trees.ConvertSortedListToBinarySearchTree import sorted_list_to_bst_rec, sorted_list_to_bst_using_array
 
 
 class MyTestCase(unittest.TestCase):
     def test_sorted_list_to_bst(self):
         linked_list = transform_array_to_list_node(self, [-10, -3, 0, 5, 9])
-        root = sorted_list_to_bst(self, linked_list)
+        root = sorted_list_to_bst_rec(self, linked_list)
         self.assertEqual(0, root.val)
         self.assertEqual(-3, root.left.val)
         self.assertEqual(9, root.right.val)
         self.assertEqual(-10, root.left.left.val)
         self.assertEqual(5, root.right.left.val)
+
+    def test_sorted_list_to_bst_1(self):
+        linked_list = transform_array_to_list_node(self, [-10, -3, 0, 5, 9])
+        root = sorted_list_to_bst_using_array(self, linked_list)
+        self.assertEqual(0, root.val)
+        self.assertEqual(-10, root.left.val)
+        self.assertEqual(-3, root.left.right.val)
+        self.assertEqual(5, root.right.val)
+        self.assertEqual(9, root.right.right.val)
 
 
 if __name__ == '__main__':
