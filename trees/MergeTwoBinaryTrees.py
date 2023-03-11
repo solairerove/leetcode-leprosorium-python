618ee779990dc0754305d0bbc53b4ca048dc27ff
solairerove <solairerove@gmail.com>
2023-03-11 19:06:38 +0800

    feature: merge two binary trees iterative approach

--- trees/MergeTwoBinaryTrees.py
+++ trees/MergeTwoBinaryTrees.py
@@ -4,6 +4,31 @@ from trees.TreeNode import TreeNode
 
 
 # O(min(n, n)) time || O(min(n, m)) space
+def merge_trees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    if not root1 or not root2:
+        return root1 or root2
+
+    stack = [(root1, root2)]
+    while stack:
+        node1, node2 = stack.pop()
+        if not node2:
+            continue
+
+        node1.val += node2.val
+
+        if not node1.left and node2.left:
+            node1.left = TreeNode(0)
+
+        if not node1.right and node2.right:
+            node1.right = TreeNode(0)
+
+        stack.append((node1.right, node2.right))
+        stack.append((node1.left, node2.left))
+
+    return root1
+
+
+# O(min(n, n)) time || O(min(n, m)) space
 def merge_trees_rec(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
     if not root1:
         return root2
--- trees/test/test_merge_trees.py
+++ trees/test/test_merge_trees.py
@@ -1,11 +1,11 @@
 import unittest
 
-from trees.MergeTwoBinaryTrees import merge_trees_rec
+from trees.MergeTwoBinaryTrees import merge_trees_rec, merge_trees
 from trees.TreeNode import TreeNode
 
 
 class MyTestCase(unittest.TestCase):
-    def test_merge_trees(self):
+    def test_merge_trees_rec(self):
         root1 = TreeNode(
             val=1,
             left=TreeNode(
@@ -34,6 +34,43 @@ class MyTestCase(unittest.TestCase):
             )
         )
         actual = merge_trees_rec(self, root1, root2)
+        self.assertEqual(3, actual.val)
+        self.assertEqual(4, actual.left.val)
+        self.assertEqual(5, actual.left.left.val)
+        self.assertEqual(4, actual.left.right.val)
+        self.assertEqual(5, actual.right.val)
+        self.assertEqual(7, actual.right.right.val)
+
+    def test_merge_trees_stack(self):
+        root1 = TreeNode(
+            val=1,
+            left=TreeNode(
+                val=3,
+                left=TreeNode(
+                    val=5
+                )
+            ),
+            right=TreeNode(
+                val=2
+            )
+        )
+        root2 = TreeNode(
+            val=2,
+            left=TreeNode(
+                val=1,
+                right=TreeNode(
+                    val=4
+                )
+            ),
+            right=TreeNode(
+                val=3,
+                right=TreeNode(
+                    val=7
+                )
+            )
+        )
+
+        actual = merge_trees(self, root1, root2)
         self.assertEqual(3, actual.val)
         self.assertEqual(4, actual.left.val)
         self.assertEqual(5, actual.left.left.val)
