a9beca4dfcb86ff3ed32c28f2dcb70a997fa9a21
solairerove <solairerove@gmail.com>
2023-03-11 18:42:58 +0800

    feature: convert sorted list to binary search tree using inorder

--- trees/ConvertSortedListToBinarySearchTree.py
+++ trees/ConvertSortedListToBinarySearchTree.py
@@ -4,6 +4,40 @@ from linked_list.ListNode import ListNode
 from trees.TreeNode import TreeNode
 
 
+# O(n) time || O(log(n)) space
+def sorted_list_to_bst(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+    size = find_size(head)
+
+    def convert(low, high):
+        nonlocal head
+
+        if high < low:
+            return None
+
+        mid = low + (high - low) // 2
+
+        left = convert(low, mid - 1)
+
+        node = TreeNode(head.val)
+        node.left = left
+
+        head = head.next
+
+        node.right = convert(mid + 1, high)
+
+        return node
+
+    return convert(0, size - 1)
+
+
+def find_size(head):
+    curr, size = head, 0
+    while curr:
+        curr, size = curr.next, size + 1
+
+    return size
+
+
 # O(n) time || O(n) space
 def sorted_list_to_bst_using_array(self, head: Optional[ListNode]) -> Optional[TreeNode]:
     if not head:
--- trees/test/test_sorted_list_to_bst.py
+++ trees/test/test_sorted_list_to_bst.py
@@ -1,7 +1,8 @@
 import unittest
 
 from linked_list.test.utils import transform_array_to_list_node
-from trees.ConvertSortedListToBinarySearchTree import sorted_list_to_bst_rec, sorted_list_to_bst_using_array
+from trees.ConvertSortedListToBinarySearchTree import sorted_list_to_bst_rec, sorted_list_to_bst_using_array, \
+    sorted_list_to_bst
 
 
 class MyTestCase(unittest.TestCase):
@@ -17,6 +18,15 @@ class MyTestCase(unittest.TestCase):
     def test_sorted_list_to_bst_1(self):
         linked_list = transform_array_to_list_node(self, [-10, -3, 0, 5, 9])
         root = sorted_list_to_bst_using_array(self, linked_list)
+        self.assertEqual(0, root.val)
+        self.assertEqual(-10, root.left.val)
+        self.assertEqual(-3, root.left.right.val)
+        self.assertEqual(5, root.right.val)
+        self.assertEqual(9, root.right.right.val)
+
+    def test_sorted_list_to_bst_2(self):
+        linked_list = transform_array_to_list_node(self, [-10, -3, 0, 5, 9])
+        root = sorted_list_to_bst(self, linked_list)
         self.assertEqual(0, root.val)
         self.assertEqual(-10, root.left.val)
         self.assertEqual(-3, root.left.right.val)
