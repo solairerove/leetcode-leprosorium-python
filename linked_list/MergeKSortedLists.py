1d580d0225dde58a9bcaa85ae5a9cbfc61cebeff
solairerove <solairerove@gmail.com>
2023-03-13 12:15:33 +0800

    feature: merge k sorted lists using heap

--- linked_list/MergeKSortedLists.py
+++ linked_list/MergeKSortedLists.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List, Optional
 
 from linked_list.ListNode import ListNode
@@ -23,3 +24,31 @@ def merge_k_lists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         interval *= 2
 
     return lists[0]
+
+
+# O(Nlog(k)) time | O(k) space
+# N - is total number of nodes
+# k - is number of linked lists
+def merge_k_lists_heap(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    if not lists:
+        return None
+
+    if len(lists) == 1:
+        return lists[0]
+
+    sentinel = prev = ListNode(0)
+    heap = []
+    for i in range(len(lists)):
+        if lists[i]:
+            heapq.heappush(heap, (lists[i].val, i))
+            lists[i] = lists[i].next
+
+    while heap:
+        val, i = heapq.heappop(heap)
+        prev.next = ListNode(val)
+        prev = prev.next
+        if lists[i]:
+            heapq.heappush(heap, (lists[i].val, i))
+            lists[i] = lists[i].next
+
+    return sentinel.next
