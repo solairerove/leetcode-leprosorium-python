a4b3b5871e3a31006e80db5b914a53ad26f82a64
solairerove <solairerove@gmail.com>
2023-03-17 13:10:17 +0800

    feature: squares of a sorted array using two pointers

--- arrays/SquaresOfASortedArray.py
+++ arrays/SquaresOfASortedArray.py
@@ -3,7 +3,18 @@ from typing import List
 
 # O(n) time || O(n) space
 def sorted_squares(self, nums: List[int]) -> List[int]:
-    return []
+    res = [0] * len(nums)
+    low, high = 0, len(nums) - 1
+    for i in range(len(nums) - 1, -1, -1):
+        if abs(nums[low]) < abs(nums[high]):
+            sq = nums[high]
+            high -= 1
+        else:
+            sq = nums[low]
+            low += 1
+        res[i] = sq * sq
+
+    return res
 
 
 # O(n * log(n)) time || O(n) space
