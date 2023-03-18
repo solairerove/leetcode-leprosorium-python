65d0d96752afe999b07c6027e1044a659700b6a8
solairerove <solairerove@gmail.com>
2023-03-18 13:47:49 +0800

    feature: backspace string compare using stack

--- arrays/BackspaceStringCompare.py
+++ arrays/BackspaceStringCompare.py
@@ -14,3 +14,18 @@ def backspace_compare_two_pointers(self, s: str, t: str) -> bool:
                 yield c
 
     return all(x == y for x, y in itertools.zip_longest(trim(s), trim(t)))
+
+
+# O(n+m) time || O(n+m) space
+def backspace_compare_stack(self, s: str, t: str) -> bool:
+    def trim(to_trim):
+        res = []
+        for c in to_trim:
+            if c != '#':
+                res.append(c)
+            elif res:
+                res.pop()
+
+        return "".join(res)
+
+    return trim(s) == trim(t)
