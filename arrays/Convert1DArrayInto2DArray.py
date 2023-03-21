6a0518d52e76546e8dab6d0dd50d84b31e9d8c92
solairerove <solairerove@gmail.com>
2023-03-21 15:52:11 +0800

    feature: convert 1d array into 2d array using efficient approach

--- arrays/Convert1DArrayInto2DArray.py
+++ arrays/Convert1DArrayInto2DArray.py
@@ -1,6 +1,18 @@
 from typing import List
 
 
+# O(n) time || O(1) space
+def construct_2d_array_smart(self, original: List[int], m: int, n: int) -> List[List[int]]:
+    if len(original) != n * m:
+        return []
+
+    res = []
+    for i in range(0, len(original), n):
+        res.append(original[i:i + n])
+
+    return res
+
+
 # O(n * m) time || O(1) space
 def construct_2d_array(self, original: List[int], m: int, n: int) -> List[List[int]]:
     if len(original) > m * n:
