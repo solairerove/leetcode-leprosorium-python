08a2f5921150f2af3fb99fe16ab97ee3ee33be97
solairerove <solairerove@gmail.com>
2023-03-21 12:56:04 +0800

    feature: majority element using sorting

--- arrays/MajorityElement.py
+++ arrays/MajorityElement.py
@@ -2,6 +2,13 @@ from collections import Counter
 from typing import List
 
 
+# O(n * log(n)) time || O(1) space
+def majority_element_sort(self, nums: List[int]) -> int:
+    nums.sort()
+
+    return nums[len(nums) // 2]
+
+
 # O(n) time || O(n) space
 def majority_element_hm(self, nums: List[int]) -> int:
     cnt = Counter(nums)
--- arrays/test/test_majority_element.py
+++ arrays/test/test_majority_element.py
@@ -1,14 +1,16 @@
 import unittest
 
-from arrays.MajorityElement import majority_element_hm
+from arrays.MajorityElement import majority_element_hm, majority_element_sort
 
 
 class MyTestCase(unittest.TestCase):
     def test_majority_element(self):
         self.assertEqual(3, majority_element_hm(self, [3, 2, 3]))
+        self.assertEqual(3, majority_element_sort(self, [3, 2, 3]))
 
     def test_majority_element_1(self):
         self.assertEqual(2, majority_element_hm(self, [2, 2, 1, 1, 1, 2, 2]))
+        self.assertEqual(2, majority_element_sort(self, [2, 2, 1, 1, 1, 2, 2]))
 
 
 if __name__ == '__main__':
