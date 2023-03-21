51c4a23878006b61f7d6fb7c05153f4fe4941534
solairerove <solairerove@gmail.com>
2023-03-21 13:01:22 +0800

    feature: majority element using boyer moore voting alg

--- arrays/MajorityElement.py
+++ arrays/MajorityElement.py
@@ -2,6 +2,17 @@ from collections import Counter
 from typing import List
 
 
+# O(n) time || O() space
+def majority_element_boyer_moore_voting(self, nums: List[int]) -> int:
+    cnt, res = 0, None
+    for num in nums:
+        if cnt == 0:
+            res = num
+        cnt += (1 if num == res else -1)
+
+    return res
+
+
 # O(n * log(n)) time || O(1) space
 def majority_element_sort(self, nums: List[int]) -> int:
     nums.sort()
--- arrays/test/test_majority_element.py
+++ arrays/test/test_majority_element.py
@@ -1,16 +1,18 @@
 import unittest
 
-from arrays.MajorityElement import majority_element_hm, majority_element_sort
+from arrays.MajorityElement import majority_element_hm, majority_element_sort, majority_element_boyer_moore_voting
 
 
 class MyTestCase(unittest.TestCase):
     def test_majority_element(self):
         self.assertEqual(3, majority_element_hm(self, [3, 2, 3]))
         self.assertEqual(3, majority_element_sort(self, [3, 2, 3]))
+        self.assertEqual(3, majority_element_boyer_moore_voting(self, [3, 2, 3]))
 
     def test_majority_element_1(self):
         self.assertEqual(2, majority_element_hm(self, [2, 2, 1, 1, 1, 2, 2]))
         self.assertEqual(2, majority_element_sort(self, [2, 2, 1, 1, 1, 2, 2]))
+        self.assertEqual(2, majority_element_boyer_moore_voting(self, [2, 2, 1, 1, 1, 2, 2]))
 
 
 if __name__ == '__main__':
