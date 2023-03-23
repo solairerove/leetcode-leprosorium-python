accfdbcc873080c6ac1603a370c5d46e98e86926
solairerove <solairerove@gmail.com>
2023-03-23 15:55:29 +0800

    feature: number of operations to make network connected using union set

--- graphs/NumberOfOperationsToMakeNetworkConnected.py
+++ graphs/NumberOfOperationsToMakeNetworkConnected.py
@@ -58,3 +58,42 @@ def bfs(self, node, adj, visit):
             if not visit[neighbor]:
                 visit[neighbor] = True
                 dq.append(neighbor)
+
+
+# O(n + e) time || O(n) space
+def make_connected_union(self, n: int, connections: List[List[int]]) -> int:
+    if len(connections) < n - 1:
+        return -1
+
+    dsu = UnionFind(n)
+    number_of_connected_components = n
+    for connection in connections:
+        if dsu.find(connection[0]) != dsu.find(connection[1]):
+            number_of_connected_components -= 1
+            dsu.union_set(connection[0], connection[1])
+
+    return number_of_connected_components - 1
+
+
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        self.rank = [0] * size
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+    def union_set(self, x, y):
+        x_set = self.find(x)
+        y_set = self.find(y)
+
+        if self.rank[x_set] < self.rank[y_set]:
+            self.parent[x_set] = y_set
+        elif self.rank[x_set] > self.rank[y_set]:
+            self.parent[y_set] = x_set
+        else:
+            self.parent[y_set] = x_set
+            self.rank[x_set] += 1
--- graphs/test/test_make_connected.py
+++ graphs/test/test_make_connected.py
@@ -1,20 +1,23 @@
 import unittest
 
-from graphs.NumberOfOperationsToMakeNetworkConnected import make_connected_dfs, make_connected_bfs
+from graphs.NumberOfOperationsToMakeNetworkConnected import make_connected_dfs, make_connected_bfs, make_connected_union
 
 
 class MyTestCase(unittest.TestCase):
     def test_make_connected(self):
         self.assertEqual(1, make_connected_dfs(self, 4, [[0, 1], [0, 2], [1, 2]]))
         self.assertEqual(1, make_connected_bfs(self, 4, [[0, 1], [0, 2], [1, 2]]))
+        self.assertEqual(1, make_connected_union(self, 4, [[0, 1], [0, 2], [1, 2]]))
 
     def test_make_connected_1(self):
         self.assertEqual(2, make_connected_dfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
         self.assertEqual(2, make_connected_bfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
+        self.assertEqual(2, make_connected_union(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
 
     def test_make_connected_2(self):
         self.assertEqual(-1, make_connected_dfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
         self.assertEqual(-1, make_connected_bfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
+        self.assertEqual(-1, make_connected_union(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
 
 
 if __name__ == '__main__':
