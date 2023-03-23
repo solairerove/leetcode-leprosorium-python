20ce6484d4b490e4dd7606a330445209828f06fb
solairerove <solairerove@gmail.com>
2023-03-23 15:43:52 +0800

    feature: number of operations to make network connected using bfs

--- graphs/NumberOfOperationsToMakeNetworkConnected.py
+++ graphs/NumberOfOperationsToMakeNetworkConnected.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 
@@ -12,7 +13,7 @@ def make_connected_dfs(self, n: int, connections: List[List[int]]) -> int:
         adj[connection[1]].append(connection[0])
 
     number_of_connected_components = 0
-    visit = [False for _ in range(n)]
+    visit = [False] * n
     for i in range(n):
         if not visit[i]:
             number_of_connected_components += 1
@@ -26,3 +27,34 @@ def dfs(self, node, adj, visit):
     for neighbor in adj[node]:
         if not visit[neighbor]:
             dfs(self, neighbor, adj, visit)
+
+
+# O(n + e) time || O(n) space
+def make_connected_bfs(self, n: int, connections: List[List[int]]) -> int:
+    if len(connections) < n - 1:
+        return -1
+
+    adj = [[] for _ in range(n)]
+    for connection in connections:
+        adj[connection[0]].append(connection[1])
+        adj[connection[1]].append(connection[0])
+
+    number_of_connected_components = 0
+    visit = [False] * n
+    for i in range(n):
+        if not visit[i]:
+            number_of_connected_components += 1
+            bfs(self, i, adj, visit)
+
+    return number_of_connected_components - 1
+
+
+def bfs(self, node, adj, visit):
+    dq = collections.deque([node])
+    visit[node] = True
+    while dq:
+        node = dq.popleft()
+        for neighbor in adj[node]:
+            if not visit[neighbor]:
+                visit[neighbor] = True
+                dq.append(neighbor)
--- graphs/test/test_make_connected.py
+++ graphs/test/test_make_connected.py
@@ -1,17 +1,20 @@
 import unittest
 
-from graphs.NumberOfOperationsToMakeNetworkConnected import make_connected_dfs
+from graphs.NumberOfOperationsToMakeNetworkConnected import make_connected_dfs, make_connected_bfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_make_connected(self):
         self.assertEqual(1, make_connected_dfs(self, 4, [[0, 1], [0, 2], [1, 2]]))
+        self.assertEqual(1, make_connected_bfs(self, 4, [[0, 1], [0, 2], [1, 2]]))
 
     def test_make_connected_1(self):
         self.assertEqual(2, make_connected_dfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
+        self.assertEqual(2, make_connected_bfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
 
     def test_make_connected_2(self):
         self.assertEqual(-1, make_connected_dfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
+        self.assertEqual(-1, make_connected_bfs(self, 6, [[0, 1], [0, 2], [0, 3], [1, 2]]))
 
 
 if __name__ == '__main__':
