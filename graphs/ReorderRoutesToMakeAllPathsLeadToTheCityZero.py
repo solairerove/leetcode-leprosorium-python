72794c690b60a2efea6246d07f140c69ab82de25
solairerove <solairerove@gmail.com>
2023-03-24 16:50:10 +0800

    feature: reorder routes to make all path lead to the city zero using bfs

--- graphs/ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
+++ graphs/ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -2,8 +2,31 @@ import collections
 from typing import List
 
 
+# O(n + e) time || O(n) space
+def min_reorder_bfs(self, n: int, connections: List[List[int]]) -> int:
+    graph = collections.defaultdict(list)
+
+    for u, v in connections:
+        graph[u].append((v, 1))
+        graph[v].append((u, 0))
+
+    dq = collections.deque([0])
+    visited = {0}
+    res = 0
+    while dq:
+        city = dq.popleft()
+        for neighbor, cost in graph[city]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                res += cost
+                dq.append(neighbor)
+
+    return res
+
+
 # O(n + e) time || O(n + e) space
 def min_reorder_dfs(self, n: int, connections: List[List[int]]) -> int:
+    # adj?
     graph = collections.defaultdict(list)
 
     # don't like this
--- graphs/test/test_min_reorder.py
+++ graphs/test/test_min_reorder.py
@@ -1,17 +1,20 @@
 import unittest
 
-from graphs.ReorderRoutesToMakeAllPathsLeadToTheCityZero import min_reorder_dfs
+from graphs.ReorderRoutesToMakeAllPathsLeadToTheCityZero import min_reorder_dfs, min_reorder_bfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_min_reorder(self):
         self.assertEqual(3, min_reorder_dfs(self, 6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]))
+        self.assertEqual(3, min_reorder_bfs(self, 6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]))
 
     def test_min_reorder_1(self):
         self.assertEqual(2, min_reorder_dfs(self, 5, [[1, 0], [1, 2], [3, 2], [3, 4]]))
+        self.assertEqual(2, min_reorder_bfs(self, 5, [[1, 0], [1, 2], [3, 2], [3, 4]]))
 
     def test_min_reorder_2(self):
         self.assertEqual(0, min_reorder_dfs(self, 3, [[1, 0], [2, 0]]))
+        self.assertEqual(0, min_reorder_bfs(self, 3, [[1, 0], [2, 0]]))
 
 
 if __name__ == '__main__':
