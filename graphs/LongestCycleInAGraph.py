f789866fca2df09c3ab32f10bbb3feba82d8b43b
solairerove <solairerove@gmail.com>
2023-03-26 11:51:55 +0800

    feature: longest cycle in a graph using kahn

--- graphs/LongestCycleInAGraph.py
+++ graphs/LongestCycleInAGraph.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 # O(n) time || O(n) space
-def longest_cycle(self, edges: List[int]) -> int:
+def longest_cycle_dfs(self, edges: List[int]) -> int:
     n = len(edges)
     visit = set()
     ranks = [float('inf')] * n
@@ -25,5 +25,46 @@ def longest_cycle(self, edges: List[int]) -> int:
         cycle_len = dfs(node, 0)
         if cycle_len > 0:
             res = max(res, cycle_len)
+
+    return res
+
+
+# O(n) time || O(n) space
+def longest_cycle_kahn(self, edges: List[int]) -> int:
+    n = len(edges)
+    visit = [False] * n
+    indegree = [0] * n
+
+    for edge in edges:
+        if edge != -1:
+            indegree[edge] = indegree[edge] + 1
+
+    q = []
+    for i in range(n):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        node = q.pop(0)
+        visit[node] = True
+        neighbor = edges[node]
+
+        if neighbor != -1:
+            indegree[neighbor] = indegree[neighbor] - 1
+            if indegree[neighbor] == 0:
+                q.append(neighbor)
+
+    res = -1
+    for i in range(n):
+        if not visit[i]:
+            neighbor = edges[i]
+            cnt = 1
+            visit[i] = True
+            while neighbor != i:
+                visit[neighbor] = True
+                cnt += 1
+                neighbor = edges[neighbor]
+
+            res = max(cnt, res)
 
     return res
--- graphs/test/test_longest_cycle.py
+++ graphs/test/test_longest_cycle.py
@@ -1,14 +1,16 @@
 import unittest
 
-from graphs.LongestCycleInAGraph import longest_cycle
+from graphs.LongestCycleInAGraph import longest_cycle_dfs, longest_cycle_kahn
 
 
 class MyTestCase(unittest.TestCase):
     def test_longest_cycle(self):
-        self.assertEqual(3, longest_cycle(self, [3, 3, 4, 2, 3]))
+        self.assertEqual(3, longest_cycle_dfs(self, [3, 3, 4, 2, 3]))
+        self.assertEqual(3, longest_cycle_kahn(self, [3, 3, 4, 2, 3]))
 
     def test_longest_cycle_1(self):
-        self.assertEqual(-1, longest_cycle(self, [2, -1, 3, 1]))
+        self.assertEqual(-1, longest_cycle_dfs(self, [2, -1, 3, 1]))
+        self.assertEqual(-1, longest_cycle_kahn(self, [2, -1, 3, 1]))
 
 
 if __name__ == '__main__':
