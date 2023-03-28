5ce973ea4ccdd75a4b29493076d99423b2e83df8
solairerove <solairerove@gmail.com>
2023-03-28 13:33:57 +0800

    feature: minimum cost for tickets bottom up

--- dynamic_programming/MinimumCostForTickets.py
+++ dynamic_programming/MinimumCostForTickets.py
@@ -39,3 +39,20 @@ def min_cost_tickets_window(self, days: List[int], costs: List[int]) -> int:
         return res
 
     return dp(0)
+
+
+# O(n) time || O(n) space, where n - unique days
+def min_cost_tickets_bottom_up(self, days: List[int], costs: List[int]) -> int:
+    dp = [0] * (days[-1] + 1)
+    day_set = set(days)
+    for i in range(1, len(dp)):
+        if i in days:
+            dp[i] = min(
+                dp[max(i - 1, 0)] + costs[0],
+                dp[max(i - 7, 0)] + costs[1],
+                dp[max(i - 30, 0)] + costs[2]
+            )
+        else:
+            dp[i] = dp[i - 1]
+
+    return dp[-1]
