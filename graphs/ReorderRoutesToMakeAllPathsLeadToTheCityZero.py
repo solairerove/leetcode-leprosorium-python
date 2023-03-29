289bb16fc3b2126bc99aa5f5d2670c5aab628009
solairerove <solairerove@gmail.com>
2023-03-29 16:43:10 +0800

    feature: reorder routes to make all path lead to the city dfs revisit

--- graphs/ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
+++ graphs/ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -26,30 +26,21 @@ def min_reorder_bfs(self, n: int, connections: List[List[int]]) -> int:
 
 # O(n + e) time || O(n + e) space
 def min_reorder_dfs(self, n: int, connections: List[List[int]]) -> int:
-    # adj?
-    graph = collections.defaultdict(list)
-
-    # don't like this
-    # adj[connection[0]].append(connection[1]) is better for me
-    # need to create some code practice
+    adj = {x: [] for x in range(n)}
     for x, y in connections:
-        graph[x].append((y, 1))
-        graph[y].append((x, 0))
+        adj[x].append((y, 1))
+        adj[y].append((x, 0))
 
-    # don't like this
-    # like using [False] * n
-    visited = {0}
     res = [0]
-    dfs(self, 0, graph, visited, res)
+    visit = [False] * n
+    dfs(self, 0, adj, visit, res)
 
     return res[0]
 
 
-# don't like city, node is better
-def dfs(self, city, graph, visited, res):
-    visited.add(city)
-    for neighbor, cost in graph[city]:
-        if neighbor not in visited:
-            visited.add(neighbor)
+def dfs(self, node, adj, visit, res):
+    visit[node] = True
+    for neighbor, cost in adj[node]:
+        if not visit[neighbor]:
             res[0] += cost
-            dfs(self, neighbor, graph, visited, res)
+            dfs(self, neighbor, adj, visit, res)
