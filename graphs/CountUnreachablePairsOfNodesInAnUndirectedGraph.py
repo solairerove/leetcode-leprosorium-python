bb257174064836f55d40ed0d73b16b002f875f4e
solairerove <solairerove@gmail.com>
2023-03-30 20:42:07 +0800

    feature: count unreachable pairs of nodes in an undirected graph revisit

--- graphs/CountUnreachablePairsOfNodesInAnUndirectedGraph.py
+++ graphs/CountUnreachablePairsOfNodesInAnUndirectedGraph.py
@@ -1,21 +1,22 @@
-import collections
 from typing import List
 
 
 # O(n + e) time || O(n + e) space
 def count_pairs(self, n: int, edges: List[List[int]]) -> int:
-    def dfs(node):
-        if node in visited:
-            return 0
-        visited.add(node)
+    adj = {x: [] for x in range(n)}
+    for x, y in edges:
+        adj[x].append(y)
+        adj[y].append(x)
 
-        return sum(dfs(nei) for nei in adj_list[node]) + 1
+    visit = [False] * n
 
-    adj_list = collections.defaultdict(list)
-    for u, v in edges:
-        adj_list[u].append(v)
-        adj_list[v].append(u)
+    return sum((size := dfs(self, i, adj, visit)) * (n - size) for i in range(n) if not visit[i]) // 2
 
-    visited = set()
 
-    return sum((size := dfs(i)) * (n - size) for i in range(n) if i not in visited) // 2
+def dfs(self, node, adj, visit):
+    if visit[node]:
+        return 0
+
+    visit[node] = True
+
+    return sum(dfs(self, neighbor, adj, visit) for neighbor in adj[node]) + 1
