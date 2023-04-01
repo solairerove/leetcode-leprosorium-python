a0e6944ae92b7d541172efbd869fbb736003e34a
solairerove <solairerove@gmail.com>
2023-04-01 10:15:50 +0800

    feature: range sum query revisit

--- dynamic_programming/RangeSumQuery.py
+++ dynamic_programming/RangeSumQuery.py
@@ -3,12 +3,15 @@ from typing import List
 
 class NumArray:
 
+    # O(n) time || O(n) space
     def __init__(self, nums: List[int]):
-        self.sums = [0] * (len(nums) + 1)
+        self.sums = nums
+        for i in range(len(nums) - 1):
+            self.sums[i + 1] += self.sums[i]
 
-        for i in range(len(nums)):
-            self.sums[i + 1] = self.sums[i] + nums[i]
-
-    # O(1) time || O(n) space
+    # O(1) time || O(1) space
     def sum_range(self, left: int, right: int) -> int:
-        return self.sums[right + 1] - self.sums[left]
+        if left == 0:
+            return self.sums[right]
+
+        return self.sums[right] - self.sums[left - 1]
