a96f5f08d8aaf19d0413d8fe28cc26413efbccd1
solairerove <solairerove@gmail.com>
2023-04-02 19:53:03 +0800

    feature: range sum query mutable using segmented tree

--- dynamic_programming/RangeSumQueryMutable.py
+++ dynamic_programming/RangeSumQueryMutable.py
@@ -62,7 +62,7 @@ class FenwickTree:
         return self.sum_range(low, mid, node.left) + self.sum_range(mid + 1, high, node.right)
 
 
-class NumArray:
+class NumArrayBIT:
 
     def __init__(self, nums: List[int]):
         self.tree = FenwickTree(nums)
@@ -74,3 +74,62 @@ class NumArray:
     # O(log(n)) time || O(log(n)) space
     def sum_range(self, left: int, right: int) -> int:
         return self.tree.sum_range(left, right)
+
+
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.tree = [0] * 4 * self.n
+        self.build_tree(0, self.n - 1, arr, 0)
+
+    def build_tree(self, low, high, arr, idx):
+        if low == high:
+            self.tree[idx] = arr[low]
+            return
+
+        mid = low + (high - low) // 2
+        self.build_tree(low, mid, arr, 2 * idx + 1)
+        self.build_tree(mid + 1, high, arr, 2 * idx + 2)
+        self.tree[idx] = self.tree[2 * idx + 1] + self.tree[2 * idx + 2]
+
+    def sum_range(self, low, high, idx, left, right):
+        if right < low or left > high:
+            return 0
+
+        if left <= low and right >= high:
+            return self.tree[idx]
+
+        mid = low + (high - low) // 2
+
+        return self.sum_range(low, mid, 2 * idx + 1, left, right) \
+            + self.sum_range(mid + 1, high, 2 * idx + 2, left, right)
+
+    def update(self, low, high, idx, pos, val):
+        if pos < low or pos > high:
+            return
+
+        if low == high:
+            self.tree[idx] = val
+            return
+
+        mid = low + (high - low) // 2
+        if pos <= mid:
+            self.update(low, mid, 2 * idx + 1, pos, val)
+        else:
+            self.update(mid + 1, high, 2 * idx + 2, pos, val)
+
+        self.tree[idx] = self.tree[2 * idx + 1] + self.tree[2 * idx + 2]
+
+
+class NumArraySegmentTree:
+
+    def __init__(self, nums: List[int]):
+        self.tree = SegmentTree(nums)
+
+    # O(log(n)) time || O(n) space
+    def update(self, index: int, val: int) -> None:
+        self.tree.update(0, self.tree.n - 1, 0, index, val)
+
+    # O(log(n)) time || O(n) space
+    def sum_range(self, left: int, right: int) -> int:
+        return self.tree.sum_range(0, self.tree.n - 1, 0, left, right)
--- dynamic_programming/test/test_sum_range_mutable.py
+++ dynamic_programming/test/test_sum_range_mutable.py
@@ -1,14 +1,19 @@
 import unittest
 
-from dynamic_programming.RangeSumQueryMutable import NumArray
+from dynamic_programming.RangeSumQueryMutable import NumArrayBIT, NumArraySegmentTree
 
 
 class MyTestCase(unittest.TestCase):
     def test_sum_range_mutable(self):
-        num_array = NumArray([1, 3, 5])
-        self.assertEqual(9, num_array.sum_range(0, 2))
-        num_array.update(1, 2)
-        self.assertEqual(8, num_array.sum_range(0, 2))
+        num_array_bit = NumArrayBIT([1, 3, 5])
+        self.assertEqual(9, num_array_bit.sum_range(0, 2))
+        num_array_bit.update(1, 2)
+        self.assertEqual(8, num_array_bit.sum_range(0, 2))
+
+        num_array_seg_tree = NumArraySegmentTree([1, 3, 5])
+        self.assertEqual(9, num_array_seg_tree.sum_range(0, 2))
+        num_array_seg_tree.update(1, 2)
+        self.assertEqual(8, num_array_seg_tree.sum_range(0, 2))
 
 
 if __name__ == '__main__':
