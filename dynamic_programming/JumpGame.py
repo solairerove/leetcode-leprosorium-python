682fbfa4d4117161c135af081f06095c7020ba98
solairerove <solairerove@gmail.com>
2023-04-02 21:40:09 +0800

    feature: jump game remove top down tle solution

--- dynamic_programming/JumpGame.py
+++ dynamic_programming/JumpGame.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from typing import List
 
 
@@ -11,24 +10,3 @@ def can_jump_greedy(self, nums: List[int]) -> bool:
         high = max(high, i + n)
 
     return True
-
-
-# O(n) time || O(n) space TLE
-def can_jump_top_down(self, nums: List[int]) -> bool:
-    n = len(nums)
-
-    @lru_cache(n)
-    def dp(i):
-        if i == n - 1:
-            return True
-
-        if nums[i] == 0:
-            return False
-
-        for j in range(i + 1, min(i + nums[i], n - 1) + 1):
-            if dp(j):
-                return True
-
-        return False
-
-    return dp(0)
--- dynamic_programming/test/test_can_jump.py
+++ dynamic_programming/test/test_can_jump.py
@@ -1,16 +1,23 @@
 import unittest
 
-from dynamic_programming.JumpGame import can_jump_greedy, can_jump_top_down
+from dynamic_programming.JumpGame import can_jump_greedy
 
 
 class MyTestCase(unittest.TestCase):
     def test_can_jump(self):
         self.assertEqual(True, can_jump_greedy(self, [2, 3, 1, 1, 4]))
-        self.assertEqual(True, can_jump_top_down(self, [2, 3, 1, 1, 4]))
 
     def test_can_jump_1(self):
         self.assertEqual(False, can_jump_greedy(self, [3, 2, 1, 0, 4]))
-        self.assertEqual(False, can_jump_top_down(self, [3, 2, 1, 0, 4]))
+
+    def test_can_jump_2(self):
+        self.assertEqual(True, can_jump_greedy(self, [2, 0]))
+
+    def test_can_jump_3(self):
+        self.assertEqual(True, can_jump_greedy(self, [2, 5, 0, 0]))
+
+    def test_can_jump_4(self):
+        self.assertEqual(True, can_jump_greedy(self, [3, 0, 8, 2, 0, 0, 1]))
 
 
 if __name__ == '__main__':
