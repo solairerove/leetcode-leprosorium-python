0c18a5126169dff717cc734dbe7e935c0573e1a4
solairerove <solairerove@gmail.com>
2023-04-03 20:15:36 +0800

    feature: equal sum arrays with minimum number of operations revisit

--- greedy/EqualSumArraysWithMinimumNumberOfOperations.py
+++ greedy/EqualSumArraysWithMinimumNumberOfOperations.py
@@ -1,22 +1,17 @@
+import collections
 from typing import List
 
 
-# O(n) time || O(1) space
+# O(m + n) time || O(1) space
 def min_operations(self, nums1: List[int], nums2: List[int]) -> int:
     if len(nums1) * 6 < len(nums2) or len(nums2) * 6 < len(nums1):
         return -1
 
-    sum1, sum2 = sum(nums1), sum(nums2)
+    sum1, sum2 = map(sum, (nums1, nums2))
     if sum1 > sum2:
         return min_operations(self, nums2, nums1)
 
-    cnt = [0] * 6
-    for n in nums1:
-        cnt[6 - n] += 1  # count increases
-
-    for n in nums2:
-        cnt[n - 1] += 1  # count decreases
-
+    cnt = collections.Counter([6 - n for n in nums1] + [n - 1 for n in nums2])
     i, res = 5, 0
     while sum2 > sum1:
         while cnt[i] == 0:
