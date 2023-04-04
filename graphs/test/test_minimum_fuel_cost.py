465290dd6369b5ea4e0487c6bb1f81cd8eca8a93
solairerove <solairerove@gmail.com>
2023-04-04 13:58:00 +0800

    feature: minimum fuel cost to report to the capital using dfs

---
+++ graphs/test/test_minimum_fuel_cost.py
@@ -0,0 +1,18 @@
+import unittest
+
+from graphs.MinimumFuelCostToReportToTheCapital import minimum_fuel_cost_dfs
+
+
+class MyTestCase(unittest.TestCase):
+    def test_minimum_fuel_cost(self):
+        self.assertEqual(3, minimum_fuel_cost_dfs(self, [[0, 1], [0, 2], [0, 3]], 5))
+
+    def test_minimum_fuel_cost_1(self):
+        self.assertEqual(7, minimum_fuel_cost_dfs(self, [[3, 1], [3, 2], [1, 0], [0, 4], [0, 5], [4, 6]], 2))
+
+    def test_minimum_fuel_cost_2(self):
+        self.assertEqual(0, minimum_fuel_cost_dfs(self, [], 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
