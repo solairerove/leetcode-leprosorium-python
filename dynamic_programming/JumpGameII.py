1e1e3d5a1fecfe29abdd08e8a86d2ce97a3f7673
solairerove <solairerove@gmail.com>
2023-04-04 11:11:57 +0800

    feature: jump game II bottom up

--- dynamic_programming/JumpGameII.py
+++ dynamic_programming/JumpGameII.py
@@ -14,3 +14,18 @@ def jump(self, nums: List[int]) -> int:
             jumps, steps = jumps + 1, reach - i
 
     return jumps + 1
+
+
+# O(n) time || O(n) space
+def jump_bottom_up(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+        return 0
+
+    dp = [0] * len(nums)
+    j = 0
+    for i in range(1, len(nums)):
+        while j + nums[j] < i:
+            j += 1
+        dp[i] = dp[j] + 1
+
+    return dp[-1]
--- dynamic_programming/test/test_jump.py
+++ dynamic_programming/test/test_jump.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.JumpGameII import jump
+from dynamic_programming.JumpGameII import jump, jump_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_jump(self):
         self.assertEqual(2, jump(self, [2, 3, 1, 1, 4]))
+        self.assertEqual(2, jump_bottom_up(self, [2, 3, 1, 1, 4]))
 
     def test_jump_1(self):
         self.assertEqual(2, jump(self, [2, 3, 0, 1, 4]))
+        self.assertEqual(2, jump_bottom_up(self, [2, 3, 0, 1, 4]))
 
 
 if __name__ == '__main__':
