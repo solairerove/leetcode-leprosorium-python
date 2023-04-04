53925a6be3372b3a25c08232e081da2665106a6e
solairerove <solairerove@gmail.com>
2023-04-04 14:04:28 +0800

    feature: minimum fuel cost to report to the capital using bfs

--- graphs/MinimumFuelCostToReportToTheCapital.py
+++ graphs/MinimumFuelCostToReportToTheCapital.py
@@ -1,3 +1,4 @@
+import collections
 import math
 from typing import List
 
@@ -26,3 +27,39 @@ def minimum_fuel_cost_dfs(self, roads: List[List[int]], seats: int) -> int:
     dfs(0, -1, adj, seats)
 
     return self.fuel
+
+
+# O(n + e) time || O(n) space
+def minimum_fuel_cost_bfs(self, roads: List[List[int]], seats: int) -> int:
+    n = len(roads) + 1
+    adj = [[] for _ in range(n)]
+    degree = [0] * n
+
+    for road in roads:
+        adj[road[0]].append(road[1])
+        adj[road[1]].append(road[0])
+        degree[road[0]] += 1
+        degree[road[1]] += 1
+
+    return bfs(self, n, adj, degree, seats)
+
+
+def bfs(self, n, adj, degree, seats):
+    dq = collections.deque()
+    for i in range(1, n):
+        if degree[i] == 1:
+            dq.append(i)
+
+    representatives = [1] * n
+    fuel = 0
+
+    while dq:
+        node = dq.popleft()
+        fuel += math.ceil(representatives[node] / seats)
+        for neighbor in adj[node]:
+            degree[neighbor] -= 1
+            representatives[neighbor] += representatives[node]
+            if degree[neighbor] == 1 and neighbor != 0:
+                dq.append(neighbor)
+
+    return fuel
--- graphs/test/test_minimum_fuel_cost.py
+++ graphs/test/test_minimum_fuel_cost.py
@@ -1,17 +1,20 @@
 import unittest
 
-from graphs.MinimumFuelCostToReportToTheCapital import minimum_fuel_cost_dfs
+from graphs.MinimumFuelCostToReportToTheCapital import minimum_fuel_cost_dfs, minimum_fuel_cost_bfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_minimum_fuel_cost(self):
         self.assertEqual(3, minimum_fuel_cost_dfs(self, [[0, 1], [0, 2], [0, 3]], 5))
+        self.assertEqual(3, minimum_fuel_cost_bfs(self, [[0, 1], [0, 2], [0, 3]], 5))
 
     def test_minimum_fuel_cost_1(self):
         self.assertEqual(7, minimum_fuel_cost_dfs(self, [[3, 1], [3, 2], [1, 0], [0, 4], [0, 5], [4, 6]], 2))
+        self.assertEqual(7, minimum_fuel_cost_bfs(self, [[3, 1], [3, 2], [1, 0], [0, 4], [0, 5], [4, 6]], 2))
 
     def test_minimum_fuel_cost_2(self):
         self.assertEqual(0, minimum_fuel_cost_dfs(self, [], 1))
+        self.assertEqual(0, minimum_fuel_cost_bfs(self, [], 1))
 
 
 if __name__ == '__main__':
