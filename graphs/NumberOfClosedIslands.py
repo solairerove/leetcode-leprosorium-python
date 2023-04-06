3899c1618bac3c431a29482436258e52de56d90a
solairerove <solairerove@gmail.com>
2023-04-06 11:37:07 +0800

    feature: number of closed islands union

--- graphs/NumberOfClosedIslands.py
+++ graphs/NumberOfClosedIslands.py
@@ -37,3 +37,31 @@ def closed_island_bfs(self, grid: List[List[int]]) -> int:
     return sum(
         bfs(i, j) for i, row in enumerate(grid) for j, cell in enumerate(row) if not cell and (i, j) not in visit
     )
+
+
+# O(n * m) time || O(n) space
+def closed_island_union(self, grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0])
+    id = list(range(m * n))
+
+    def union(x, y):
+        def find(x):
+            while x != id[x]:
+                x = id[x]
+            return x
+
+        root_x, root_y = find(x), find(y)
+        if root_x != root_y:
+            if root_y // n in (0, m - 1) or root_y % n in (0, n - 1):
+                id[root_x] = root_y
+            else:
+                id[root_y] = root_x
+
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            if not grid[i][j]:
+                for r, c in (i, j + 1), (i + 1, j), (i, j - 1), (i - 1, j):
+                    if not grid[r][c]:
+                        union(i * n + j, r * n + c)
+
+    return sum(not grid[i][j] and id[i * n + j] == i * n + j for i in range(1, m - 1) for j in range(1, n - 1))
--- graphs/test/test_closed_island.py
+++ graphs/test/test_closed_island.py
@@ -1,6 +1,6 @@
 import unittest
 
-from graphs.NumberOfClosedIslands import closed_island_dfs, closed_island_bfs
+from graphs.NumberOfClosedIslands import closed_island_dfs, closed_island_bfs, closed_island_union
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +15,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(2, closed_island_dfs(self, [row[:] for row in grid]))
         self.assertEqual(2, closed_island_bfs(self, [row[:] for row in grid]))
+        self.assertEqual(2, closed_island_union(self, [row[:] for row in grid]))
 
     def test_closed_island_1(self):
         grid = [
@@ -24,6 +25,7 @@ class MyTestCase(unittest.TestCase):
         ]
         self.assertEqual(1, closed_island_dfs(self, [row[:] for row in grid]))
         self.assertEqual(1, closed_island_bfs(self, [row[:] for row in grid]))
+        self.assertEqual(1, closed_island_union(self, [row[:] for row in grid]))
 
     def test_closed_island_2(self):
         grid = [
@@ -37,6 +39,25 @@ class MyTestCase(unittest.TestCase):
         ]
         self.assertEqual(2, closed_island_dfs(self, [row[:] for row in grid]))
         self.assertEqual(2, closed_island_bfs(self, [row[:] for row in grid]))
+        self.assertEqual(2, closed_island_union(self, [row[:] for row in grid]))
+
+    def test_closed_island_3(self):
+        grid = [
+            [0, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+            [1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+            [1, 0, 1, 1, 1, 0, 0, 1, 1, 0],
+            [0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+            [1, 1, 1, 0, 1, 1, 0, 1, 1, 0]
+        ]
+
+        self.assertEqual(5, closed_island_dfs(self, [row[:] for row in grid]))
+        self.assertEqual(5, closed_island_bfs(self, [row[:] for row in grid]))
+        self.assertEqual(5, closed_island_union(self, [row[:] for row in grid]))
 
 
 if __name__ == '__main__':
