2549a3aa93e05c8615c8b5a12d719fd42743bf8f
solairerove <solairerove@gmail.com>
2023-04-10 20:47:26 +0800

    feature: tribonacci number top down revisit

--- dynamic_programming/NthTribonacciNumber.py
+++ dynamic_programming/NthTribonacciNumber.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 # O(n) time || O(1) space
 def tribonacci(self, n: int) -> int:
     a, b, c = 1, 0, 0
@@ -14,3 +17,18 @@ def tribonacci_from_smart_guy(self, n: int) -> int:
         dp[i % 3] = sum(dp)
 
     return dp[n % 3]
+
+
+# O(n) time || O(n) space
+def tribonacci_top_down(self, n: int) -> int:
+    @lru_cache(None)
+    def dp(i):
+        if i == 0:
+            return 0
+
+        if i == 1 or i == 2:
+            return 1
+
+        return dp(i - 1) + dp(i - 2) + dp(i - 3)
+
+    return dp(n)
--- dynamic_programming/test/test_tribonacci.py
+++ dynamic_programming/test/test_tribonacci.py
@@ -1,16 +1,18 @@
 import unittest
 
-from dynamic_programming.NthTribonacciNumber import tribonacci, tribonacci_from_smart_guy
+from dynamic_programming.NthTribonacciNumber import tribonacci, tribonacci_from_smart_guy, tribonacci_top_down
 
 
 class MyTestCase(unittest.TestCase):
     def test_tribonacci(self):
         self.assertEqual(4, tribonacci(self, 4))
         self.assertEqual(4, tribonacci_from_smart_guy(self, 4))
+        self.assertEqual(4, tribonacci_top_down(self, 4))
 
     def test_tribonacci_1(self):
         self.assertEqual(1389537, tribonacci(self, 25))
         self.assertEqual(1389537, tribonacci_from_smart_guy(self, 25))
+        self.assertEqual(1389537, tribonacci_top_down(self, 25))
 
 
 if __name__ == '__main__':
