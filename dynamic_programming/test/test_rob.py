e12e938527943b19f433ca6bd94ea82517086ae9
solairerove <solairerove@gmail.com>
2023-04-12 13:11:11 +0800

    feature: house robber top down revisit

--- dynamic_programming/test/test_rob.py
+++ dynamic_programming/test/test_rob.py
@@ -1,17 +1,17 @@
 import unittest
 
-from dynamic_programming.HouseRobber import rob, rob_rec, rob_bottom_up
+from dynamic_programming.HouseRobber import rob, rob_top_down, rob_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_rob(self):
         self.assertEqual(4, rob(self, [1, 2, 3, 1]))
-        self.assertEqual(4, rob_rec(self, [1, 2, 3, 1]))
+        self.assertEqual(4, rob_top_down(self, [1, 2, 3, 1]))
         self.assertEqual(4, rob_bottom_up(self, [1, 2, 3, 1]))
 
     def test_rob_1(self):
         self.assertEqual(12, rob(self, [2, 7, 9, 3, 1]))
-        self.assertEqual(12, rob_rec(self, [2, 7, 9, 3, 1]))
+        self.assertEqual(12, rob_top_down(self, [2, 7, 9, 3, 1]))
         self.assertEqual(12, rob_bottom_up(self, [2, 7, 9, 3, 1]))
 
 
