f2e4fb0e4d33038dffdf228ba822eabf98021b0e
solairerove <solairerove@gmail.com>
2023-04-12 16:12:26 +0800

    feature: delete and earn top down revisit

--- dynamic_programming/DeleteAndEarn.py
+++ dynamic_programming/DeleteAndEarn.py
@@ -1,4 +1,6 @@
+import collections
 from collections import Counter
+from functools import cache
 from typing import List
 
 
@@ -10,3 +12,24 @@ def delete_and_earn(self, nums: List[int]) -> int:
         prev, curr = curr, max(prev + value * points[value], curr)
 
     return curr
+
+
+# O(n + k) time || O(n + k) space
+def delete_and_earn_top_down(self, nums: List[int]) -> int:
+    points = collections.defaultdict(int)
+    max_number = 0
+    for num in nums:
+        points[num] += num
+        max_number = max(max_number, num)
+
+    @cache
+    def dp(i):
+        if i == 0:
+            return 0
+
+        if i == 1:
+            return points[1]
+
+        return max(dp(i - 1), dp(i - 2) + points[i])
+
+    return dp(max_number)
--- dynamic_programming/test/test_delete_and_earn.py
+++ dynamic_programming/test/test_delete_and_earn.py
@@ -1,17 +1,20 @@
 import unittest
 
-from dynamic_programming.DeleteAndEarn import delete_and_earn
+from dynamic_programming.DeleteAndEarn import delete_and_earn, delete_and_earn_top_down
 
 
 class MyTestCase(unittest.TestCase):
     def test_delete_and_earn(self):
         self.assertEqual(6, delete_and_earn(self, [3, 4, 2]))
+        self.assertEqual(6, delete_and_earn_top_down(self, [3, 4, 2]))
 
     def test_delete_and_earn_1(self):
         self.assertEqual(9, delete_and_earn(self, [2, 2, 3, 3, 3, 4]))
+        self.assertEqual(9, delete_and_earn_top_down(self, [2, 2, 3, 3, 3, 4]))
 
     def test_delete_and_earn_2(self):
         self.assertEqual(18, delete_and_earn(self, [1, 1, 1, 2, 4, 5, 5, 5, 6]))
+        self.assertEqual(18, delete_and_earn_top_down(self, [1, 1, 1, 2, 4, 5, 5, 5, 6]))
 
 
 if __name__ == '__main__':
