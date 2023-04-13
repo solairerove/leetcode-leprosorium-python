7ad1d92290470d46e7a410050a4b5c9a576ad0b4
solairerove <solairerove@gmail.com>
2023-04-13 15:34:11 +0800

    feature: validate stack sequence two pointers

--- stack/ValidateStackSequences.py
+++ stack/ValidateStackSequences.py
@@ -1,6 +1,18 @@
 from typing import List
 
 
+# O(n) time || O(1) space
+def validate_stack_sequences(self, pushed: List[int], popped: List[int]) -> bool:
+    i = j = 0
+    for e in pushed:
+        pushed[i] = e
+        while i >= 0 and pushed[i] == popped[j]:
+            i, j = i - 1, j + 1
+        i += 1
+
+    return i == 0
+
+
 # O(n) time || O(n) space
 def validate_stack_sequences_using_stack(self, pushed: List[int], popped: List[int]) -> bool:
     stack = []
--- stack/test/test_validate_stack_sequences.py
+++ stack/test/test_validate_stack_sequences.py
@@ -1,13 +1,15 @@
 import unittest
 
-from stack.ValidateStackSequences import validate_stack_sequences_using_stack
+from stack.ValidateStackSequences import validate_stack_sequences_using_stack, validate_stack_sequences
 
 
 class MyTestCase(unittest.TestCase):
     def test_validate_stack_sequences(self):
+        self.assertEqual(True, validate_stack_sequences(self, [1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
         self.assertEqual(True, validate_stack_sequences_using_stack(self, [1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
 
     def test_validate_stack_sequences_1(self):
+        self.assertEqual(False, validate_stack_sequences(self, [1, 2, 3, 4, 5], [4, 3, 5, 1, 2]))
         self.assertEqual(False, validate_stack_sequences_using_stack(self, [1, 2, 3, 4, 5], [4, 3, 5, 1, 2]))
 
 
