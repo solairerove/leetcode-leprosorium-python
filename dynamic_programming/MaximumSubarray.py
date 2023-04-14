887c79f4c2816f1e25ee53d11389b062f57be75f
solairerove <solairerove@gmail.com>
2023-04-14 13:54:52 +0800

    feature: maximum subarray kadane and top down

--- dynamic_programming/MaximumSubarray.py
+++ dynamic_programming/MaximumSubarray.py
@@ -1,19 +1,32 @@
+from functools import lru_cache
 from typing import List
 
 
-# O(n) time || O(n) space
+# O(n) time || O(1) space
 def max_sub_array(self, nums: List[int]) -> int:
+    for i in range(1, len(nums)):
+        nums[i] = max(nums[i], nums[i] + nums[i - 1])
+
+    return max(nums)
+
+
+# O(n) time || O(n) space
+def max_sub_array_top_down(self, nums: List[int]) -> int:
+    @lru_cache(None)
+    def dp(i, should_pick):
+        if i >= len(nums):
+            return 0 if should_pick else float('-inf')
+
+        return max(nums[i] + dp(i + 1, True), 0 if should_pick else dp(i + 1, False))
+
+    return dp(0, False)
+
+
+# O(n) time || O(n) space
+def max_sub_array_bottom_up(self, nums: List[int]) -> int:
     dp = [0] * (len(nums))
     dp[0] = nums[0]
     for i in range(1, len(nums)):
         dp[i] = max(nums[i], dp[i - 1] + nums[i])
 
     return max(dp)
-
-
-# O(n) time || O(1) space
-def max_sub_array_const(self, nums: List[int]) -> int:
-    for i in range(1, len(nums)):
-        nums[i] = max(nums[i], nums[i] + nums[i - 1])
-
-    return max(nums)
--- dynamic_programming/test/test_max_sub_array.py
+++ dynamic_programming/test/test_max_sub_array.py
@@ -1,28 +1,33 @@
 import unittest
 
-from dynamic_programming.MaximumSubarray import max_sub_array, max_sub_array_const
+from dynamic_programming.MaximumSubarray import max_sub_array, max_sub_array_bottom_up, max_sub_array_top_down
 
 
 class MyTestCase(unittest.TestCase):
     def test_max_sub_array(self):
         self.assertEqual(6, max_sub_array(self, [-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-        self.assertEqual(6, max_sub_array_const(self, [-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+        self.assertEqual(6, max_sub_array_top_down(self, [-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+        self.assertEqual(6, max_sub_array_bottom_up(self, [-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 
     def test_max_sub_array_1(self):
         self.assertEqual(1, max_sub_array(self, [1]))
-        self.assertEqual(1, max_sub_array_const(self, [1]))
+        self.assertEqual(1, max_sub_array_top_down(self, [1]))
+        self.assertEqual(1, max_sub_array_bottom_up(self, [1]))
 
     def test_max_sub_array_2(self):
         self.assertEqual(23, max_sub_array(self, [5, 4, -1, 7, 8]))
-        self.assertEqual(23, max_sub_array_const(self, [5, 4, -1, 7, 8]))
+        self.assertEqual(23, max_sub_array_top_down(self, [5, 4, -1, 7, 8]))
+        self.assertEqual(23, max_sub_array_bottom_up(self, [5, 4, -1, 7, 8]))
 
     def test_max_sub_array_3(self):
         self.assertEqual(-1, max_sub_array(self, [-1]))
-        self.assertEqual(-1, max_sub_array_const(self, [-1]))
+        self.assertEqual(-1, max_sub_array_top_down(self, [-1]))
+        self.assertEqual(-1, max_sub_array_bottom_up(self, [-1]))
 
     def test_max_sub_array_4(self):
         self.assertEqual(-1, max_sub_array(self, [-1, -2]))
-        self.assertEqual(-1, max_sub_array_const(self, [-1, -2]))
+        self.assertEqual(-1, max_sub_array_top_down(self, [-1, -2]))
+        self.assertEqual(-1, max_sub_array_bottom_up(self, [-1, -2]))
 
 
 if __name__ == '__main__':
