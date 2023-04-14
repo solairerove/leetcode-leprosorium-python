7229adc119e1dc4afd1c24841acbd882f1708659
solairerove <solairerove@gmail.com>
2023-04-14 13:15:25 +0800

    feature: longest palindromic subsequence bottom up space optimized

--- dynamic_programming/LongestPalindromicSubsequence.py
+++ dynamic_programming/LongestPalindromicSubsequence.py
@@ -32,3 +32,19 @@ def longest_palindrome_subseq_bottom_up(self, s: str) -> int:
                 dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
 
     return dp[0][n - 1]
+
+
+# O(n^2) time || O(n) space
+def longest_palindrome_subseq_space_optimized(self, s: str) -> int:
+    n = len(s)
+    dp, dp_prev = [0] * n, [0] * n
+    for i in range(n - 1, -1, -1):
+        dp[i] = 1
+        for j in range(i + 1, n):
+            if s[i] == s[j]:
+                dp[j] = dp_prev[j - 1] + 2
+            else:
+                dp[j] = max(dp_prev[j], dp[j - 1])
+        dp, dp_prev = dp_prev, dp
+
+    return dp_prev[n - 1]
--- dynamic_programming/test/test_longest_palindrome_subseq.py
+++ dynamic_programming/test/test_longest_palindrome_subseq.py
@@ -1,17 +1,19 @@
 import unittest
 
 from dynamic_programming.LongestPalindromicSubsequence import longest_palindrome_subseq_top_down, \
-    longest_palindrome_subseq_bottom_up
+    longest_palindrome_subseq_bottom_up, longest_palindrome_subseq_space_optimized
 
 
 class MyTestCase(unittest.TestCase):
     def test_longest_palindrome_subseq(self):
         self.assertEqual(4, longest_palindrome_subseq_top_down(self, "bbbab"))
         self.assertEqual(4, longest_palindrome_subseq_bottom_up(self, "bbbab"))
+        self.assertEqual(4, longest_palindrome_subseq_space_optimized(self, "bbbab"))
 
     def test_longest_palindrome_subseq_1(self):
         self.assertEqual(2, longest_palindrome_subseq_top_down(self, "cbbd"))
         self.assertEqual(2, longest_palindrome_subseq_bottom_up(self, "cbbd"))
+        self.assertEqual(2, longest_palindrome_subseq_space_optimized(self, "cbbd"))
 
 
 if __name__ == '__main__':
