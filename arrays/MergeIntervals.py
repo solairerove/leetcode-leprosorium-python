02b9c5d81f3c42a27e71e2ecd175f0cc7a56971f
solairerove <solairerove@gmail.com>
2023-04-17 17:11:22 +0800

    feature: merge intervals revisit

--- arrays/MergeIntervals.py
+++ arrays/MergeIntervals.py
@@ -9,7 +9,6 @@ def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not merged or merged[-1][1] < interval[0]:
             merged.append(interval)
         else:
-            # merged[-1][1] = interval[1]
             merged[-1][1] = max(merged[-1][1], interval[1])
 
     return merged
