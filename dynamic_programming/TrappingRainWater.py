7d9531b6711f014b981eaf9c49f6f54fa56d4b33
solairerove <solairerove@gmail.com>
2023-04-18 18:49:02 +0800

    feature: trapping rain water two pointers solution

--- dynamic_programming/TrappingRainWater.py
+++ dynamic_programming/TrappingRainWater.py
@@ -19,3 +19,29 @@ def trap(self, height: List[int]) -> int:
             res += level - height[i]
 
     return res
+
+
+# O(n) time || O(1) space
+def trap_two_pointers(self, height: List[int]) -> int:
+    if len(height) <= 2:
+        return 0
+
+    n = len(height)
+    max_low, max_high = height[0], height[-1]
+    low, high = 1, n - 2
+    res = 0
+    while low <= high:
+        if max_low < max_high:
+            if height[low] > max_low:
+                max_low = height[low]
+            else:
+                res += max_low - height[low]
+            low += 1
+        else:
+            if height[high] > max_high:
+                max_high = height[high]
+            else:
+                res += max_high - height[high]
+            high -= 1
+
+    return res
--- dynamic_programming/test/test_trap.py
+++ dynamic_programming/test/test_trap.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.TrappingRainWater import trap
+from dynamic_programming.TrappingRainWater import trap, trap_two_pointers
 
 
 class MyTestCase(unittest.TestCase):
     def test_trap(self):
         self.assertEqual(6, trap(self, [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+        self.assertEqual(6, trap_two_pointers(self, [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 
     def test_trap_1(self):
         self.assertEqual(9, trap(self, [4, 2, 0, 3, 2, 5]))
+        self.assertEqual(9, trap_two_pointers(self, [4, 2, 0, 3, 2, 5]))
 
 
 if __name__ == '__main__':
