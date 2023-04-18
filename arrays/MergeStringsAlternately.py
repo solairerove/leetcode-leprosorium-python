8d8f0ff2052cb4caf04095f3e6927d9b9a4211cb
solairerove <solairerove@gmail.com>
2023-04-18 15:13:18 +0800

    feature: merge string alternate one liner using zip

--- arrays/MergeStringsAlternately.py
+++ arrays/MergeStringsAlternately.py
@@ -1,4 +1,7 @@
-# O(n) time || O(1) space
+from itertools import zip_longest
+
+
+# O(n + m) time || O(n + m) space
 def merge_alternately(self, word1: str, word2: str) -> str:
     idx1, idx2, res = 0, 0, ""
     while idx1 < len(word1) and idx2 < len(word2):
@@ -15,3 +18,8 @@ def merge_alternately(self, word1: str, word2: str) -> str:
         res += word2[idx2:]
 
     return res
+
+
+# O(n + m) time || O(n + m) space
+def merge_alternately_one_liner(self, word1: str, word2: str) -> str:
+    return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
--- arrays/test/test_merge_alternately.py
+++ arrays/test/test_merge_alternately.py
@@ -1,17 +1,20 @@
 import unittest
 
-from arrays.MergeStringsAlternately import merge_alternately
+from arrays.MergeStringsAlternately import merge_alternately, merge_alternately_one_liner
 
 
 class MyTestCase(unittest.TestCase):
     def test_merge_alternately(self):
         self.assertEqual("apbqcr", merge_alternately(self, "abc", "pqr"))
+        self.assertEqual("apbqcr", merge_alternately_one_liner(self, "abc", "pqr"))
 
     def test_merge_alternately_1(self):
         self.assertEqual("apbqrs", merge_alternately(self, "ab", "pqrs"))
+        self.assertEqual("apbqrs", merge_alternately_one_liner(self, "ab", "pqrs"))
 
     def test_merge_alternately_2(self):
         self.assertEqual("apbqcd", merge_alternately(self, "abcd", "pq"))
+        self.assertEqual("apbqcd", merge_alternately_one_liner(self, "abcd", "pq"))
 
 
 if __name__ == '__main__':
