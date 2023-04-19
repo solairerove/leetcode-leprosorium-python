7453de8d54f39643fcd4e0c1f8595d2f7d0b3e7c
solairerove <solairerove@gmail.com>
2023-04-19 12:28:15 +0800

    feature: arithmetic slices revisit

--- dynamic_programming/ArithmeticSlices.py
+++ dynamic_programming/ArithmeticSlices.py
@@ -1,6 +1,19 @@
 from typing import List
 
 
+# O(n) time || O(1) space
+def number_of_arithmetic_slices(self, nums: List[int]) -> int:
+    dp, res = 0, 0
+    for i in range(2, len(nums)):
+        if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+            dp += 1
+            res += dp
+        else:
+            dp = 0
+
+    return res
+
+
 # O(n) time || O(n) space
 def number_of_arithmetic_slices_top_down(self, nums: List[int]) -> int:
     self.res = 0
--- dynamic_programming/test/test_number_of_arithmetic_slices.py
+++ dynamic_programming/test/test_number_of_arithmetic_slices.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.ArithmeticSlices import number_of_arithmetic_slices_top_down
+from dynamic_programming.ArithmeticSlices import number_of_arithmetic_slices_top_down, number_of_arithmetic_slices
 
 
 class MyTestCase(unittest.TestCase):
     def test_number_of_arithmetic_slices(self):
         self.assertEqual(3, number_of_arithmetic_slices_top_down(self, [1, 2, 3, 4]))
+        self.assertEqual(3, number_of_arithmetic_slices(self, [1, 2, 3, 4]))
 
     def test_number_of_arithmetic_slices_1(self):
         self.assertEqual(0, number_of_arithmetic_slices_top_down(self, [1]))
+        self.assertEqual(0, number_of_arithmetic_slices(self, [1]))
 
 
 if __name__ == '__main__':
