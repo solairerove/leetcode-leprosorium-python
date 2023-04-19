e316f589a7f6d72a9bef8033e3d8bffd39be0a21
solairerove <solairerove@gmail.com>
2023-04-19 11:35:11 +0800

    feature: decode ways top down revisit

--- dynamic_programming/DecodeWays.py
+++ dynamic_programming/DecodeWays.py
@@ -21,33 +21,22 @@ def num_decodings(self, s: str) -> int:
 
 
 # O(n) time || O(n) space
-def num_decodings_recursive(self, s: str) -> int:
-    @lru_cache(maxsize=None)
-    def helper(ith: int) -> int:
-        if ith == len(s):
+def num_decodings_top_down(self, s: str) -> int:
+    @lru_cache(None)
+    def dp(i):
+        if i == len(s):
             return 1
 
-        if s[ith] == "0":
+        if s[i] == "0":
             return 0
 
-        if ith == len(s) - 1:
+        if i == len(s) - 1:
             return 1
 
-        res = helper(ith + 1)
-        if 9 < int(s[ith: ith + 2]) < 27:
-            res += helper(ith + 2)
+        res = dp(i + 1)
+        if 9 < int(s[i: i + 2]) < 27:
+            res += dp(i + 2)
 
         return res
 
-    return helper(0)
-
-
-# O(n) time || O(1) space
-def num_decodings_this_is_python(self, s: str) -> int:
-    prev_ways, curr_ways, prev_digit = 0, 1, ''
-    for curr_digit in s:
-        tmp = curr_ways
-        curr_ways = (curr_digit > '0') * curr_ways + (10 <= int(prev_digit + curr_digit) <= 26) * prev_ways
-        prev_ways, prev_digit = tmp, curr_digit
-
-    return curr_ways
+    return dp(0)
--- dynamic_programming/test/test_num_decodings.py
+++ dynamic_programming/test/test_num_decodings.py
@@ -1,33 +1,32 @@
 import unittest
 
-from dynamic_programming.DecodeWays import num_decodings, num_decodings_recursive, num_decodings_this_is_python
+from dynamic_programming.DecodeWays import num_decodings, num_decodings_top_down
 
 
 class MyTestCase(unittest.TestCase):
     def test_num_decodings(self):
         self.assertEqual(2, num_decodings(self, "12"))
-        self.assertEqual(2, num_decodings_recursive(self, "12"))
-        self.assertEqual(2, num_decodings_this_is_python(self, "12"))
+        self.assertEqual(2, num_decodings_top_down(self, "12"))
 
     def test_num_decodings_1(self):
         self.assertEqual(3, num_decodings(self, "226"))
-        self.assertEqual(3, num_decodings_recursive(self, "226"))
-        self.assertEqual(3, num_decodings_this_is_python(self, "226"))
+        self.assertEqual(3, num_decodings_top_down(self, "226"))
 
     def test_num_decodings_2(self):
         self.assertEqual(0, num_decodings(self, "06"))
-        self.assertEqual(0, num_decodings_recursive(self, "06"))
-        self.assertEqual(0, num_decodings_this_is_python(self, "06"))
+        self.assertEqual(0, num_decodings_top_down(self, "06"))
 
     def test_num_decodings_3(self):
         self.assertEqual(5, num_decodings(self, "2125"))
-        self.assertEqual(5, num_decodings_recursive(self, "2125"))
-        self.assertEqual(5, num_decodings_this_is_python(self, "2125"))
+        self.assertEqual(5, num_decodings_top_down(self, "2125"))
 
     def test_num_decodings_4(self):
         self.assertEqual(2, num_decodings(self, "11106"))
-        self.assertEqual(2, num_decodings_recursive(self, "11106"))
-        self.assertEqual(2, num_decodings_this_is_python(self, "11106"))
+        self.assertEqual(2, num_decodings_top_down(self, "11106"))
+
+    def test_num_decodings_5(self):
+        self.assertEqual(0, num_decodings(self, "0"))
+        self.assertEqual(0, num_decodings_top_down(self, "0"))
 
 
 if __name__ == '__main__':
