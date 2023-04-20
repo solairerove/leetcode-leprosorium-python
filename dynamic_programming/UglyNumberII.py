2261274b0eea8e4e2189e17512a286a84472600a
solairerove <solairerove@gmail.com>
2023-04-20 15:04:56 +0800

    feature: ugly number ii revisit

--- dynamic_programming/UglyNumberII.py
+++ dynamic_programming/UglyNumberII.py
@@ -1,4 +1,28 @@
 # O(n) time || O(n) space
+def nth_ugly_number(self, n: int) -> int:
+    res = [1]
+    prod_2 = prod_3 = prod_5 = 0
+    a, b, c = 2, 3, 5
+    for i in range(1, n):
+        m = min(a, b, c)
+        res.append(m)
+
+        if m == a:
+            prod_2 += 1
+            a = res[prod_2] * 2
+
+        if m == b:
+            prod_3 += 1
+            b = res[prod_3] * 3
+
+        if m == c:
+            prod_5 += 1
+            c = res[prod_5] * 5
+
+    return res[-1]
+
+
+# O(n) time || O(n) space
 def nth_ugly_number_pre_calculated(self, n: int) -> int:
     ugly = sorted(2 ** a * 3 ** b * 5 ** c
                   for a in range(32) for b in range(20) for c in range(14))
--- dynamic_programming/test/test_nth_ugly_number.py
+++ dynamic_programming/test/test_nth_ugly_number.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.UglyNumberII import nth_ugly_number_pre_calculated
+from dynamic_programming.UglyNumberII import nth_ugly_number_pre_calculated, nth_ugly_number
 
 
 class MyTestCase(unittest.TestCase):
     def test_nth_ugly_number(self):
         self.assertEqual(12, nth_ugly_number_pre_calculated(self, 10))
+        self.assertEqual(12, nth_ugly_number(self, 10))
 
     def test_nth_ugly_number_1(self):
         self.assertEqual(1, nth_ugly_number_pre_calculated(self, 1))
+        self.assertEqual(1, nth_ugly_number(self, 1))
 
 
 if __name__ == '__main__':
