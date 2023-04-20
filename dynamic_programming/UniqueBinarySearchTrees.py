154bfbd1ac54387542e7fdb3828c5976d050e578
solairerove <solairerove@gmail.com>
2023-04-20 12:31:04 +0800

    feature: unique binary search trees bottom up

--- dynamic_programming/UniqueBinarySearchTrees.py
+++ dynamic_programming/UniqueBinarySearchTrees.py
@@ -8,3 +8,14 @@ def num_trees_top_down(self, n: int) -> int:
         return 1
 
     return sum(num_trees_top_down(self, i - 1) * num_trees_top_down(self, n - i) for i in range(1, n + 1))
+
+
+# O(n ^ 2) time || O(n) space
+def num_trees_bottom_up(self, n: int) -> int:
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n + 1):
+        for j in range(1, i + 1):
+            dp[i] += dp[j - 1] * dp[i - j]
+
+    return dp[-1]
--- dynamic_programming/test/test_num_trees.py
+++ dynamic_programming/test/test_num_trees.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.UniqueBinarySearchTrees import num_trees_top_down
+from dynamic_programming.UniqueBinarySearchTrees import num_trees_top_down, num_trees_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_num_trees(self):
         self.assertEqual(5, num_trees_top_down(self, 3))
+        self.assertEqual(5, num_trees_bottom_up(self, 3))
 
     def test_num_trees_1(self):
         self.assertEqual(1, num_trees_top_down(self, 1))
+        self.assertEqual(1, num_trees_bottom_up(self, 1))
 
 
 if __name__ == '__main__':
