51db5b6e971b0308961a8f6b1dc028e20312e0c1
solairerove <solairerove@gmail.com>
2023-04-21 13:22:11 +0800

    feature: profitable schemes bottom up

--- dynamic_programming/ProfitableSchemes.py
+++ dynamic_programming/ProfitableSchemes.py
@@ -3,6 +3,18 @@ from typing import List
 
 
 # O(n * m * k) time || O(n * m * k) space
+def profitable_schemes(self, n: int, min_profit: int, group: List[int], profit: List[int]) -> int:
+    dp = [[0] * (n + 1) for i in range(min_profit + 1)]
+    dp[0][0] = 1
+    for p, g in zip(profit, group):
+        for i in range(min_profit, -1, -1):
+            for j in range(n - g, -1, -1):
+                dp[min(min_profit, i + p)][j + g] += dp[i][j]
+
+    return sum(dp[min_profit]) % (10 ** 9 + 7)
+
+
+# O(n * m * k) time || O(n * m * k) space
 def profitable_schemes_top_down(self, n: int, min_profit: int, group: List[int], profit: List[int]) -> int:
     @lru_cache(None)
     def dp(i, p, n):
--- dynamic_programming/test/test_profitable_schemes.py
+++ dynamic_programming/test/test_profitable_schemes.py
@@ -1,13 +1,15 @@
 import unittest
 
-from dynamic_programming.ProfitableSchemes import profitable_schemes_top_down
+from dynamic_programming.ProfitableSchemes import profitable_schemes_top_down, profitable_schemes
 
 
 class MyTestCase(unittest.TestCase):
     def test_profitable_schemes(self):
+        self.assertEqual(2, profitable_schemes(self, 5, 3, [2, 2], [2, 3]))
         self.assertEqual(2, profitable_schemes_top_down(self, 5, 3, [2, 2], [2, 3]))
 
     def test_profitable_schemes_1(self):
+        self.assertEqual(7, profitable_schemes(self, 10, 5, [2, 3, 5], [6, 7, 8]))
         self.assertEqual(7, profitable_schemes_top_down(self, 10, 5, [2, 3, 5], [6, 7, 8]))
 
 
