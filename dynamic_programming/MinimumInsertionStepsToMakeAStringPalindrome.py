9e85a3742694045b5b05954dc400b04720d5cf15
solairerove <solairerove@gmail.com>
2023-04-22 10:30:58 +0800

    feature: min insertion steps to make a string palindrome bottom up

--- dynamic_programming/MinimumInsertionStepsToMakeAStringPalindrome.py
+++ dynamic_programming/MinimumInsertionStepsToMakeAStringPalindrome.py
@@ -14,3 +14,17 @@ def min_insertions_top_down(self, s: str) -> int:
         return 1 + min(dp(low, high - 1), dp(low + 1, high))
 
     return dp(0, len(s) - 1)
+
+
+# O(n^2) time || O(n^2) space
+def min_insertions_bottom_up(self, s: str) -> int:
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            if s[i] == s[j]:
+                dp[i][j] = dp[i + 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i + 1][j])
+
+    return dp[0][n - 1]
--- dynamic_programming/test/test_min_insertions.py
+++ dynamic_programming/test/test_min_insertions.py
@@ -1,17 +1,21 @@
 import unittest
 
-from dynamic_programming.MinimumInsertionStepsToMakeAStringPalindrome import min_insertions_top_down
+from dynamic_programming.MinimumInsertionStepsToMakeAStringPalindrome import min_insertions_top_down, \
+    min_insertions_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_min_insertions(self):
         self.assertEqual(0, min_insertions_top_down(self, "zzazz"))
+        self.assertEqual(0, min_insertions_bottom_up(self, "zzazz"))
 
     def test_min_insertions_1(self):
         self.assertEqual(2, min_insertions_top_down(self, "mbadm"))
+        self.assertEqual(2, min_insertions_bottom_up(self, "mbadm"))
 
     def test_min_insertions_2(self):
         self.assertEqual(5, min_insertions_top_down(self, "leetcode"))
+        self.assertEqual(5, min_insertions_bottom_up(self, "leetcode"))
 
 
 if __name__ == '__main__':
