fbe096571049de1af61ffc31b5e64601b1b02fee
solairerove <solairerove@gmail.com>
2023-04-24 18:07:05 +0800

    feature: unique paths revisit

--- dynamic_programming/UniquePaths.py
+++ dynamic_programming/UniquePaths.py
@@ -11,7 +11,7 @@ def unique_paths_top_down(self, m: int, n: int) -> int:
         if i == 1 and j == 1:
             return 1
 
-        return dp(i - 1, j) + dp(i, j - 1)
+        return dp(i, j - 1) + dp(i - 1, j)
 
     return dp(m, n)
 
