9533bcde7402e4bbc2e683c1037b86ac4e77d4aa
solairerove <solairerove@gmail.com>
2023-04-25 17:11:56 +0800

    feature: maximum square revisit

--- dynamic_programming/MaximalSquare.py
+++ dynamic_programming/MaximalSquare.py
@@ -6,15 +6,13 @@ def maximal_square(self, matrix: List[List[str]]) -> int:
     if not matrix or len(matrix) < 1:
         return 0
 
-    rows, columns = len(matrix), len(matrix[0])
-
-    dp = [[0] * (columns + 1) for _ in range(rows + 1)]
+    m, n = len(matrix), len(matrix[0])
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
     max_so_far = 0
-
-    for r in range(rows):
-        for c in range(columns):
-            if matrix[r][c] == '1':
-                dp[r + 1][c + 1] = min(dp[r][c], dp[r + 1][c], dp[r][c + 1]) + 1
-                max_so_far = max(max_so_far, dp[r + 1][c + 1])
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == '1':
+                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
+                max_so_far = max(max_so_far, dp[i + 1][j + 1])
 
     return max_so_far * max_so_far
