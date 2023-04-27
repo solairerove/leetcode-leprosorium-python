0a8549e54c2f19ba835884213485cf8990b1c14e
solairerove <solairerove@gmail.com>
2023-04-27 18:51:10 +0800

    feature: longest increasing subsequence revisit

--- dynamic_programming/LongestIncreasingSubsequence.py
+++ dynamic_programming/LongestIncreasingSubsequence.py
@@ -3,9 +3,10 @@ from typing import List
 
 
 # O(n^2) time || O(n) space
-def length_of_lis(self, nums: List[int]) -> int:
-    dp = [1] * len(nums)
-    for i in range(1, len(nums)):
+def length_of_lis_bottom_up(self, nums: List[int]) -> int:
+    n = len(nums)
+    dp = [1] * n
+    for i in range(1, n):
         for j in range(i):
             if nums[i] > nums[j]:
                 dp[i] = max(dp[i], dp[j] + 1)
@@ -14,7 +15,7 @@ def length_of_lis(self, nums: List[int]) -> int:
 
 
 # O(n * log(n)) time || O(n) space
-def length_of_lis_bs(self, nums: List[int]) -> int:
+def length_of_lis(self, nums: List[int]) -> int:
     sub = []
     for n in nums:
         i = bisect_left(sub, n)
