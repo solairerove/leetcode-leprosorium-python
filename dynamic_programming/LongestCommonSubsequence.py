771691d1a80c1849349adda9077d0a12a7bfa8de
solairerove <solairerove@gmail.com>
2023-04-28 18:09:00 +0800

    feature: longest common subsequence revisit

--- dynamic_programming/LongestCommonSubsequence.py
+++ dynamic_programming/LongestCommonSubsequence.py
@@ -2,29 +2,29 @@ from functools import lru_cache
 
 
 # O(n * m) time || O(n * m) space
-def longest_common_subsequence(self, s1: str, s2: str) -> int:
-    n, m = len(s1), len(s2)
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-    return dp[-1][-1]
-
-
-# O(n * m) time || O(n * m) space
-def longest_common_subsequence_req(self, s1: str, s2: str) -> int:
+def longest_common_subsequence_top_down(self, s1: str, s2: str) -> int:
     @lru_cache(None)
-    def dp(i: int, j: int) -> int:
+    def dp(i, j):
         if i < 0 or j < 0:
             return 0
 
         if s1[i] == s2[j]:
             return dp(i - 1, j - 1) + 1
 
-        return max(dp(i - 1, j), dp(i, j - 1))
+        return max(dp(i, j - 1), dp(i - 1, j))
 
     return dp(len(s1) - 1, len(s2) - 1)
+
+
+# O(n * m) time || O(n * m) space
+def longest_common_subsequence_bottom_up(self, s1: str, s2: str) -> int:
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+    return dp[-1][-1]
--- dynamic_programming/test/test_longest_common_subsequence.py
+++ dynamic_programming/test/test_longest_common_subsequence.py
@@ -1,28 +1,29 @@
 import unittest
 
-from dynamic_programming.LongestCommonSubsequence import longest_common_subsequence, longest_common_subsequence_req
+from dynamic_programming.LongestCommonSubsequence import longest_common_subsequence_top_down, \
+    longest_common_subsequence_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_longest_common_subsequence(self):
-        self.assertEqual(3, longest_common_subsequence(self, "abcde", "ace"))
-        self.assertEqual(3, longest_common_subsequence_req(self, "abcde", "ace"))
+        self.assertEqual(3, longest_common_subsequence_top_down(self, "abcde", "ace"))
+        self.assertEqual(3, longest_common_subsequence_bottom_up(self, "abcde", "ace"))
 
     def test_longest_common_subsequence_1(self):
-        self.assertEqual(3, longest_common_subsequence(self, "abc", "abc"))
-        self.assertEqual(3, longest_common_subsequence_req(self, "abc", "abc"))
+        self.assertEqual(3, longest_common_subsequence_top_down(self, "abc", "abc"))
+        self.assertEqual(3, longest_common_subsequence_bottom_up(self, "abc", "abc"))
 
     def test_longest_common_subsequence_2(self):
-        self.assertEqual(0, longest_common_subsequence(self, "abc", "def"))
-        self.assertEqual(0, longest_common_subsequence_req(self, "abc", "def"))
+        self.assertEqual(0, longest_common_subsequence_top_down(self, "abc", "def"))
+        self.assertEqual(0, longest_common_subsequence_bottom_up(self, "abc", "def"))
 
     def test_longest_common_subsequence_3(self):
-        self.assertEqual(3, longest_common_subsequence(self, "hish", "fish"))
-        self.assertEqual(3, longest_common_subsequence_req(self, "hish", "fish"))
+        self.assertEqual(3, longest_common_subsequence_top_down(self, "hish", "fish"))
+        self.assertEqual(3, longest_common_subsequence_bottom_up(self, "hish", "fish"))
 
     def test_longest_common_subsequence_4(self):
-        self.assertEqual(2, longest_common_subsequence(self, "hish", "vista"))
-        self.assertEqual(2, longest_common_subsequence_req(self, "hish", "vista"))
+        self.assertEqual(2, longest_common_subsequence_top_down(self, "hish", "vista"))
+        self.assertEqual(2, longest_common_subsequence_bottom_up(self, "hish", "vista"))
 
 
 if __name__ == '__main__':
