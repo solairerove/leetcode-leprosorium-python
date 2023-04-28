7bdfe6d92725b5f45522880c268686fe988dddd5
solairerove <solairerove@gmail.com>
2023-04-28 21:56:09 +0800

    feature: similar string groups using bfs

---
+++ graphs/SimilarStringGroups.py
@@ -0,0 +1,41 @@
+import collections
+from typing import List
+
+
+# O(n^2 * m) time || O(n + e) space
+def num_similar_groups_bfs(self, strs: List[str]) -> int:
+    adj = collections.defaultdict(list)
+    for i in range(len(strs)):
+        for j in range(i + 1, len(strs)):
+            if are_neighbors(self, strs[i], strs[j]):
+                adj[strs[i]].append(strs[j])
+                adj[strs[j]].append(strs[i])
+
+    res = 0
+    visit = set()
+    for i in range(len(strs)):
+        if strs[i] not in visit:
+            visit.add(strs[i])  # ?
+            res += 1
+            bfs(self, strs[i], adj, visit)
+
+    return res
+
+
+def are_neighbors(self, a, b):
+    dif = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            dif += 1
+
+    return dif == 0 or dif == 2
+
+
+def bfs(self, node, adj, visit):
+    dq = collections.deque([node])
+    while dq:
+        node = dq.popleft()
+        for neighbor in adj[node]:
+            if neighbor not in visit:
+                visit.add(neighbor)
+                dq.append(neighbor)
---
+++ graphs/test/test_num_similar_groups.py
@@ -0,0 +1,15 @@
+import unittest
+
+from graphs.SimilarStringGroups import num_similar_groups_bfs
+
+
+class MyTestCase(unittest.TestCase):
+    def test_num_similar_groups(self):
+        self.assertEqual(2, num_similar_groups_bfs(self, ["tars", "rats", "arts", "star"]))
+
+    def test_num_similar_groups_1(self):
+        self.assertEqual(1, num_similar_groups_bfs(self, ["omv", "ovm"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
