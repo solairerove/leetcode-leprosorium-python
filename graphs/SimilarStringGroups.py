dd1518239989ca515618f64a0a559b4d467da917
solairerove <solairerove@gmail.com>
2023-04-29 16:45:55 +0800

    feature: similar string groups using dfs

--- graphs/SimilarStringGroups.py
+++ graphs/SimilarStringGroups.py
@@ -39,3 +39,30 @@ def bfs(self, node, adj, visit):
             if neighbor not in visit:
                 visit.add(neighbor)
                 dq.append(neighbor)
+
+
+# O(n^2 * m) time || O(n + e) space
+def num_similar_groups_dfs(self, strs: List[str]) -> int:
+    adj = collections.defaultdict(list)
+    for i in range(len(strs)):
+        for j in range(i + 1, len(strs)):
+            if are_neighbors(self, strs[i], strs[j]):
+                adj[strs[i]].append(strs[j])
+                adj[strs[j]].append(strs[i])
+
+    res = 0
+    visit = set()
+    for i in range(len(strs)):
+        if strs[i] not in visit:
+            res += 1
+            dfs(self, strs[i], adj, visit)
+
+    return res
+
+
+def dfs(self, node, adj, visit):
+    visit.add(node)
+    for neighbor in adj[node]:
+        if neighbor not in visit:
+            visit.add(neighbor)
+            dfs(self, neighbor, adj, visit)
--- graphs/test/test_num_similar_groups.py
+++ graphs/test/test_num_similar_groups.py
@@ -1,14 +1,16 @@
 import unittest
 
-from graphs.SimilarStringGroups import num_similar_groups_bfs
+from graphs.SimilarStringGroups import num_similar_groups_bfs, num_similar_groups_dfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_num_similar_groups(self):
         self.assertEqual(2, num_similar_groups_bfs(self, ["tars", "rats", "arts", "star"]))
+        self.assertEqual(2, num_similar_groups_dfs(self, ["tars", "rats", "arts", "star"]))
 
     def test_num_similar_groups_1(self):
         self.assertEqual(1, num_similar_groups_bfs(self, ["omv", "ovm"]))
+        self.assertEqual(1, num_similar_groups_dfs(self, ["omv", "ovm"]))
 
 
 if __name__ == '__main__':
