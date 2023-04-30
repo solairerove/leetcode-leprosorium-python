295cdfcd8874e92bd8697069ac69638d7a455da9
solairerove <solairerove@gmail.com>
2023-04-30 18:33:01 +0800

    feature: combination sum iv bottom up

--- dynamic_programming/CombinationSumIV.py
+++ dynamic_programming/CombinationSumIV.py
@@ -17,3 +17,16 @@ def combination_sum_4_top_down(self, nums: List[int], target: int) -> int:
         return res
 
     return dp(target)
+
+
+# O(n * t) time || O(t) space
+def combination_sum_4_bottom_up(self, nums: List[int], target: int) -> int:
+    dp = [0 for _ in range(target + 1)]
+    dp[0] = 1
+
+    for i in range(target + 1):
+        for num in nums:
+            if i - num >= 0:
+                dp[i] += dp[i - num]
+
+    return dp[-1]
--- dynamic_programming/test/test_combination_sum_4.py
+++ dynamic_programming/test/test_combination_sum_4.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.CombinationSumIV import combination_sum_4_top_down
+from dynamic_programming.CombinationSumIV import combination_sum_4_top_down, combination_sum_4_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
     def test_combination_sum_4(self):
         self.assertEqual(7, combination_sum_4_top_down(self, [1, 2, 3], 4))
+        self.assertEqual(7, combination_sum_4_bottom_up(self, [1, 2, 3], 4))
 
     def test_combination_sum_4_1(self):
         self.assertEqual(0, combination_sum_4_top_down(self, [9], 3))
+        self.assertEqual(0, combination_sum_4_bottom_up(self, [9], 3))
 
 
 if __name__ == '__main__':
