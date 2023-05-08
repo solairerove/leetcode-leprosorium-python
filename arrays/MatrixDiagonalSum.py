13704c4e860dadd7c6f72f579077d0b3a38f346e
solairerove <solairerove@gmail.com>
2023-05-08 22:44:30 +0800

    feature: matrix diagonal sum linear approach

--- arrays/MatrixDiagonalSum.py
+++ arrays/MatrixDiagonalSum.py
@@ -1,6 +1,21 @@
 from typing import List
 
 
+# O(n) time || O(1) space
+def diagonal_sum_linear(self, mat: List[List[int]]) -> int:
+    res = 0
+    n = len(mat)
+
+    for i in range(n):
+        res += mat[i][i]
+        res += mat[i][n - 1 - i]
+
+    if n % 2 != 0:
+        res -= mat[n // 2][n // 2]
+
+    return res
+
+
 # O(n ^ 2) time || O(1) space
 def diagonal_sum(self, mat: List[List[int]]) -> int:
     res = 0
--- arrays/test/test_diagonal_sum.py
+++ arrays/test/test_diagonal_sum.py
@@ -1,11 +1,15 @@
 import unittest
 
-from arrays.MatrixDiagonalSum import diagonal_sum
+from arrays.MatrixDiagonalSum import diagonal_sum, diagonal_sum_linear
 
 
 class MyTestCase(unittest.TestCase):
     def test_diagonal_sum(self):
         self.assertEqual(25, diagonal_sum(self, [[1, 2, 3],
+                                                 [4, 5, 6],
+                                                 [7, 8, 9]]))
+
+        self.assertEqual(25, diagonal_sum_linear(self, [[1, 2, 3],
                                                  [4, 5, 6],
                                                  [7, 8, 9]]))
 
@@ -15,8 +19,15 @@ class MyTestCase(unittest.TestCase):
                                                 [1, 1, 1, 1],
                                                 [1, 1, 1, 1]]))
 
+        self.assertEqual(8, diagonal_sum_linear(self, [[1, 1, 1, 1],
+                                                [1, 1, 1, 1],
+                                                [1, 1, 1, 1],
+                                                [1, 1, 1, 1]]))
+
     def test_diagonal_sum_2(self):
         self.assertEqual(5, diagonal_sum(self, [[5]]))
+
+        self.assertEqual(5, diagonal_sum_linear(self, [[5]]))
 
     def test_diagonal_sum_3(self):
         self.assertEqual(55, diagonal_sum(self, [
@@ -26,6 +37,14 @@ class MyTestCase(unittest.TestCase):
             [9, 5, 8, 5]
         ]
                                          ))
+
+        self.assertEqual(55, diagonal_sum_linear(self, [
+            [7, 3, 1, 9],
+            [3, 4, 6, 9],
+            [6, 9, 6, 6],
+            [9, 5, 8, 5]
+        ]
+                                          ))
 
 
 if __name__ == '__main__':
