54e536a883622e655cdbb82d1f745b0f2d57bcb4
solairerove <solairerove@gmail.com>
2023-05-18 13:17:51 +0800

    feature: minimum number of vertices to reach all nodes bfs

--- graphs/MinimumNumberOfVerticesToReachAllNodes.py
+++ graphs/MinimumNumberOfVerticesToReachAllNodes.py
@@ -1,8 +1,9 @@
+import collections
 from typing import List
 
 
 # O(n + e) time || O(n + e) space
-def find_smallest_set_of_vertices(self, n: int, edges: List[List[int]]) -> List[int]:
+def find_smallest_set_of_vertices_dfs(self, n: int, edges: List[List[int]]) -> List[int]:
     adj = {x: [] for x in range(n)}
     for x, y in edges:
         adj[x].append(y)
@@ -24,3 +25,32 @@ def dfs(self, node, adj, visit, res):
             dfs(self, neighbor, adj, visit, res)
         elif neighbor in res:
             res.remove(neighbor)
+
+
+# O(n + e) time || O(n + e) space
+def find_smallest_set_of_vertices_bfs(self, n: int, edges: List[List[int]]) -> List[int]:
+    adj = {x: [] for x in range(n)}
+    for x, y in edges:
+        adj[x].append(y)
+
+    res = set()
+    visit = [False] * n
+    for i in range(n):
+        if not visit[i]:
+            res.add(i)
+            bfs(self, i, adj, visit, res)
+
+    return list(res)
+
+
+def bfs(self, node, adj, visit, res):
+    dq = collections.deque([node])
+    visit[node] = True
+    while dq:
+        node = dq.popleft()
+        for neighbor in adj[node]:
+            if not visit[neighbor]:
+                visit[neighbor] = True
+                dq.append(neighbor)
+            elif neighbor in res:
+                res.remove(neighbor)
--- graphs/test/test_find_smallest_set_of_vertices.py
+++ graphs/test/test_find_smallest_set_of_vertices.py
@@ -1,14 +1,23 @@
 import unittest
 
-from graphs.MinimumNumberOfVerticesToReachAllNodes import find_smallest_set_of_vertices
+from graphs.MinimumNumberOfVerticesToReachAllNodes import find_smallest_set_of_vertices_bfs, \
+    find_smallest_set_of_vertices_dfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_find_smallest_set_of_vertices(self):
-        self.assertEqual([0, 3], find_smallest_set_of_vertices(self, 6, [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]]))
+        self.assertEqual([0, 3], find_smallest_set_of_vertices_dfs(self, 6, [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]]))
+        self.assertEqual([0, 3], find_smallest_set_of_vertices_bfs(self, 6, [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]]))
 
     def test_find_smallest_set_of_vertices_1(self):
-        self.assertEqual([0, 2, 3], find_smallest_set_of_vertices(self, 5, [[0, 1], [2, 1], [3, 1], [1, 4], [2, 4]]))
+        self.assertEqual([0, 2, 3],
+                         find_smallest_set_of_vertices_dfs(self, 5, [[0, 1], [2, 1], [3, 1], [1, 4], [2, 4]]))
+        self.assertEqual([0, 2, 3],
+                         find_smallest_set_of_vertices_bfs(self, 5, [[0, 1], [2, 1], [3, 1], [1, 4], [2, 4]]))
+
+    def test_find_smallest_set_of_vertices_2(self):
+        self.assertEqual([1], find_smallest_set_of_vertices_dfs(self, 3, [[1, 2], [1, 0], [0, 2]]))
+        self.assertEqual([1], find_smallest_set_of_vertices_bfs(self, 3, [[1, 2], [1, 0], [0, 2]]))
 
 
 if __name__ == '__main__':
