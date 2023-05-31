d863d76bfaf86baa970c683e25b3b78d345dcb6c
solairerove <solairerove@gmail.com>
2023-05-31 13:18:50 +0800

    feature: contains duplicate revisit

--- arrays/ContainsDuplicate.py
+++ arrays/ContainsDuplicate.py
@@ -1,6 +1,9 @@
 from typing import List
 
 
+# just compare lengths of array and set
+# also we can sort and check if we have value that appears at least twice
+
 # O(n) time || O(n) space
 def contains_duplicate(self, nums: List[int]) -> bool:
     return len(nums) != len((set(nums)))
