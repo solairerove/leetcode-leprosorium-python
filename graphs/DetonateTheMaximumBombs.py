4d1c98ff3730ab78608d731332fcc2162dcd1a51
solairerove <solairerove@gmail.com>
2023-06-02 13:35:33 +0800

    feature: detonate the maximum bombs dfs revisit

--- graphs/DetonateTheMaximumBombs.py
+++ graphs/DetonateTheMaximumBombs.py
@@ -1,18 +1,19 @@
-import collections
 from typing import List
 
 
 # O(n^3) time || O(n^2) space
 def maximum_detonation(self, bombs: List[List[int]]) -> int:
-    n, res, adj = len(bombs), 0, collections.defaultdict(list)
+    n = len(bombs)
+    adj = {x: [] for x in range(n)}
     for i in range(n):
         for j in range(n):
             if i == j:
                 continue
 
             if bombs[i][2] ** 2 >= (bombs[i][0] - bombs[j][0]) ** 2 + (bombs[i][1] - bombs[j][1]) ** 2:
-                adj[i] += [j]
+                adj[i].append(j)
 
+    res = 0
     for i in range(n):
         visit = {i}
         dfs(self, i, adj, visit)
