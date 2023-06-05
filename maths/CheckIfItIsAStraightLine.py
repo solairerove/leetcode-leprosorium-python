830d81fdc48026d299a612c1c883345acccfbe87
solairerove <solairerove@gmail.com>
2023-06-05 14:11:35 +0800

    feature: check if it is a straight line

---
+++ maths/CheckIfItIsAStraightLine.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+# O(n) time || O(1) space
+def check_straight_line(self, coordinates: List[List[int]]) -> bool:
+    (x0, y0), (x1, y1) = coordinates[: 2]
+    for x, y in coordinates:
+        if (x1 - x0) * (y - y1) != (x - x1) * (y1 - y0):
+            return False
+
+    return True
