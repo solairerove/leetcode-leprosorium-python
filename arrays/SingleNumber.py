21fe5b0e5e4021b5ecb70095a9ca9be41117e210
solairerove <solairerove@gmail.com>
2023-06-05 14:16:49 +0800

    feature: single number revisit

--- arrays/SingleNumber.py
+++ arrays/SingleNumber.py
@@ -1,6 +1,9 @@
 from typing import List
 
 
+# we can use xor here on all elements
+# as result we are going to get single number
+
 # O(n) time || O(1) space
 def single_number(self, nums: List[int]) -> int:
     a = 0
