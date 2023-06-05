1d329cd1a186d2e2fa03ac4cd4cb077ad6d2ca7f
solairerove <solairerove@gmail.com>
2023-06-05 14:13:58 +0800

    feature: check if it is a straight line 1 liner

--- maths/CheckIfItIsAStraightLine.py
+++ maths/CheckIfItIsAStraightLine.py
@@ -9,3 +9,10 @@ def check_straight_line(self, coordinates: List[List[int]]) -> bool:
             return False
 
     return True
+
+
+# O(n) time || O(1) space
+def check_straight_line_one_liner(self, coordinates: List[List[int]]) -> bool:
+    (x0, y0), (x1, y1) = coordinates[: 2]
+
+    return all((x1 - x0) * (y - y1) == (x - x1) * (y1 - y0) for x, y in coordinates)
--- maths/test/test_check_straight_line.py
+++ maths/test/test_check_straight_line.py
@@ -1,14 +1,16 @@
 import unittest
 
-from maths.CheckIfItIsAStraightLine import check_straight_line
+from maths.CheckIfItIsAStraightLine import check_straight_line, check_straight_line_one_liner
 
 
 class MyTestCase(unittest.TestCase):
     def test_check_straight_line(self):
         self.assertEqual(True, check_straight_line(self, [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]))
+        self.assertEqual(True, check_straight_line_one_liner(self, [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]))
 
     def test_check_straight_line_1(self):
         self.assertEqual(False, check_straight_line(self, [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]))
+        self.assertEqual(False, check_straight_line_one_liner(self, [[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]))
 
 
 if __name__ == '__main__':
