8e8ea651a80a14956d8d01147767791277b8aab0
solairerove <solairerove@gmail.com>
2023-06-06 13:43:58 +0800

    feature: can make arithmetic progression from sequence linear

--- maths/CanMakeArithmeticProgressionFromSequence.py
+++ maths/CanMakeArithmeticProgressionFromSequence.py
@@ -1,6 +1,22 @@
 from typing import List
 
 
+# O(n) time || O(n) space
+def can_make_arithmetic_progression(self, arr: List[int]) -> bool:
+    mi, mx, n, s = min(arr), max(arr), len(arr), set(arr)
+    if (mx - mi) % (n - 1) != 0:
+        return False
+
+    diff = (mx - mi) // (n - 1)
+    for _ in range(n):
+        if mi not in s:
+            return False
+
+        mi += diff
+
+    return True
+
+
 # O(n * log(n)) time || O(1) space
 def can_make_arithmetic_progression_sorting(self, arr: List[int]) -> bool:
     arr.sort()
--- maths/test/test_can_make_arithmetic_progression.py
+++ maths/test/test_can_make_arithmetic_progression.py
@@ -1,14 +1,17 @@
 import unittest
 
-from maths.CanMakeArithmeticProgressionFromSequence import can_make_arithmetic_progression_sorting
+from maths.CanMakeArithmeticProgressionFromSequence import can_make_arithmetic_progression_sorting, \
+    can_make_arithmetic_progression
 
 
 class MyTestCase(unittest.TestCase):
     def test_can_make_arithmetic_progression(self):
         self.assertEqual(True, can_make_arithmetic_progression_sorting(self, [3, 5, 1]))
+        self.assertEqual(True, can_make_arithmetic_progression(self, [3, 5, 1]))
 
     def test_can_make_arithmetic_progression_1(self):
         self.assertEqual(False, can_make_arithmetic_progression_sorting(self, [1, 2, 4]))
+        self.assertEqual(False, can_make_arithmetic_progression(self, [1, 2, 4]))
 
 
 if __name__ == '__main__':
