7b59fd0cee43c6610bcf3ee516b30a14dfe82d1b
solairerove <solairerove@gmail.com>
2023-06-27 10:47:49 +0800

    feature: reverse linked list revisit

--- linked_list/ReverseLinkedList.py
+++ linked_list/ReverseLinkedList.py
@@ -3,6 +3,10 @@ from typing import Optional
 from linked_list.ListNode import ListNode
 
 
+# traverse through linked list, relink curr.next to prev, set prev as curr
+# in common it looks like changing direction of linking
+
+
 # O(n) time || O(1) space
 def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
     prev, curr = None, head
