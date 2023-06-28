6c02d5e44b09f1a2eba0425fb928cdce8f2313a9
solairerove <solairerove@gmail.com>
2023-06-28 15:18:43 +0800

    feature: path with maximum probability bellman ford

---
+++ graphs/PathWithMaximumProbability.py
@@ -0,0 +1,21 @@
+import collections
+from typing import List
+
+
+# O(n * m) time || O(n * m) space
+def max_probability(self, n: int, edges: List[List[int]], succ_prob: List[float], start: int, end: int) -> float:
+    g, dq = collections.defaultdict(list), collections.deque([start])
+    for i, (a, b) in enumerate(edges):
+        g[a].append([b, i])
+        g[b].append([a, i])
+
+    p = [0.0] * n
+    p[start] = 1.0
+    while dq:
+        curr = dq.popleft()
+        for neighbor, i in g.get(curr, []):
+            if p[curr] * succ_prob[i] > p[neighbor]:
+                p[neighbor] = p[curr] * succ_prob[i]
+                dq.append(neighbor)
+
+    return p[end]
---
+++ graphs/test/test_max_probability.py
@@ -0,0 +1,22 @@
+import unittest
+
+from graphs.PathWithMaximumProbability import max_probability
+
+
+class MyTestCase(unittest.TestCase):
+    def test_max_probability(self):
+        self.assertEqual(0.25000,
+                         max_probability(self, n=3, edges=[[0, 1], [1, 2], [0, 2]], succ_prob=[0.5, 0.5, 0.2], start=0,
+                                         end=2))
+
+    def test_max_probability_1(self):
+        self.assertEqual(0.30000,
+                         max_probability(self, n=3, edges=[[0, 1], [1, 2], [0, 2]], succ_prob=[0.5, 0.5, 0.3], start=0,
+                                         end=2))
+
+    def test_max_probability_2(self):
+        self.assertEqual(0.00000, max_probability(self, n=3, edges=[[0, 1]], succ_prob=[0.5], start=0, end=2))
+
+
+if __name__ == '__main__':
+    unittest.main()
