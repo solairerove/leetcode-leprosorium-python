e9e348186a8de16f14021e42714029823970a61b
solairerove <solairerove@gmail.com>
2023-06-28 15:24:52 +0800

    feature: path with maximum probability bellman ford revisit

--- graphs/PathWithMaximumProbability.py
+++ graphs/PathWithMaximumProbability.py
@@ -3,19 +3,21 @@ from typing import List
 
 
 # O(n * e) time || O(n + e) space
-def max_probability_bellman_ford(self, n: int, edges: List[List[int]], succ_prob: List[float], start: int, end: int) -> float:
-    g, dq = collections.defaultdict(list), collections.deque([start])
-    for i, (a, b) in enumerate(edges):
-        g[a].append([b, i])
-        g[b].append([a, i])
+def max_probability_bellman_ford(self, n: int, edges: List[List[int]], succ_prob: List[float], start: int,
+                                 end: int) -> float:
+    adj = {x: [] for x in range(n)}
+    for i, (x, y) in enumerate(edges):
+        adj[x].append([y, i])
+        adj[y].append([x, i])
 
-    p = [0.0] * n
-    p[start] = 1.0
+    res = [0.0] * n
+    res[start] = 1.0
+    dq = collections.deque([start])
     while dq:
-        curr = dq.popleft()
-        for neighbor, i in g.get(curr, []):
-            if p[curr] * succ_prob[i] > p[neighbor]:
-                p[neighbor] = p[curr] * succ_prob[i]
+        node = dq.popleft()
+        for neighbor, i in adj.get(node, []):
+            if res[node] * succ_prob[i] > res[neighbor]:
+                res[neighbor] = res[node] * succ_prob[i]
                 dq.append(neighbor)
 
-    return p[end]
+    return res[end]
