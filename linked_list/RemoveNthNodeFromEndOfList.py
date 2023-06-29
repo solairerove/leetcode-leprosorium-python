0b2727d80769f0db78cdfcf003b0e9b068a26d2a
solairerove <solairerove@gmail.com>
2023-06-29 12:32:14 +0800

    feature: remove nth node from end of list revisit

--- linked_list/RemoveNthNodeFromEndOfList.py
+++ linked_list/RemoveNthNodeFromEndOfList.py
@@ -3,6 +3,9 @@ from typing import Optional
 from linked_list.ListNode import ListNode
 
 
+# traverse linked list using two pointers. the first is current, the second is offset to n.
+# when traverse is done, the second pointer will be prev to deleted.
+
 # O(n) time || O(1) space
 def remove_nth_from_end(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
     sentinel = ListNode(-1, head)
