8f60f1af03dd9cb118d14e30c4133e1af22ef536
solairerove <solairerove@gmail.com>
2023-07-21 14:15:20 +0800

    feature: linked list cycle revisit

--- linked_list/LinkedListCycle.py
+++ linked_list/LinkedListCycle.py
@@ -3,9 +3,14 @@ from typing import Optional
 from linked_list.ListNode import ListNode
 
 
+# as usual some algo that we should know to solve this shit.
+
 # O(n) time || O(1) space
 def has_cycle(self, head: Optional[ListNode]) -> bool:
-    slow = fast = head
+    if not head or not head.next:
+        return False
+
+    slow, fast = head, head.next
     while fast and fast.next:
         slow, fast = slow.next, fast.next.next
 
