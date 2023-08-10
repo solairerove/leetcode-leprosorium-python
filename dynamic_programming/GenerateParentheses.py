8df4081b6689577f248edafbd3d8903d7ebd54b3
solairerove <solairerove@gmail.com>
2023-08-10 15:27:14 +0800

    feature: generate parentheses bfs

--- dynamic_programming/GenerateParentheses.py
+++ dynamic_programming/GenerateParentheses.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 # O(n^2) time || O(n) space
-def generate_parenthesis_top_down(self, n: int) -> List[str]:
+def generate_parenthesis_dfs(self, n: int) -> List[str]:
     res = []
 
     def dfs(left, right, s):
@@ -17,5 +17,25 @@ def generate_parenthesis_top_down(self, n: int) -> List[str]:
             dfs(left, right + 1, s + ')')
 
     dfs(0, 0, '')
+
+    return res
+
+
+# O(n^2) time || O(n) space
+def generate_parenthesis_bfs(self, n: int) -> List[str]:
+    res = []
+    left = right = 0
+    q = [(left, right, '')]
+    while q:
+        left, right, s = q.pop()
+
+        if len(s) == n * 2:
+            res.append(s)
+
+        if left < n:
+            q.append((left + 1, right, s + '('))
+
+        if right < left:
+            q.append((left, right + 1, s + ')'))
 
     return res
--- dynamic_programming/test/test_generate_parenthesis_top_down.py
+++ dynamic_programming/test/test_generate_parenthesis_top_down.py
@@ -1,14 +1,16 @@
 import unittest
 
-from dynamic_programming.GenerateParentheses import generate_parenthesis_top_down
+from dynamic_programming.GenerateParentheses import generate_parenthesis_dfs, generate_parenthesis_bfs
 
 
 class MyTestCase(unittest.TestCase):
     def test_generate_parenthesis(self):
-        self.assertEqual(["((()))", "(()())", "(())()", "()(())", "()()()"], generate_parenthesis_top_down(self, 3))
+        self.assertEqual(["((()))", "(()())", "(())()", "()(())", "()()()"], generate_parenthesis_dfs(self, 3))
+        self.assertEqual(['()()()', '()(())', '(())()', '(()())', '((()))'], generate_parenthesis_bfs(self, 3))
 
     def test_generate_parenthesis_1(self):
-        self.assertEqual(["()"], generate_parenthesis_top_down(self, 1))
+        self.assertEqual(["()"], generate_parenthesis_dfs(self, 1))
+        self.assertEqual(["()"], generate_parenthesis_bfs(self, 1))
 
 
 if __name__ == '__main__':
