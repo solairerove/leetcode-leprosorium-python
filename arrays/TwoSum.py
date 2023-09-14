88fed4e8e7b2c5299bacd9d123c8cf8d3c74d41c
solairerove <solairerove@gmail.com>
2023-09-14 14:57:58 +0700

    feature: two sum revisit

--- arrays/TwoSum.py
+++ arrays/TwoSum.py
@@ -5,10 +5,11 @@ from typing import List
 def two_sum(self, nums: List[int], target: int) -> List[int]:
     dic = {}
     for i in range(len(nums)):
-        to_find = target - nums[i]
+        num = nums[i]
+        to_find = target - num
         if to_find in dic:
             return [dic[to_find], i]
-        else:
-            dic[nums[i]] = i
 
-    return []
+        dic[num] = i
+
+    return [-1, -1]
