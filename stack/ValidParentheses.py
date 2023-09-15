f1aa232d051d5029d903a33f8403426b416d46a9
solairerove <solairerove@gmail.com>
2023-09-15 13:57:14 +0700

    feature: valid parentheses revisit

--- stack/ValidParentheses.py
+++ stack/ValidParentheses.py
@@ -1,15 +1,11 @@
 # O(n) time || O(n) space
 def is_valid(self, s: str) -> bool:
-    dic = {")": "(", "]": "[", "}": "{"}
-    stack = []
-    for c in s:
-        if c not in dic:
-            stack.append(c)
-            continue
-
-        if not stack or stack[-1] != dic[c]:
-            return False
-
-        stack.pop()
+    close_to_open, stack = {")": "(", "]": "[", "}": "{"}, []
+    for br in s:
+        if br in close_to_open:
+            if not stack or close_to_open[br] != stack.pop():
+                return False
+        else:
+            stack.append(br)
 
     return not stack
