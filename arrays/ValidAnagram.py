4510bedf4d9b8bd54eca5225f63d150a6b261cd0
solairerove <solairerove@gmail.com>
2023-09-18 20:37:00 +0700

    feature: valid anagram revisit

--- arrays/ValidAnagram.py
+++ arrays/ValidAnagram.py
@@ -4,11 +4,11 @@ def is_anagram(self, s: str, t: str) -> bool:
         return False
 
     arr = [0] * 26
-    for char in s:
-        arr[ord(char) - ord('a')] += 1
+    for ch in s:
+        arr[ord(ch) - ord('a')] += 1
 
-    for char in t:
-        arr[ord(char) - ord('a')] -= 1
+    for ch in t:
+        arr[ord(ch) - ord('a')] -= 1
 
     return max(arr) == 0
 
