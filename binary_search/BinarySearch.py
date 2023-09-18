0378cfba46c782942fd6deaa6608c2866d7f5100
solairerove <solairerove@gmail.com>
2023-09-18 20:33:14 +0700

    feature: binary search revisit

--- binary_search/BinarySearch.py
+++ binary_search/BinarySearch.py
@@ -6,11 +6,11 @@ def search(self, nums: List[int], target: int) -> int:
     low, high = 0, len(nums) - 1
     while low <= high:
         mid = low + (high - low) // 2
-
-        if target < nums[mid]:
-            high = mid - 1
-        elif target > nums[mid]:
+        curr = nums[mid]
+        if curr < target:
             low = mid + 1
+        elif curr > target:
+            high = mid - 1
         else:
             return mid
 
