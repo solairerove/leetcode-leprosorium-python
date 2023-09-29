9ef430c454808d8305def19eb633b3904ae2a90a
solairerove <solairerove@gmail.com>
2023-09-29 14:01:43 +0800

    feature: distinct event pair sum revisit

--- microsoft/DistinctEvenPairSum.py
+++ microsoft/DistinctEvenPairSum.py
@@ -23,29 +23,29 @@ from typing import List
 
 # O(n) time | O(1) space
 def get_distinct_pair(self, arr: List[int]) -> int:
-    n = len(arr)
+    def is_even(i, j):
+        return (arr[i] + arr[j]) % 2 == 0
 
+    n = len(arr)
     if n == 1:
         return 0
 
     if n == 2:
-        # [][]
-        return 1 if (arr[0] + arr[1]) % 2 == 0 else 0
+        return 1 if is_even(0, 1) else 0
 
-    cnt, slow, fast = 0, 0, n - 1
-    if (arr[slow] + arr[fast]) % 2 == 0:
-        cnt, slow, fast = cnt + 1, 1, 2
+    if is_even(0, n - 1):
+        res, low, high = 1, 1, 2
     else:
-        fast = 1
+        res, low, high = 0, 0, 1
 
-    end_idx = n - 2 if cnt == 1 else n - 1
-    while fast <= end_idx:
-        if (arr[slow] + arr[fast]) % 2 == 0:
-            slow, fast, cnt = slow + 2, fast + 2, cnt + 1
-        else:
-            slow, fast = slow + 1, fast + 1
+    end_idx = n - 2 if res == 1 else n - 1
+    while high <= end_idx:
+        if is_even(low, high):
+            low, high, res = low + 1, high + 1, res + 1
 
-    return cnt
+        low, high = low + 1, high + 1
+
+    return res
 
 
 # O(n) time | O(n) space
