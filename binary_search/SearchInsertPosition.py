081e0ab4d4775eb5d321134743b76fde9c6548da
solairerove <solairerove@gmail.com>
2023-10-02 16:17:48 +0800

    feature: search insert position revisit

--- binary_search/SearchInsertPosition.py
+++ binary_search/SearchInsertPosition.py
@@ -3,12 +3,14 @@ from typing import List
 
 # O(log(n)) time || O(1) space
 def search_insert(self, nums: List[int], target: int) -> int:
-    low, high = 0, len(nums)
-    while low < high:
+    low, high = 0, len(nums) - 1
+    while low <= high:
         mid = low + (high - low) // 2
         if target > nums[mid]:
             low = mid + 1
+        elif target < nums[mid]:
+            high = mid - 1
         else:
-            high = mid
+            return mid
 
     return low
