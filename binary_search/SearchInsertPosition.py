9ba1b3cbf2202c4bd086b22915a7f0aa6ee87cfa
solairerove <solairerove@gmail.com>
2023-10-02 16:13:52 +0800

    feature: search insert position revisit

--- binary_search/SearchInsertPosition.py
+++ binary_search/SearchInsertPosition.py
@@ -6,7 +6,6 @@ def search_insert(self, nums: List[int], target: int) -> int:
     low, high = 0, len(nums)
     while low < high:
         mid = low + (high - low) // 2
-
         if target > nums[mid]:
             low = mid + 1
         else:
