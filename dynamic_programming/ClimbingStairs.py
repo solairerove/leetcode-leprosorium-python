8d4ce8aa34bad87382899a603948859743285cf5
solairerove <solairerove@gmail.com>
2023-10-03 13:29:57 +0800

    feature: climbing stairs top down revisit

--- dynamic_programming/ClimbingStairs.py
+++ dynamic_programming/ClimbingStairs.py
@@ -13,17 +13,17 @@ def climb_stairs(self, n: int) -> int:
 
 # O(n) time || O(n) space
 def climb_stairs_top_down(self, n: int) -> int:
-    @lru_cache(n)
+    @lru_cache(None)
     def dp(i):
-        if i == 0:
+        if i > n:
             return 0
 
-        if i == 1:
+        if i == n:
             return 1
 
-        return dp(i - 1) + dp(i - 2)
+        return dp(i + 1) + dp(i + 2)
 
-    return dp(n + 1)
+    return dp(0)
 
 
 # O(n) time || O(n) space
