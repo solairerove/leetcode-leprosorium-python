33f9a21c8d8742ee852c7fca46d28b3e0f1c72d8
solairerove <solairerove@gmail.com>
2023-10-03 13:38:40 +0800

    feature: climbing stairs bottom up revisit

--- dynamic_programming/ClimbingStairs.py
+++ dynamic_programming/ClimbingStairs.py
@@ -31,9 +31,9 @@ def climb_stairs_bottom_up(self, n: int) -> int:
     if n < 3:
         return n
 
-    dp = [0] * (n + 1)
-    dp[1], dp[2] = 1, 2
-    for i in range(3, n + 1):
+    dp = [0] * n
+    dp[0], dp[1] = 1, 2
+    for i in range(2, n):
         dp[i] = dp[i - 1] + dp[i - 2]
 
     return dp[-1]
