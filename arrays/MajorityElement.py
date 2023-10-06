784a5bfc35120deecdfa24171e3645f8641ea452
solairerove <solairerove@gmail.com>
2023-10-06 10:59:50 +0800

    feature: majority element revisit - Boyer-Moore Voting Algorithm

--- arrays/MajorityElement.py
+++ arrays/MajorityElement.py
@@ -5,10 +5,11 @@ from typing import List
 def majority_element(self, nums: List[int]) -> int:
     cnt, can = 0, None
     for num in nums:
+        if cnt == 0:
+            can = num
+
         if can == num:
             cnt += 1
-        elif cnt == 0:
-            can = num
         else:
             cnt -= 1
 
