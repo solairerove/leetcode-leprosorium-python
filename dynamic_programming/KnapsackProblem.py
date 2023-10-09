076549297a88226a21bcc5abe8ea1d9a8ff81f8f
solairerove <solairerove@gmail.com>
2023-10-09 14:17:51 +0800

    feature: knapsack problem revisit

--- dynamic_programming/KnapsackProblem.py
+++ dynamic_programming/KnapsackProblem.py
@@ -3,21 +3,21 @@ from typing import List
 
 
 # O(n * w + n) time || O(n * w) space
-def knapsack_recursion(self, items: List[int], weights: List[int], n: int, capacity: int) -> int:
+def knapsack_top_down(self, items: List[int], weights: List[int], n: int, capacity: int) -> int:
     @lru_cache(None)
-    def helper(ith: int, free_space: int) -> int:
+    def dp(ith: int, free_space: int) -> int:
         if ith >= n - 1 or free_space < 0:
             return 0
 
         if weights[ith + 1] > free_space:
-            return helper(ith + 1, free_space)
-        else:
-            return max(
-                helper(ith + 1, free_space),
-                helper(ith + 1, free_space - weights[ith + 1]) + items[ith + 1]
-            )
+            return dp(ith + 1, free_space)
 
-    return helper(-1, capacity)
+        return max(
+            dp(ith + 1, free_space),
+            dp(ith + 1, free_space - weights[ith + 1]) + items[ith + 1]
+        )
+
+    return dp(-1, capacity)
 
 
 # O(n * capacity) time || O(n * capacity) space
--- dynamic_programming/test/test_knapsack.py
+++ dynamic_programming/test/test_knapsack.py
@@ -1,36 +1,36 @@
 import unittest
 
-from dynamic_programming.KnapsackProblem import knapsack_recursion, knapsack_2d_dp, knapsack_1d_dp
+from dynamic_programming.KnapsackProblem import knapsack_top_down, knapsack_2d_dp, knapsack_1d_dp
 
 
 class MyTestCase(unittest.TestCase):
     def test_knapsack(self):
-        self.assertEqual(3, knapsack_recursion(self, [1, 2, 3], [4, 5, 1], 3, 4))
+        self.assertEqual(3, knapsack_top_down(self, [1, 2, 3], [4, 5, 1], 3, 4))
         self.assertEqual(3, knapsack_2d_dp(self, [1, 2, 3], [4, 5, 1], 3, 4))
         self.assertEqual(3, knapsack_1d_dp(self, [1, 2, 3], [4, 5, 1], 3, 4))
 
     def test_knapsack_1(self):
-        self.assertEqual(0, knapsack_recursion(self, [1, 2, 3], [4, 5, 6], 3, 3))
+        self.assertEqual(0, knapsack_top_down(self, [1, 2, 3], [4, 5, 6], 3, 3))
         self.assertEqual(0, knapsack_2d_dp(self, [1, 2, 3], [4, 5, 6], 3, 3))
         self.assertEqual(0, knapsack_1d_dp(self, [1, 2, 3], [4, 5, 6], 3, 3))
 
     def test_knapsack_2(self):
-        self.assertEqual(220, knapsack_recursion(self, [60, 100, 120], [10, 20, 30], 3, 50))
+        self.assertEqual(220, knapsack_top_down(self, [60, 100, 120], [10, 20, 30], 3, 50))
         self.assertEqual(220, knapsack_2d_dp(self, [60, 100, 120], [10, 20, 30], 3, 50))
         self.assertEqual(220, knapsack_1d_dp(self, [60, 100, 120], [10, 20, 30], 3, 50))
 
     def test_knapsack_3(self):
-        self.assertEqual(6, knapsack_recursion(self, [2, 3, 1, 4], [3, 4, 6, 5], 4, 8))
+        self.assertEqual(6, knapsack_top_down(self, [2, 3, 1, 4], [3, 4, 6, 5], 4, 8))
         self.assertEqual(6, knapsack_2d_dp(self, [2, 3, 1, 4], [3, 4, 6, 5], 4, 8))
         self.assertEqual(6, knapsack_1d_dp(self, [2, 3, 1, 4], [3, 4, 6, 5], 4, 8))
 
     def test_knapsack_4(self):
-        self.assertEqual(3500, knapsack_recursion(self, [3000, 2000, 1500], [4, 3, 1], 3, 4))
+        self.assertEqual(3500, knapsack_top_down(self, [3000, 2000, 1500], [4, 3, 1], 3, 4))
         self.assertEqual(3500, knapsack_2d_dp(self, [3000, 2000, 1500], [4, 3, 1], 3, 4))
         self.assertEqual(3500, knapsack_1d_dp(self, [3000, 2000, 1500], [4, 3, 1], 3, 4))
 
     def test_knapsack_5(self):
-        self.assertEqual(4000, knapsack_recursion(self, [3000, 2000, 1500, 2000], [4, 3, 1, 1], 4, 4))
+        self.assertEqual(4000, knapsack_top_down(self, [3000, 2000, 1500, 2000], [4, 3, 1, 1], 4, 4))
         self.assertEqual(4000, knapsack_2d_dp(self, [3000, 2000, 1500, 2000], [4, 3, 1, 1], 4, 4))
         self.assertEqual(4000, knapsack_1d_dp(self, [3000, 2000, 1500, 2000], [4, 3, 1, 1], 4, 4))
 
