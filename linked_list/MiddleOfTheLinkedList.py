8e6b9629d2bc6044ad4b9d41a7cb1041f58e3321
solairerove <solairerove@gmail.com>
2023-10-11 11:04:31 +0800

    feature: middle of the linked list revisit

--- linked_list/MiddleOfTheLinkedList.py
+++ linked_list/MiddleOfTheLinkedList.py
@@ -3,6 +3,8 @@ from typing import Optional
 from linked_list.ListNode import ListNode
 
 
+# fast traverse two times faster, so when it's in the end, slow is in middle
+
 # O(n) time || O(1) space
 def middle_node(self, head: Optional[ListNode]) -> Optional[ListNode]:
     if not head.next:
