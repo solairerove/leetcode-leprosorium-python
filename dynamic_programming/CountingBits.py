4a84d8e54c8e0a5ea3aff3045e02a684811b77ea
solairerove <solairerove@gmail.com>
2023-10-17 21:39:54 +0800

    feature: counting bits revisit

--- dynamic_programming/CountingBits.py
+++ dynamic_programming/CountingBits.py
@@ -6,6 +6,7 @@ def count_bits(self, n: int) -> List[int]:
     dp = [0] * (n + 1)
 
     for i in range(1, n + 1):
-        dp[i] = dp[i >> 1] + i % 2
+        # dp[i] = dp[i >> 1] + (i & 1)
+        dp[i] = dp[i // 2] + (i % 2)
 
-    return dp[:n + 1]
+    return dp
