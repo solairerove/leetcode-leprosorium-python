0c2b0b4e24a799dc1258381e2a5f0da6d956c5a0
solairerove <solairerove@gmail.com>
2023-10-17 21:30:02 +0800

    feature: backspace string compare two pointers

--- arrays/BackspaceStringCompare.py
+++ arrays/BackspaceStringCompare.py
@@ -50,3 +50,22 @@ def backspace_compare_lambda(self, s: str, t: str) -> bool:
     trim = lambda stack, c: stack[:-1] if c == '#' else stack + c
 
     return reduce(trim, s, "") == reduce(trim, t, "")
+
+
+# O(n + m) time || O(1) space
+def backspace_compare_two_pointers_straight(self, s: str, t: str) -> bool:
+    i, j = len(s) - 1, len(t) - 1
+    back_s = back_t = 0
+    while True:
+        while i >= 0 and (back_s or s[i] == '#'):
+            back_s += 1 if s[i] == '#' else -1
+            i -= 1
+
+        while j >= 0 and (back_t or t[j] == '#'):
+            back_t += 1 if t[j] == '#' else -1
+            j -= 1
+
+        if not (i >= 0 and j >= 0 and s[i] == t[j]):
+            return i == j == -1
+
+        i, j = i - 1, j - 1
--- arrays/test/test_backspace_compare.py
+++ arrays/test/test_backspace_compare.py
@@ -1,7 +1,7 @@
 import unittest
 
 from arrays.BackspaceStringCompare import backspace_compare_two_pointers, backspace_compare_stack, \
-    backspace_compare_stack_2, backspace_compare_lambda
+    backspace_compare_stack_2, backspace_compare_lambda, backspace_compare_two_pointers_straight
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,18 +10,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, backspace_compare_stack(self, "ab#c", "ad#c"))
         self.assertEqual(True, backspace_compare_stack_2(self, "ab#c", "ad#c"))
         self.assertEqual(True, backspace_compare_lambda(self, "ab#c", "ad#c"))
+        self.assertEqual(True, backspace_compare_two_pointers_straight(self, "ab#c", "ad#c"))
 
     def test_backspace_compare_1(self):
         self.assertEqual(True, backspace_compare_two_pointers(self, "ab##", "c#d#"))
         self.assertEqual(True, backspace_compare_stack(self, "ab##", "c#d#"))
         self.assertEqual(True, backspace_compare_stack_2(self, "ab##", "c#d#"))
         self.assertEqual(True, backspace_compare_lambda(self, "ab##", "c#d#"))
+        self.assertEqual(True, backspace_compare_two_pointers_straight(self, "ab##", "c#d#"))
 
     def test_backspace_compare_2(self):
         self.assertEqual(False, backspace_compare_two_pointers(self, "a#c", "b"))
         self.assertEqual(False, backspace_compare_stack(self, "a#c", "b"))
         self.assertEqual(False, backspace_compare_stack_2(self, "a#c", "b"))
         self.assertEqual(False, backspace_compare_lambda(self, "a#c", "b"))
+        self.assertEqual(False, backspace_compare_two_pointers_straight(self, "a#c", "b"))
 
 
 if __name__ == '__main__':
