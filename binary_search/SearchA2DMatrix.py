85008164a133d11f6d3c0de4c05438a7fd3da232
solairerove <solairerove@gmail.com>
2023-10-20 17:18:42 +0800

    feature: search a 2d matrix revisit

--- binary_search/SearchA2DMatrix.py
+++ binary_search/SearchA2DMatrix.py
@@ -7,13 +7,48 @@ def search_matrix(self, matrix: List[List[int]], target: int) -> bool:
     low, high = 0, rows * cols - 1
     while low <= high:
         mid = low + (high - low) // 2
-        num = matrix[mid // cols][mid % cols]
+        mid_row, mid_col = divmod(mid, cols)
 
-        if target == num:
+        if target == matrix[mid_row][mid_col]:
             return True
-        elif target < num:
+        elif target < matrix[mid_row][mid_col]:
             high = mid - 1
         else:
             low = mid + 1
 
     return False
+
+
+# O(log(m) + log(n)) time || O(1) space
+def search_matrix_2bs(self, matrix: List[List[int]], target: int) -> bool:
+    def bs(low, high, row):
+        while low <= high:
+            mid = low + (high - low) // 2
+            if target == matrix[row][mid]:
+                return mid
+            elif target < matrix[row][mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
+
+    def bs_row(low, high):
+        while low <= high:
+            mid = low + (high - low) // 2
+            if matrix[mid][0] <= target <= matrix[mid][len(matrix[mid]) - 1]:
+                return mid
+            elif matrix[mid][0] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
+
+    row = bs_row(0, len(matrix) - 1)
+    if row == -1:
+        return False
+
+    col = bs(0, len(matrix[row]) - 1, row)
+
+    return col != -1
--- binary_search/test/test_search_matrix.py
+++ binary_search/test/test_search_matrix.py
@@ -1,15 +1,19 @@
 import unittest
 
-from binary_search.SearchA2DMatrix import search_matrix
+from binary_search.SearchA2DMatrix import search_matrix, search_matrix_2bs
 
 
 class MyTestCase(unittest.TestCase):
     def test_search_matrix(self):
         self.assertEqual(True, search_matrix(self, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=3))
+        self.assertEqual(True,
+                         search_matrix_2bs(self, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=3))
 
     def test_search_matrix_1(self):
         self.assertEqual(False,
                          search_matrix(self, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=13))
+        self.assertEqual(False,
+                         search_matrix_2bs(self, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=13))
 
 
 if __name__ == '__main__':
