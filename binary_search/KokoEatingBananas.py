5ba52cf09720ac99130955eb334032cc037247ed
solairerove <solairerove@gmail.com>
2023-10-23 10:42:10 +0800

    feature: koko eating bananas revisit

--- binary_search/KokoEatingBananas.py
+++ binary_search/KokoEatingBananas.py
@@ -1,14 +1,15 @@
+import math
 from typing import List
 
 
 # O(n * log(max(p)) time || O(1) space
 def min_eating_speed(self, piles: List[int], h: int) -> int:
     low, high = 1, max(piles)
-    while low <= high:
-        mid = low + (high - low) // 2
-        if sum((pile + mid - 1) // mid for pile in piles) > h:  # (math.ceil(pile / mid)
-            low = mid + 1
+    while low < high:
+        hours, mid = 0, low + (high - low) // 2
+        if sum((math.ceil(pile / mid)) for pile in piles) <= h:
+            high = mid
         else:
-            high = mid - 1
+            low = mid + 1
 
-    return low
+    return high
