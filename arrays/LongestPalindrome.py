bcb19c11c73ec4dbf6374f87d5d9e2d7038eea5a
solairerove <solairerove@gmail.com>
2023-10-24 14:51:52 +0800

    feature: longest palindrome

--- arrays/LongestPalindrome.py
+++ arrays/LongestPalindrome.py
@@ -3,10 +3,6 @@ import collections
 
 # O(n) time | O(1) space
 def longest_palindrome(self, s: str) -> int:
-    res = 0
-    for v in collections.Counter(s).values():
-        res += v // 2 * 2
-        if res % 2 == 0 and v % 2 == 1:
-            res += 1
+    odds = sum(v % 2 for v in collections.Counter(s).values())
 
-    return res
+    return len(s) - odds + bool(odds)
