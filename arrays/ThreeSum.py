36f1edda49e950ed62c56b9a74fa52ba9abf1025
solairerove <solairerove@gmail.com>
2023-10-24 10:19:51 +0800

    feature: three sum revisit

--- arrays/ThreeSum.py
+++ arrays/ThreeSum.py
@@ -3,31 +3,29 @@ from typing import List
 
 # O(n^2) time || O(1) space
 def three_sum(self, nums: List[int]) -> List[List[int]]:
-    if len(nums) == 3:
-        return [nums] if sum(nums) == 0 else []
-
-    res = []
     nums.sort()
-    for i, n in enumerate(nums):
-        if n > 0:
-            break
-
-        if i > 0 and n == nums[i - 1]:
+    triplets = []
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
             continue
 
         low, high = i + 1, len(nums) - 1
         while low < high:
-            tree_sum = n + nums[low] + nums[high]
+            sum_ = nums[i] + nums[low] + nums[high]
 
-            if tree_sum < 0:
-                low += 1
-            elif tree_sum > 0:
-                high -= 1
-            else:
-                res.append([n, nums[low], nums[high]])
+            if sum_ == 0:
+                triplets.append([nums[i], nums[low], nums[high]])
                 low, high = low + 1, high - 1
 
-                while nums[low] == nums[low - 1] and low < high:
+                while low < high and nums[low] == nums[low - 1]:
                     low += 1
 
-    return res
+                while low < high and nums[high] == nums[high + 1]:
+                    high -= 1
+
+            elif sum_ < 0:
+                low += 1
+            else:
+                high -= 1
+
+    return triplets
