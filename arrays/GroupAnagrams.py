5ba750808f1dce8cbfde25ad7f1b57c14521c088
solairerove <solairerove@gmail.com>
2023-10-24 12:27:30 +0800

    feature: group anagrams revisit

--- arrays/GroupAnagrams.py
+++ arrays/GroupAnagrams.py
@@ -5,8 +5,9 @@ from typing import List
 # O(w * n * log(n)) time || O(wn) space
 def group_anagrams(self, strs: List[str]) -> List[List[str]]:
     dic = collections.defaultdict(list)
+
     for s in strs:
-        dic[''.join(sorted(s))].append(s)
+        dic[tuple(sorted(s))].append(s)
 
     return list(dic.values())
 
