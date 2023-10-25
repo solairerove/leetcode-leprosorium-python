7ae6bd36ee58ae34fc32540ebf7aa14870691f2b
solairerove <solairerove@gmail.com>
2023-10-25 14:22:40 +0800

    feature: four sum revisit

--- arrays/FourSum.py
+++ arrays/FourSum.py
@@ -4,21 +4,21 @@ from typing import List
 # O(n^3) time || O(1) space
 def four_sum(self, nums: List[int], target: int) -> List[List[int]]:
     nums.sort()
-    res, n = [], len(nums)
-    for i in range(n - 3):
+    quadruplets = []
+    for i in range(len(nums) - 3):
         if i > 0 and nums[i] == nums[i - 1]:
             continue
 
-        for j in range(i + 1, n - 2):
+        for j in range(i + 1, len(nums) - 2):
             if j > i + 1 and nums[j] == nums[j - 1]:
                 continue
 
-            low, high = j + 1, n - 1
+            low, high = j + 1, len(nums) - 1
             while low < high:
                 sum_ = nums[i] + nums[j] + nums[low] + nums[high]
 
                 if sum_ == target:
-                    res.append([nums[i], nums[j], nums[low], nums[high]])
+                    quadruplets.append([nums[i], nums[j], nums[low], nums[high]])
                     low, high = low + 1, high - 1
 
                     while low < high and nums[low] == nums[low - 1]:
@@ -32,4 +32,4 @@ def four_sum(self, nums: List[int], target: int) -> List[List[int]]:
                 else:
                     high -= 1
 
-    return res
+    return quadruplets
