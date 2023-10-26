6e9c7a24b40fe26f5174b22627381090a90d0dc6
solairerove <solairerove@gmail.com>
2023-10-26 14:49:39 +0800

    feature: number of islands revisit bfs revisit

--- graphs/NumberOfIslands.py
+++ graphs/NumberOfIslands.py
@@ -59,18 +59,22 @@ def num_islands_bfs(self, grid: List[List[str]]) -> int:
     n, m = len(grid), len(grid[0])
     res = 0
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    def bfs(i, j):
+        dq = collections.deque([(i, j)])
+        while dq:
+            x, y = dq.popleft()
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == "1":
+                    grid[nx][ny] = "0"
+                    dq.append((nx, ny))
+
     for i in range(n):
         for j in range(m):
             if grid[i][j] == "1":
                 grid[i][j] = "0"
                 res += 1
-                dq = collections.deque([(i, j)])
-                while dq:
-                    x, y = dq.popleft()
-                    for dx, dy in directions:
-                        nx, ny = x + dx, y + dy
-                        if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == "1":
-                            dq.append((nx, ny))
-                            grid[nx][ny] = "0"
+                bfs(i, j)
 
     return res
