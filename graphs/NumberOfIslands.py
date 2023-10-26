e1c50ef35ea64e09f08f09a791f782354c1a1221
solairerove <solairerove@gmail.com>
2023-10-26 14:40:41 +0800

    feature: number of islands revisit bfs

--- graphs/NumberOfIslands.py
+++ graphs/NumberOfIslands.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 
@@ -26,5 +27,52 @@ def num_islands_dfs(self, grid: List[List[str]]) -> int:
             if grid[i][j] == "1":
                 res += 1
                 dfs(i, j)
+
+    return res
+
+
+# O(n * m) time || O(n * m) space
+def num_islands_dfs_shorter(self, grid: List[List[str]]) -> int:
+    if not grid:
+        return 0
+
+    n, m = len(grid), len(grid[0])
+
+    def dfs(i, j):
+        if 0 <= i < n and 0 <= j < m and grid[i][j] == "1":
+            grid[i][j] = "0"
+            list(map(dfs, [i - 1, i + 1, i, i], [j, j, j - 1, j + 1]))
+
+    res = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "1":
+                res += 1
+                dfs(i, j)
+
+    return res
+
+
+# O(n * m) time || O(min(n, m)) space
+def num_islands_bfs(self, grid: List[List[str]]) -> int:
+    if not grid:
+        return 0
+
+    n, m = len(grid), len(grid[0])
+    res = 0
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "1":
+                grid[i][j] = "0"
+                res += 1
+                dq = collections.deque([(i, j)])
+                while dq:
+                    x, y = dq.popleft()
+                    for dx, dy in directions:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == "1":
+                            dq.append((nx, ny))
+                            grid[nx][ny] = "0"
 
     return res
--- graphs/test/test_num_islands.py
+++ graphs/test/test_num_islands.py
@@ -1,6 +1,6 @@
 import unittest
 
-from graphs.NumberOfIslands import num_islands_dfs
+from graphs.NumberOfIslands import num_islands_dfs, num_islands_dfs_shorter, num_islands_bfs
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,8 +12,36 @@ class MyTestCase(unittest.TestCase):
             ["0", "0", "0", "0", "0"]
         ]))
 
+        self.assertEqual(1, num_islands_dfs_shorter(self, [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]
+        ]))
+
+        self.assertEqual(1, num_islands_bfs(self, [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]
+        ]))
+
     def test_num_islands_1(self):
         self.assertEqual(3, num_islands_dfs(self, [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"]
+        ]))
+
+        self.assertEqual(3, num_islands_dfs_shorter(self, [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"]
+        ]))
+
+        self.assertEqual(3, num_islands_bfs(self, [
             ["1", "1", "0", "0", "0"],
             ["1", "1", "0", "0", "0"],
             ["0", "0", "1", "0", "0"],
