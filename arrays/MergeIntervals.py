a98ec90186ed0e9cab5f7ece145ebc0a0da681fa
solairerove <solairerove@gmail.com>
2023-10-27 16:57:37 +0800

    feature: merge intervals revisit

--- arrays/MergeIntervals.py
+++ arrays/MergeIntervals.py
@@ -1,14 +1,15 @@
 from typing import List
 
 
-# O(n * log(n)) time || O(log(n)) space
+# O(n * log(n)) time || O(n) space
 def merge(self, intervals: List[List[int]]) -> List[List[int]]:
     intervals.sort(key=lambda x: x[0])
-    merged = []
-    for interval in intervals:
-        if not merged or merged[-1][1] < interval[0]:
-            merged.append(interval)
-        else:
-            merged[-1][1] = max(merged[-1][1], interval[1])
 
-    return merged
+    res = [intervals[0]]
+    for i in range(1, len(intervals)):
+        if intervals[i][0] > res[-1][1]:
+            res.append(intervals[i])
+        else:
+            res[-1][1] = max(res[-1][1], intervals[i][1])
+
+    return res
