05d388f2d24ccb432a423e8be643bb909804d42f
solairerove <solairerove@gmail.com>
2023-10-27 14:22:52 +0800

    feature: remove invalid parentheses revisit

--- arrays/RemoveInvalidParentheses.py
+++ arrays/RemoveInvalidParentheses.py
@@ -1,9 +1,10 @@
+import collections
 from typing import List
 
 
 # TODO: create search or dfs-bfs topic
 
-# O(n) time || O(n) space
+# O(n * 2 ^ n) time || O(2 ^ n) space
 def remove_invalid_parentheses(self, s: str) -> List[str]:
     def is_valid(string):
         cnt = 0
@@ -19,20 +20,19 @@ def remove_invalid_parentheses(self, s: str) -> List[str]:
         return cnt == 0
 
     res = []
-    stack, visited, found = [s], {s}, False
-    while stack and not found:
-        for curr_str in stack:
+    dq, visited, found = collections.deque([s]), {str}, False
+    while dq:
+        curr_str = dq.popleft()
+        if curr_str not in visited:
+            visited.add(curr_str)
+
             if is_valid(curr_str):
                 found = True
                 res.append(curr_str)
-            elif not found:
-                for i in range(len(curr_str)):
-                    if curr_str[i] not in ['(', ')']:
-                        continue
 
-                    new_str = curr_str[:i] + curr_str[i + 1:]
-                    if new_str not in visited:
-                        visited.add(new_str)
-                        stack.append(new_str)
+            if not found:
+                for i, c in enumerate(curr_str):
+                    if c in "()":
+                        dq.append(curr_str[:i] + curr_str[i + 1:])
 
     return res
