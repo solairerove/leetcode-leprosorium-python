b8598296b6f7e97071904264e7ba8963aa32d263
solairerove <solairerove@gmail.com>
2023-10-27 12:34:58 +0800

    feature: squares of a sorted array revisit

--- arrays/SquaresOfASortedArray.py
+++ arrays/SquaresOfASortedArray.py
@@ -3,15 +3,15 @@ from typing import List
 
 # O(n) time || O(1) space
 def sorted_squares(self, nums: List[int]) -> List[int]:
-    n = len(nums)
-    res = [0] * n
-    low, high, pos = 0, n - 1, n - 1
+    res = [0] * len(nums)
+    pos = len(nums) - 1
+    low, high = 0, len(nums) - 1
     while low <= high:
         if abs(nums[low]) > abs(nums[high]):
-            res[pos] = nums[low] * nums[low]
+            res[pos] = nums[low] ** 2
             low += 1
         else:
-            res[pos] = nums[high] * nums[high]
+            res[pos] = nums[high] ** 2
             high -= 1
         pos -= 1
 
