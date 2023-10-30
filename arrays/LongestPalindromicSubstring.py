7c2025ccace54fbee4d7dd2551a60615209f0118
solairerove <solairerove@gmail.com>
2023-10-30 12:39:28 +0800

    feature: longest palindromic substring revisit

--- arrays/LongestPalindromicSubstring.py
+++ arrays/LongestPalindromicSubstring.py
@@ -1,9 +1,9 @@
 # O(n^2) time || O(1) space
 def longest_palindrome(self, s: str) -> str:
-    def get_palindrome_from(low, high):
-        while low >= 0 and high < len(s) and s[low] == s[high]:
-            low, high = low - 1, high + 1
+    def expand(i, j):
+        while i >= 0 and j < len(s) and s[i] == s[j]:
+            i, j = i - 1, j + 1
 
-        return s[low + 1: high]
+        return s[i + 1: j]
 
-    return max([get_palindrome_from(i, j) for i in range(len(s)) for j in (i, i + 1)], key=len)
+    return max([expand(i, j) for i in range(len(s)) for j in (i, i + 1)], key=len)
