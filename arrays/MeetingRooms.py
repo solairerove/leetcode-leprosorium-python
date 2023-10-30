8e7f036fa3d253356f2632e7d8677f11841f5fd0
solairerove <solairerove@gmail.com>
2023-10-30 13:09:05 +0800

    feature: meeting room revisit shorter solution

--- arrays/MeetingRooms.py
+++ arrays/MeetingRooms.py
@@ -14,14 +14,7 @@ def can_attend_meetings(self, intervals: List[List[int]]) -> bool:
 
 
 # O(n * log(n)) time || O(1) space
-def can_attend_meetings_short(self, intervals: List[List[int]]) -> bool:
-    intervals.sort()
+def can_attend_meetings_shorter(self, intervals: List[List[int]]) -> bool:
+    intervals.sort(key=lambda x: x[0])
 
     return all(intervals[i][0] >= intervals[i - 1][1] for i in range(1, len(intervals)))
-
-
-# O(n * log(n)) time || O(1) space
-def can_attend_meetings_short_1(self, intervals: List[List[int]]) -> bool:
-    intervals.sort()
-
-    return all(b[0] >= a[1] for a, b in zip(intervals, intervals[1:]))
