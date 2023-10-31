e606294d93826572a3518849b8979e8855fb9d4a
solairerove <solairerove@gmail.com>
2023-10-31 15:47:04 +0800

    feature: non overlapping intervals revisit

--- greedy/NonOverlappingIntervals.py
+++ greedy/NonOverlappingIntervals.py
@@ -7,11 +7,12 @@ from typing import List
 def erase_overlap_intervals(self, intervals: List[List[int]]) -> int:
     intervals.sort(key=lambda x: x[1])
 
-    end, res = intervals[0][1], 0
+    res = 0
+    end = intervals[0][1]
     for interval in intervals[1:]:
-        if interval[0] >= end:
-            end = interval[1]
-        else:
+        if interval[0] < end:
             res += 1
+        else:
+            end = interval[1]
 
     return res
