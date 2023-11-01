983ed7a4fb7ad23c87b4d3d4f5dbe423c00d507d
solairerove <solairerove@gmail.com>
2023-11-01 13:34:43 +0800

    feature: top k frequent elements quickselect more tests

--- heap/test/test_top_k_frequent.py
+++ heap/test/test_top_k_frequent.py
@@ -14,6 +14,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([1], top_k_frequent_linear(self, [1], 1))
         self.assertEqual([1], top_k_frequent_quickselect(self, [1], 1))
 
+    def test_top_k_frequent_2(self):
+        self.assertEqual([6, 5, 4], top_k_frequent_heap(self, [4, 4, 5, 5, 5, 1, 2, 3, 9, 6, 6, 6, 6], 3))
+        self.assertEqual([6, 5, 4], top_k_frequent_linear(self, [4, 4, 5, 5, 5, 1, 2, 3, 9, 6, 6, 6, 6], 3))
+        self.assertEqual([6, 5, 4], top_k_frequent_quickselect(self, [4, 4, 5, 5, 5, 1, 2, 3, 9, 6, 6, 6, 6], 3))
+
 
 if __name__ == '__main__':
     unittest.main()
