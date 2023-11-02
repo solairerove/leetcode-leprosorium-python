d261b9e023a993db218663aa0941f129ab973388
solairerove <solairerove@gmail.com>
2023-11-02 15:56:44 +0800

    feature: find the kth largest integer in the array quickselect

--- heap/FindTheKthLargestIntegerInTheArray.py
+++ heap/FindTheKthLargestIntegerInTheArray.py
@@ -1,5 +1,41 @@
 import heapq
+import random
 from typing import List
+
+
+# O(n) time || O(n) space
+def kth_largest_number_quickselect(self, nums: List[str], k: int) -> str:
+    return str(quickselect([int(num) for num in nums], 0, len(nums) - 1, len(nums) - k))
+
+
+def quickselect(arr, low, high, k):
+    if low == high:
+        return arr[low]
+
+    pivot_idx = random.randint(low, high)
+    lt, gt = partition(arr, low, high, pivot_idx)
+    if k < lt:
+        return quickselect(arr, low, lt - 1, k)
+    elif k <= gt:
+        return arr[k]
+    else:
+        return quickselect(arr, gt + 1, high, k)
+
+
+def partition(arr, low, high, pivot_idx):
+    pivot = arr[pivot_idx]
+    lt, i, gt = low, low, high
+    while i <= gt:
+        if arr[i] < pivot:
+            arr[lt], arr[i] = arr[i], arr[lt]
+            lt, i = lt + 1, i + 1
+        elif arr[i] > pivot:
+            arr[gt], arr[i] = arr[i], arr[gt]
+            gt -= 1
+        else:
+            i += 1
+
+    return lt, gt
 
 
 # O(n * log(k * m)) time || O(k) space
--- heap/test/test_kth_largest_number.py
+++ heap/test/test_kth_largest_number.py
@@ -1,17 +1,20 @@
 import unittest
 
-from heap.FindTheKthLargestIntegerInTheArray import kth_largest_number_heap
+from heap.FindTheKthLargestIntegerInTheArray import kth_largest_number_heap, kth_largest_number_quickselect
 
 
 class MyTestCase(unittest.TestCase):
     def test_test_kth_largest_number(self):
         self.assertEqual("3", kth_largest_number_heap(self, nums=["3", "6", "7", "10"], k=4))
+        self.assertEqual("3", kth_largest_number_quickselect(self, nums=["3", "6", "7", "10"], k=4))
 
     def test_test_kth_largest_number_1(self):
         self.assertEqual("2", kth_largest_number_heap(self, nums=["2", "21", "12", "1"], k=3))
+        self.assertEqual("2", kth_largest_number_quickselect(self, nums=["2", "21", "12", "1"], k=3))
 
     def test_test_kth_largest_number_2(self):
         self.assertEqual("0", kth_largest_number_heap(self, nums=["0", "0"], k=2))
+        self.assertEqual("0", kth_largest_number_quickselect(self, nums=["0", "0"], k=2))
 
 
 if __name__ == '__main__':
