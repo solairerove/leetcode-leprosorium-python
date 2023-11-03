bdfb2d5e9d55d5835be38f6bbc6e555ef8104cd2
solairerove <solairerove@gmail.com>
2023-11-03 15:25:04 +0800

    feature: top k frequent words update space-time complexity

--- heap/TopKFrequentWords.py
+++ heap/TopKFrequentWords.py
@@ -6,15 +6,17 @@ from typing import List
 # O(n + m * log(m)) time || O(m) space
 # n is number of words in list
 # m is number of unique words
+# k is number of top freq words
 def top_k_frequent_sorting(self, words: List[str], k: int) -> List[str]:
     cnt = collections.Counter(words)
 
     return sorted(cnt, key=lambda word: (-cnt[word], word))[:k]
 
 
-# O(n + m * log(k)) time || O(m + k) space
+# O(n + k * log(m)) time || O(m) space
 # n is number of words in list
 # m is number of unique words
+# k is number of top freq words
 def top_k_frequent_heap(self, words: List[str], k: int) -> List[str]:
     heap = [(-freq, word) for word, freq in collections.Counter(words).items()]
     heapq.heapify(heap)
