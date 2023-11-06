7aaf0146bcb3c0feb252975334a6d34338240ab5
solairerove <solairerove@gmail.com>
2023-11-06 19:02:44 +0800

    feature: container with most water revisit

--- arrays/ContainerWithMostWater.py
+++ arrays/ContainerWithMostWater.py
@@ -3,12 +3,16 @@ from typing import List
 
 # O(n) time || O(1) space
 def max_area(self, height: List[int]) -> int:
-    area, low, high = 0, 0, len(height) - 1
+    res = 0
+    low, high = 0, len(height) - 1
     while low < high:
-        area = max(area, min(height[low], height[high]) * (high - low))
+        curr_width = high - low
+        curr_height = min(height[low], height[high])
+        res = max(res, curr_width * curr_height)
+
         if height[low] <= height[high]:
             low += 1
         else:
             high -= 1
 
-    return area
+    return res
