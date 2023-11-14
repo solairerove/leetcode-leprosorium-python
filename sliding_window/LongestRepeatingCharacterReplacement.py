c1ec017c98e9bf98bc56a4037cf47b97fd6ba044
solairerove <solairerove@gmail.com>
2023-11-14 14:51:20 +0800

    feature: longest repeating character replacement revisit, add comments

--- sliding_window/LongestRepeatingCharacterReplacement.py
+++ sliding_window/LongestRepeatingCharacterReplacement.py
@@ -7,8 +7,10 @@ def character_replacement(self, s: str, k: int) -> int:
     low = 0
     for high in range(len(s)):
         freq[s[high]] += 1
+        # the maximum frequency of any character in the current window.
         max_freq = max(max_freq, freq[s[high]])
 
+        # If the number of other characters (i.e., window length - max_freq) is greater than k
         if (high - low + 1) - max_freq > k:
             freq[s[low]] -= 1
             low += 1
