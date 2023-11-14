db869b5e269bef3328b1737307d50bff4bb130ee
solairerove <solairerove@gmail.com>
2023-11-14 15:39:53 +0800

    feature: same tree dfs revisit

--- trees/SameTree.py
+++ trees/SameTree.py
@@ -20,15 +20,15 @@ def is_same_tree_dfs(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool
     stack = [(p, q)]
     while stack:
         node1, node2 = stack.pop()
+
         if not node1 and not node2:
             continue
-        elif None in [node1, node2]:
+
+        if not node1 or not node2 or node1.val != node2.val:
             return False
-        else:
-            if node1.val != node2.val:
-                return False
-            stack.append((node1.right, node2.right))
-            stack.append((node1.left, node2.left))
+
+        stack.append((node1.left, node2.left))
+        stack.append((node1.right, node2.right))
 
     return True
 
