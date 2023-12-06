da8b7c2d4f113861d12f63b506a873d385c9ed33
solairerove <solairerove@gmail.com>
2023-12-06 13:57:55 +0800

    feature: three sum revisit

--- arrays/ThreeSum.py
+++ arrays/ThreeSum.py
@@ -4,17 +4,16 @@ from typing import List
 # O(n^2) time || O(1) space
 def three_sum(self, nums: List[int]) -> List[List[int]]:
     nums.sort()
-    triplets = []
+    res = []
     for i in range(len(nums) - 2):
         if i > 0 and nums[i] == nums[i - 1]:
             continue
 
         low, high = i + 1, len(nums) - 1
         while low < high:
-            sum_ = nums[i] + nums[low] + nums[high]
-
-            if sum_ == 0:
-                triplets.append([nums[i], nums[low], nums[high]])
+            _sum = nums[i] + nums[low] + nums[high]
+            if _sum == 0:
+                res.append([nums[i], nums[low], nums[high]])
                 low, high = low + 1, high - 1
 
                 while low < high and nums[low] == nums[low - 1]:
@@ -23,9 +22,9 @@ def three_sum(self, nums: List[int]) -> List[List[int]]:
                 while low < high and nums[high] == nums[high + 1]:
                     high -= 1
 
-            elif sum_ < 0:
+            elif _sum < 0:
                 low += 1
             else:
                 high -= 1
 
-    return triplets
+    return res
