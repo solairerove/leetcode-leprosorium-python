58437cb7fd460db11d9913495b73cd142dce9f19
solairerove <solairerove@gmail.com>
2024-01-02 14:06:31 +0800

    feature: two sum revisit

--- arrays/TwoSum.py
+++ arrays/TwoSum.py
@@ -1,10 +1,12 @@
+import collections
 from typing import List
 
 
 # O(n) time || O(n) space
 def two_sum(self, nums: List[int], target: int) -> List[int]:
-    dic = {}
+    dic = collections.defaultdict(int)
     for i, num in enumerate(nums):
         if target - num in dic:
-            return [dic[target - num], i]
+            return [i, dic[target - num]]
+
         dic[num] = i
--- arrays/test/test_two_sum.py
+++ arrays/test/test_two_sum.py
@@ -5,13 +5,13 @@ from arrays.TwoSum import two_sum
 
 class MyTestCase(unittest.TestCase):
     def test_two_sum(self):
-        self.assertEqual([0, 1], two_sum(self, [2, 7, 11, 15], 9))
+        self.assertEqual([1, 0], two_sum(self, [2, 7, 11, 15], 9))
 
     def test_two_sum_1(self):
-        self.assertEqual([1, 2], two_sum(self, [3, 2, 4], 6))
+        self.assertEqual([2, 1], two_sum(self, [3, 2, 4], 6))
 
     def test_two_sum_2(self):
-        self.assertEqual([0, 1], two_sum(self, [3, 3], 6))
+        self.assertEqual([1, 0], two_sum(self, [3, 3], 6))
 
 
 if __name__ == '__main__':
