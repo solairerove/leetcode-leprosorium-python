bfe9c70c1a20587441c227b9375f45a5354b015b
solairerove <solairerove@gmail.com>
2024-01-10 17:15:17 +0800

    feature: flood fill revisit

--- arrays/FloodFill.py
+++ arrays/FloodFill.py
@@ -4,24 +4,26 @@ from typing import List
 # O(n) time || O(n) space
 def flood_fill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
     color_to_change = image[sr][sc]
-    if color == color_to_change:
+    if color_to_change == color:
         return image
 
     def dfs(r, c):
-        if color_to_change == image[r][c]:
-            image[r][c] = color
+        if image[r][c] != color_to_change:
+            return
 
-            if r >= 1:
-                dfs(r - 1, c)
+        image[r][c] = color
 
-            if r < len(image) - 1:
-                dfs(r + 1, c)
+        if r > 0:
+            dfs(r - 1, c)
 
-            if c >= 1:
-                dfs(r, c - 1)
+        if r < len(image) - 1:
+            dfs(r + 1, c)
 
-            if c < len(image[0]) - 1:
-                dfs(r, c + 1)
+        if c > 0:
+            dfs(r, c - 1)
+
+        if c < len(image[0]) - 1:
+            dfs(r, c + 1)
 
     dfs(sr, sc)
 
